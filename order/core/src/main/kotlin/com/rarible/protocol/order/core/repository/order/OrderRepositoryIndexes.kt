74a0ada42c012586636c288ffc049830f92ddb7b
sergey.anufriev <s.anufriev@rarible.com>
2021-08-19 15:19:55 +0700

    Refactoring order indexes creation

---
+++ order/core/src/main/kotlin/com/rarible/protocol/order/core/repository/order/OrderRepositoryIndexes.kt
@@ -0,0 +1,73 @@
+package com.rarible.protocol.order.core.repository.order
+
+import com.rarible.protocol.order.core.model.*
+import org.springframework.data.domain.Sort
+import org.springframework.data.mongodb.core.index.Index
+
+internal object OrderRepositoryIndexes {
+
+    private val SELL_ORDERS_DEFINITION = Index()
+        .on("${Order::make.name}.${Asset::type.name}.${AssetType::nft.name}", Sort.Direction.ASC)
+        .on(OrderVersion::createdAt.name, Sort.Direction.ASC)
+        .on("_id", Sort.Direction.ASC)
+        .background()
+
+    private val SELL_ORDERS_BY_ITEM_DEFINITION = Index()
+        .on("${Order::make.name}.${Asset::type.name}.${Erc721AssetType::token.name}", Sort.Direction.ASC)
+        .on("${Order::make.name}.${Asset::type.name}.${Erc721AssetType::tokenId.name}", Sort.Direction.ASC)
+        .on(Order::makePriceUsd.name, Sort.Direction.ASC)
+        .on("_id", Sort.Direction.ASC)
+        .background()
+
+    private val SELL_ORDERS_BY_COLLECTION_DEFINITION = Index()
+        .on("${Order::make.name}.${Asset::type.name}.${AssetType::nft.name}", Sort.Direction.ASC)
+        .on("${Order::make.name}.${Asset::type.name}.${Erc721AssetType::token.name}", Sort.Direction.ASC)
+        .on(Order::lastUpdateAt.name, Sort.Direction.ASC)
+        .on("_id", Sort.Direction.ASC)
+        .background()
+
+    private val SELL_ORDERS_BY_MAKER_DEFINITION = Index()
+        .on("${Order::make.name}.${Asset::type.name}.${AssetType::nft.name}", Sort.Direction.ASC)
+        .on(Order::maker.name, Sort.Direction.ASC)
+        .on(Order::lastUpdateAt.name, Sort.Direction.ASC)
+        .on("_id", Sort.Direction.ASC)
+        .background()
+
+    private val BIDS_BY_ITEM_DEFINITION = Index()
+        .on("${Order::take.name}.${Asset::type.name}.${Erc721AssetType::token.name}", Sort.Direction.ASC)
+        .on("${Order::take.name}.${Asset::type.name}.${Erc721AssetType::tokenId.name}", Sort.Direction.ASC)
+        .on(Order::takePriceUsd.name, Sort.Direction.ASC)
+        .on("_id", Sort.Direction.ASC)
+        .background()
+
+    private val BIDS_BY_MAKER_DEFINITION = Index()
+        .on("${Order::take.name}.${Asset::type.name}.${AssetType::nft.name}", Sort.Direction.ASC)
+        .on(Order::maker.name, Sort.Direction.ASC)
+        .on(Order::lastUpdateAt.name, Sort.Direction.ASC)
+        .on("_id", Sort.Direction.ASC)
+        .background()
+
+    private val BY_LAST_UPDATE_DEFINITION = Index()
+        .on(Order::lastUpdateAt.name, Sort.Direction.ASC)
+        .background()
+
+    private val BY_MAKE_STOCK_DEFINITION = Index()
+        // orders with non-zero makeStock should be first
+        .on(Order::makeStock.name, Sort.Direction.DESC)
+        // recently updated orders should be first
+        .on(Order::lastUpdateAt.name, Sort.Direction.DESC)
+        // for queries with continuation
+        .on("_id", Sort.Direction.ASC)
+        .background()
+
+    val ALL_INDEXES = listOf(
+        SELL_ORDERS_DEFINITION,
+        SELL_ORDERS_BY_ITEM_DEFINITION,
+        SELL_ORDERS_BY_COLLECTION_DEFINITION,
+        SELL_ORDERS_BY_MAKER_DEFINITION,
+        BIDS_BY_ITEM_DEFINITION,
+        BIDS_BY_MAKER_DEFINITION,
+        BY_LAST_UPDATE_DEFINITION,
+        BY_MAKE_STOCK_DEFINITION
+    )
+}
