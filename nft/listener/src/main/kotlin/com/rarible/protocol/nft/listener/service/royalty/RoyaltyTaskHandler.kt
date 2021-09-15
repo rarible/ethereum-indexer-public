7f26a63d2924aaf7f44209eadde581dbd8bb3a38
Roman Novikov <mystdeim@gmail.com>
2021-09-15 15:29:37 +0300

    RPN-114: fix according to review

--- nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/royalty/RoyaltyTaskHandler.kt
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/royalty/RoyaltyTaskHandler.kt
@@ -1,28 +1,25 @@
 package com.rarible.protocol.nft.listener.service.royalty
 
+import com.rarible.core.task.RunTask
 import com.rarible.core.task.TaskHandler
-import com.rarible.core.task.TaskRepository
-import com.rarible.core.task.TaskStatus
 import com.rarible.protocol.nft.core.repository.item.ItemRepository
 import com.rarible.protocol.nft.core.service.RoyaltyService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.springframework.stereotype.Component
 import scalether.domain.Address
 
 @Component
 class RoyaltyTaskHandler(
     private val itemRepository: ItemRepository,
-    private val royaltyService: RoyaltyService,
-    private val taskRepository: TaskRepository
+    private val royaltyService: RoyaltyService
 ) : TaskHandler<Address> {
 
     override val type: String
         get() = ROYALTY_REDUCE
 
-    override suspend fun isAbleToRun(param: String): Boolean {
-        return !verifyCompleted()
+    override fun getAutorunParams(): List<RunTask> {
+        return listOf(RunTask("", null))
     }
 
     override fun runLongTask(from: Address?, param: String): Flow<Address> {
@@ -31,11 +28,6 @@ class RoyaltyTaskHandler(
                 royaltyService.getRoyalty(item.token, item.tokenId)
                 item.token
             }
-    }
-
-    private suspend fun verifyCompleted(): Boolean {
-        val task = taskRepository.findByTypeAndParam(ROYALTY_REDUCE, "").awaitFirstOrNull()
-        return task?.lastStatus == TaskStatus.COMPLETED
     }
 
     companion object {
