19c3fe4f0c2027ac076a40b4a2c53f07553c6b4a
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-09-21 17:28:50 +0300

    Disable flaky ItemPropertiesServiceTest.

--- nft/api/src/test/kotlin/com/rarible/protocol/nft/api/service/item/meta/ItemPropertiesServiceTest.kt
+++ nft/api/src/test/kotlin/com/rarible/protocol/nft/api/service/item/meta/ItemPropertiesServiceTest.kt
@@ -26,6 +26,7 @@ import java.math.BigInteger
 import java.time.temporal.ChronoUnit
 
 @Tag("manual")
+@Disabled
 class ItemPropertiesServiceTest {
     private val tokenRepository = mockk<TokenRepository>()
     private val lazyNftItemHistoryRepository = mockk<LazyNftItemHistoryRepository>()
@@ -66,7 +67,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun cryptoKitties() {
         val props = kittiesCacheDescriptor.get("${ItemPropertiesService.CRYPTO_KITTIES}:1001").block()!!
         assertEquals(props.name, "TheFirst")
@@ -82,7 +82,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun getCustomProperties() {
         val props = testing.getProperties(Address.apply("0x9b1aa69fe9fca10aa41250bba054aabd92aba5b6"), BigInteger.valueOf(116L)).block()!!
         assertEquals(props.name, "↜Warrior↝ 6/20")
@@ -100,7 +99,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun yInsure() {
         val props = testing.getProperties(Address.apply("0x181aea6936b407514ebfc0754a37704eb8d98f91"), "48".toBigInteger()).block()!!
         assertEquals("Balancer | 1 ETH \uD83D\uDD12 | 11/11/2020", props.name)
@@ -109,7 +107,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun hegic() {
         val props = testing.getProperties(Address.apply("0xcb9ebae59738d9dadc423adbde66c018777455a4"), "317".toBigInteger()).block()!!
         assertTrue(props.name.endsWith("call option for 1 ETH. Expires on 20.11.2020 07h44 UTC"))
@@ -118,7 +115,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun hashmasks() {
         val props = testing.getProperties(Address.apply("0xc2c747e0f7004f9e8817db2ca4997657a7746928"), "9076".toBigInteger()).block()!!
         assertEquals("https://hashmasksstore.blob.core.windows.net/hashmasks/2833.jpg", props.image)
@@ -126,7 +122,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun waifusion() {
         val props = testing.getProperties(Address.apply("0x2216d47494e516d8206b70fca8585820ed3c4946"), "1".toBigInteger()).block()!!
         assertEquals("Kurisu Makise", props.name)
@@ -144,7 +139,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun goldenStellaWithAnimationUrl() {
         val props = testing.getProperties(Address.apply("0xdb7e971d39367b20bcf4df5ae2da0fa4261bf0e8"), 426.toBigInteger()).block()!!
         assertEquals(props.name, "Golden Stella [Batch 1]")
@@ -152,7 +146,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun russianMountainWithAnimationUrl() {
         val props = testing.getProperties(Address.apply("0xfbeef911dc5821886e1dda71586d90ed28174b7d"), 200026.toBigInteger()).block()!!
         assertEquals(props.name, "A Russian Mountain")
@@ -160,7 +153,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun russianMountainWithAnimationUrlViaContract() {
         val itemProperties = propertiesCacheDescriptor.get("0xfbeef911dc5821886e1dda71586d90ed28174b7d:200026").block()!!
         assertEquals(
@@ -170,14 +162,12 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun keepCalm() {
         val props = testing.getProperties(Address.apply("0x1866c6907e70eb176109363492b95e3617b4a195"), 27.toBigInteger()).block()!!
         assertEquals(props.name, "Freaky Hallway")
     }
 
     @Test
-    @Disabled
     fun cryptoPunks() {
         val props = testing.getProperties(Address.apply("0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB"), 33.toBigInteger()).block()!!
         assertEquals("CryptoPunk #33", props.name)
@@ -186,21 +176,18 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun standardImagePreview() {
         val props = testing.getProperties(Address.apply("0xe414a94e31f5f31e640a26d2822e8fef3328b667"), 536.toBigInteger()).block()!!
         assertNotNull(props.imagePreview)
     }
 
     @Test
-    @Disabled
     fun immutable() {
         val props = testing.getProperties(Address.apply("0x0e3a2a1f2146d86a604adc220b4967a898d7fe07"), 187594956.toBigInteger()).block()!!
         assertEquals(props.image, "https://api.immutable.com/asset/0x0e3a2a1f2146d86a604adc220b4967a898d7fe07/187594956")
     }
 
     @Test
-    @Disabled
     fun standard() {
         val props = testing.getProperties(Address.apply("0xdceaf1652a131F32a821468Dc03A92df0edd86Ea"), 10400663.toBigInteger()).block()!!
         assertEquals(props.name, "MCH Extension: #10400663 Lv.60")
@@ -213,7 +200,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun standard2() {
         val props = testing.getProperties(Address.apply("0x6ebeaf8e8e946f0716e6533a6f2cefc83f60e8ab"), 142708.toBigInteger()).block()!!
         assertEquals(props.name, "Simple Satyr")
@@ -223,7 +209,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun rarible() {
         val props = testing.getProperties(Address.apply("0xf79ab01289f85b970bf33f0543e41409ed2e1c1f"), 16.toBigInteger()).block()!!
         assertTrue(props.description!!.contains("Hi!"))
@@ -233,7 +218,6 @@ class ItemPropertiesServiceTest {
     }
 
     @Test
-    @Disabled
     fun ens() {
         val props = testing.getProperties(Address.apply("0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85"), "36071955485891595785443576745556172890537718974602336938223322572747980843639".toBigInteger()).block()!!
         assertTrue(props.name.contains(".eth"))
