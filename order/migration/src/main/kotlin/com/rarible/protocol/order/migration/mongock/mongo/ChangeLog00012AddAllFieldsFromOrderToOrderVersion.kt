84d24f017a288ddd1d4f286def462a82e68b09d6
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-09-30 07:21:24 +0300

    Simplify migration.

--- order/migration/src/main/kotlin/com/rarible/protocol/order/migration/mongock/mongo/ChangeLog00012AddAllFieldsFromOrderToOrderVersion.kt
+++ order/migration/src/main/kotlin/com/rarible/protocol/order/migration/mongock/mongo/ChangeLog00012AddAllFieldsFromOrderToOrderVersion.kt
@@ -2,17 +2,13 @@ package com.rarible.protocol.order.migration.mongock.mongo
 
 import com.github.cloudyrock.mongock.ChangeLog
 import com.github.cloudyrock.mongock.ChangeSet
-import com.rarible.core.common.optimisticLock
-import com.rarible.protocol.order.core.model.OrderVersion
 import com.rarible.protocol.order.core.repository.order.MongoOrderRepository
 import com.rarible.protocol.order.core.repository.order.OrderVersionRepository
 import io.changock.migration.api.annotations.NonLockGuarded
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.reactive.awaitFirst
-import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
-import org.springframework.dao.OptimisticLockingFailureException
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 
 // RPN-1135: add pre-migration of OrderVersions.
@@ -31,30 +27,22 @@ class ChangeLog00012AddAllFieldsFromOrderToOrderVersion {
         var counter = 0L
 
         orderRepository.findAll().collect { order ->
-            fun OrderVersion.addOrderFields() = copy(
-                type = order.type,
-                salt = order.salt,
-                start = order.start,
-                end = order.end,
-                data = order.data,
-                signature = order.signature,
-                platform = order.platform
-            )
 
             try {
                 orderVersionRepository.findAllByHash(order.hash).collect { version ->
-                    try {
-                        orderVersionRepository.save(version.addOrderFields()).awaitFirst()
-                    } catch (_: OptimisticLockingFailureException) {
-                        optimisticLock {
-                            orderVersionRepository.findById(version.id).awaitFirstOrNull()
-                                ?.let {
-                                    orderVersionRepository.save(it.addOrderFields()).awaitFirst()
-                                }
-                        }
-                    }
+                    orderVersionRepository.save(
+                        version.copy(
+                            type = order.type,
+                            salt = order.salt,
+                            start = version.start ?: order.start,
+                            end = version.end ?: order.end,
+                            data = order.data,
+                            signature = version.signature ?: order.signature,
+                            platform = order.platform
+                        )
+                    ).awaitFirst()
                 }
-                if (counter % 50000L == 0L) {
+                if (counter % 5000L == 0L) {
                     logger.info("Fixed $counter orders")
                 }
                 counter++
