ae2cd2407bc4f12f49143ccf1ce9935aca21a362
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-08 18:04:35 +0300

    Cleanup and reorganize code.

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/OrderUpdateConsumerEventHandler.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/OrderUpdateConsumerEventHandler.kt
@@ -4,10 +4,9 @@ import com.rarible.core.common.nowMillis
 import com.rarible.protocol.dto.*
 import com.rarible.protocol.order.core.event.NftOrdersPriceUpdateListener
 import com.rarible.protocol.order.core.misc.MAX_SIZE
-import com.rarible.protocol.order.core.model.*
+import com.rarible.protocol.order.core.model.ItemId
+import com.rarible.protocol.order.core.model.OrderKind
 import com.rarible.protocol.order.core.service.OrderRepositoryService
-import com.rarible.protocol.order.listener.misc.makeNftItemId
-import com.rarible.protocol.order.listener.misc.takeNftItemId
 import com.rarible.protocol.order.listener.service.order.OrderPriceUpdateService
 import kotlinx.coroutines.flow.collect
 import org.springframework.stereotype.Component
@@ -45,6 +44,33 @@ class OrderUpdateConsumerEventHandler(
             orders.forEach { orderPriceUpdateService.updateOrderPrice(it.hash, at) }
             nftOrdersPriceUpdateListener.onNftOrders(itemId, kind, orders)
         }
+    }
+
+    private val OrderDto.makeNftItemId: ItemId?
+        get() = getNftItemIdFromOrder(this) { make.assetType }
+
+    private val OrderDto.takeNftItemId: ItemId?
+        get() = getNftItemIdFromOrder(this) { take.assetType }
+
+    private fun getNftItemIdFromOrder(
+        order: OrderDto,
+        assetType: (order: OrderDto) -> AssetTypeDto
+    ): ItemId? {
+        return when (order) {
+            is LegacyOrderDto -> getNftItemIdFromAssetType(assetType(order))
+            is RaribleV2OrderDto -> getNftItemIdFromAssetType(assetType(order))
+            is OpenSeaV1OrderDto -> getNftItemIdFromAssetType(assetType(order))
+            is CryptoPunkOrderDto -> getNftItemIdFromAssetType(assetType(order))
+        }
+    }
+
+    private fun getNftItemIdFromAssetType(assetType: AssetTypeDto): ItemId? = when (assetType) {
+        is CryptoPunksAssetTypeDto -> ItemId(assetType.contract, assetType.punkId.toBigInteger())
+        is Erc1155AssetTypeDto -> ItemId(assetType.contract, assetType.tokenId)
+        is Erc1155LazyAssetTypeDto -> ItemId(assetType.contract, assetType.tokenId)
+        is Erc721AssetTypeDto -> ItemId(assetType.contract, assetType.tokenId)
+        is Erc721LazyAssetTypeDto -> ItemId(assetType.contract, assetType.tokenId)
+        is Erc20AssetTypeDto, is EthAssetTypeDto, is GenerativeArtAssetTypeDto -> null
     }
 
     private fun ItemId.toOrderFilter(kind: OrderKind): OrderFilterDto {
