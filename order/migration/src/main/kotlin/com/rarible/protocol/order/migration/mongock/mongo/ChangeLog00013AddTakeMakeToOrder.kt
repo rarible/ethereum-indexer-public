399f0261723481765954c7eefae36730d9280e88
Roman Novikov <mystdeim@gmail.com>
2021-10-12 09:31:31 +0300

    RPN-1168: fix according to review

--- order/migration/src/main/kotlin/com/rarible/protocol/order/migration/mongock/mongo/ChangeLog00013AddTakeMakeToOrder.kt
+++ order/migration/src/main/kotlin/com/rarible/protocol/order/migration/mongock/mongo/ChangeLog00013AddTakeMakeToOrder.kt
@@ -28,15 +28,15 @@ class ChangeLog00013AddTakeMakeToOrder {
         val logger = LoggerFactory.getLogger(javaClass)
 
         var counter = 0L
-        var query = Query().with(Sort.by(Sort.Direction.DESC, Order::lastUpdateAt.name))
+        val query = Query().with(Sort.by(Sort.Direction.DESC, Order::lastUpdateAt.name))
         template.find<Order>(query, MongoOrderRepository.COLLECTION).asFlow().collect { order ->
             try {
                 if (order.make.type.nft) {
-                    val updated = order.copy(takePrice = BigDecimal.valueOf(order.take.value.value.toLong()))
+                    val updated = order.copy()
                     template.save(updated, MongoOrderRepository.COLLECTION).awaitFirstOrNull()
                     counter++
                 } else if (order.take.type.nft) {
-                    val updated = order.copy(makePrice = BigDecimal.valueOf(order.make.value.value.toLong()))
+                    val updated = order.copy()
                     template.save(updated, MongoOrderRepository.COLLECTION).awaitFirstOrNull()
                     counter++
                 }
@@ -50,7 +50,7 @@ class ChangeLog00013AddTakeMakeToOrder {
         logger.info("--- Prices were set for $counter orders ")
     }
 
-    @ChangeSet(id = "ChangeLog00013AddTakeMakeToOrder.orderVersions", order = "1", author = "protocol")
+    @ChangeSet(id = "ChangeLog00013AddTakeMakeToOrder.orderVersions", order = "2", author = "protocol")
     fun orderVersions(
         @NonLockGuarded template: ReactiveMongoTemplate
     ) = runBlocking {
@@ -58,14 +58,14 @@ class ChangeLog00013AddTakeMakeToOrder {
 
         var counter = 0L
         var query = Query().with(Sort.by(Sort.Direction.DESC, OrderVersion::createdAt.name))
-        template.find<Order>(query, OrderVersionRepository.COLLECTION).asFlow().collect { orderVersion ->
+        template.find<OrderVersion>(query).asFlow().collect { orderVersion ->
             try {
                 if (orderVersion.make.type.nft) {
-                    val updated = orderVersion.copy(takePrice = BigDecimal.valueOf(orderVersion.take.value.value.toLong()))
+                    val updated = orderVersion.copy()
                     template.save(updated, MongoOrderRepository.COLLECTION).awaitFirstOrNull()
                     counter++
                 } else if (orderVersion.take.type.nft) {
-                    val updated = orderVersion.copy(makePrice = BigDecimal.valueOf(orderVersion.make.value.value.toLong()))
+                    val updated = orderVersion.copy()
                     template.save(updated, MongoOrderRepository.COLLECTION).awaitFirstOrNull()
                     counter++
                 }
