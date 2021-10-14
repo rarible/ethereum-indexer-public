f7ea6c6525fabef3f485aaf8ec6cf85a5c4ae654
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-15 00:11:32 +0300

    Simplify code in OrderUpdateConsumerEventHandler.

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/OrderUpdateConsumerEventHandler.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/OrderUpdateConsumerEventHandler.kt
@@ -26,72 +26,52 @@ class OrderUpdateConsumerEventHandler(
                 val order = event.order
                 orderPriceUpdateService.updateOrderVersionPrice(order.hash, at)
 
-                val makeNftItemId = order.makeNftItemId
-                if (makeNftItemId != null) {
-                    updateItemOrders(makeNftItemId, OrderKind.SELL, at)
+                val makeItemId = order.make.assetType.getItemId()
+                if (makeItemId != null) {
+                    updateItemOrders(makeItemId, OrderKind.SELL, at)
                 }
 
-                val takeNftItemId = order.takeNftItemId
-                if (takeNftItemId != null) {
-                    updateItemOrders(takeNftItemId, OrderKind.BID, at)
+                val takeItemId = order.take.assetType.getItemId()
+                if (takeItemId != null) {
+                    updateItemOrders(takeItemId, OrderKind.BID, at)
                 }
             }
         }
     }
 
     private suspend fun updateItemOrders(itemId: ItemId, kind: OrderKind, at: Instant) {
-        orderRepositoryService.search(itemId.toOrderFilter(kind), MAX_SIZE).collect { orders ->
-            orders.forEach { orderPriceUpdateService.updateOrderPrice(it.hash, at) }
-            nftOrdersPriceUpdateListener.onNftOrders(itemId, kind, orders)
-        }
-    }
-
-    private val OrderDto.makeNftItemId: ItemId?
-        get() = getNftItemIdFromOrder(this) { make.assetType }
-
-    private val OrderDto.takeNftItemId: ItemId?
-        get() = getNftItemIdFromOrder(this) { take.assetType }
-
-    private fun getNftItemIdFromOrder(
-        order: OrderDto,
-        assetType: (order: OrderDto) -> AssetTypeDto
-    ): ItemId? {
-        return when (order) {
-            is LegacyOrderDto -> getNftItemIdFromAssetType(assetType(order))
-            is RaribleV2OrderDto -> getNftItemIdFromAssetType(assetType(order))
-            is OpenSeaV1OrderDto -> getNftItemIdFromAssetType(assetType(order))
-            is CryptoPunkOrderDto -> getNftItemIdFromAssetType(assetType(order))
-        }
-    }
-
-    private fun getNftItemIdFromAssetType(assetType: AssetTypeDto): ItemId? = when (assetType) {
-        is CryptoPunksAssetTypeDto -> ItemId(assetType.contract, assetType.punkId.toBigInteger())
-        is Erc1155AssetTypeDto -> ItemId(assetType.contract, assetType.tokenId)
-        is Erc1155LazyAssetTypeDto -> ItemId(assetType.contract, assetType.tokenId)
-        is Erc721AssetTypeDto -> ItemId(assetType.contract, assetType.tokenId)
-        is Erc721LazyAssetTypeDto -> ItemId(assetType.contract, assetType.tokenId)
-        is Erc20AssetTypeDto, is EthAssetTypeDto, is GenerativeArtAssetTypeDto -> null
-    }
-
-    private fun ItemId.toOrderFilter(kind: OrderKind): OrderFilterDto {
-        return when (kind) {
+        val orderFilter = when (kind) {
             OrderKind.SELL -> OrderFilterSellByItemDto(
-                tokenId = tokenId,
-                contract = contract,
+                tokenId = itemId.tokenId,
+                contract = itemId.contract,
                 sort = OrderFilterDto.Sort.LAST_UPDATE_DESC,
                 platform = null,
                 maker = null,
                 origin = null
             )
             OrderKind.BID -> OrderFilterBidByItemDto(
-                tokenId = tokenId,
-                contract = contract,
+                tokenId = itemId.tokenId,
+                contract = itemId.contract,
                 sort = OrderFilterDto.Sort.LAST_UPDATE_DESC,
                 platform = null,
                 maker = null,
                 origin = null
             )
         }
+        orderRepositoryService.search(orderFilter, MAX_SIZE).collect { orders ->
+            orders.forEach { orderPriceUpdateService.updateOrderPrice(it.hash, at) }
+            nftOrdersPriceUpdateListener.onNftOrders(itemId, kind, orders)
+        }
     }
+
+    private fun AssetTypeDto.getItemId(): ItemId? = when (this) {
+        is CryptoPunksAssetTypeDto -> ItemId(contract, punkId.toBigInteger())
+        is Erc1155AssetTypeDto -> ItemId(contract, tokenId)
+        is Erc1155LazyAssetTypeDto -> ItemId(contract, tokenId)
+        is Erc721AssetTypeDto -> ItemId(contract, tokenId)
+        is Erc721LazyAssetTypeDto -> ItemId(contract, tokenId)
+        is Erc20AssetTypeDto, is EthAssetTypeDto, is GenerativeArtAssetTypeDto -> null
+    }
+
 }
 
