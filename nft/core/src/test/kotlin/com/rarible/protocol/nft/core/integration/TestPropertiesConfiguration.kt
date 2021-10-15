b4ee053357bef0b2c0430714c761bae5b8a8ece4
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-15 12:57:50 +0300

    Define meta extender worker bean only in the 'nft-indexer-listener' module.

--- nft/core/src/test/kotlin/com/rarible/protocol/nft/core/integration/TestPropertiesConfiguration.kt
+++ nft/core/src/test/kotlin/com/rarible/protocol/nft/core/integration/TestPropertiesConfiguration.kt
@@ -1,11 +1,16 @@
 package com.rarible.protocol.nft.core.integration
 
 import com.rarible.core.application.ApplicationEnvironmentInfo
+import com.rarible.core.daemon.sequential.ConsumerWorker
+import com.rarible.protocol.dto.NftItemEventDto
+import com.rarible.protocol.nft.core.configuration.NftIndexerProperties
 import com.rarible.protocol.nft.core.model.ReduceSkipTokens
+import com.rarible.protocol.nft.core.service.item.meta.InternalItemHandler
 import io.daonomic.rpc.mono.WebClientTransport
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import scalether.core.MonoEthereum
@@ -40,4 +45,37 @@ class TestPropertiesConfiguration {
 
     @Bean
     fun meterRegistry(): MeterRegistry = SimpleMeterRegistry()
+
+    @Bean
+    fun itemMetaExtenderWorker() {
+
+    }
+
+    /**
+     * This bean is needed to make possible publishing of item with extended meta.
+     * In production this bean is defined in the 'nft-indexer-listener' module.
+     */
+    @Bean
+    fun itemMetaExtenderWorker(
+        applicationEnvironmentInfo: ApplicationEnvironmentInfo,
+        internalItemHandler: InternalItemHandler,
+        nftIndexerProperties: NftIndexerProperties,
+        meterRegistry: MeterRegistry
+    ): ConsumerWorker<NftItemEventDto> {
+        return ConsumerWorker(
+            consumer = InternalItemHandler.createInternalItemConsumer(
+                applicationEnvironmentInfo,
+                nftIndexerProperties.blockchain,
+                nftIndexerProperties.kafkaReplicaSet
+            ),
+            properties = nftIndexerProperties.daemonWorkerProperties,
+            eventHandler = internalItemHandler,
+            meterRegistry = meterRegistry,
+            workerName = "nftItemMetaExtender"
+        )
+    }
+
+    @Bean
+    fun itemMetaExtenderWorkerStarter(itemMetaExtenderWorker: ConsumerWorker<NftItemEventDto>): CommandLineRunner =
+        CommandLineRunner { itemMetaExtenderWorker.start() }
 }
