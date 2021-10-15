24216f69ce26517ecb32b70a3ca6a1d52c8fba7c
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-15 12:57:50 +0300

    Define meta extender worker bean only in the 'nft-indexer-listener' module.

--- nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/configuration/NftListenerConfiguration.kt
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/configuration/NftListenerConfiguration.kt
@@ -1,15 +1,23 @@
 package com.rarible.protocol.nft.listener.configuration
 
 import com.github.cloudyrock.spring.v5.EnableMongock
+import com.rarible.core.application.ApplicationEnvironmentInfo
+import com.rarible.core.daemon.sequential.ConsumerWorker
 import com.rarible.ethereum.converters.EnableScaletherMongoConversions
 import com.rarible.ethereum.listener.log.EnableLogListeners
 import com.rarible.ethereum.listener.log.persist.BlockRepository
 import com.rarible.ethereum.monitoring.BlockchainMonitoringWorker
+import com.rarible.protocol.dto.NftItemEventDto
 import com.rarible.protocol.nft.core.configuration.NftIndexerProperties
+import com.rarible.protocol.nft.core.configuration.ProducerConfiguration
 import com.rarible.protocol.nft.core.model.ItemId
 import com.rarible.protocol.nft.core.model.ReduceSkipTokens
+import com.rarible.protocol.nft.core.producer.BatchedConsumerWorker
+import com.rarible.protocol.nft.core.service.item.meta.InternalItemHandler
 import com.rarible.protocol.nft.listener.NftListenerApplication
 import io.micrometer.core.instrument.MeterRegistry
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -23,8 +31,11 @@ class NftListenerConfiguration(
     private val nftIndexerProperties: NftIndexerProperties,
     private val nftListenerProperties: NftListenerProperties,
     private val meterRegistry: MeterRegistry,
-    private val blockRepository: BlockRepository
+    private val blockRepository: BlockRepository,
+    private val applicationEnvironmentInfo: ApplicationEnvironmentInfo
 ) {
+    private val logger = LoggerFactory.getLogger(ProducerConfiguration::class.java)
+
     @Bean
     fun reduceSkipTokens(): ReduceSkipTokens {
         return ReduceSkipTokens(nftListenerProperties.skipReduceTokens.map { ItemId.parseId(it) })
@@ -39,4 +50,27 @@ class NftListenerConfiguration(
             blockRepository = blockRepository
         )
     }
+
+    @Bean
+    fun itemMetaExtenderWorker(internalItemHandler: InternalItemHandler): BatchedConsumerWorker<NftItemEventDto> {
+        logger.info("Creating batch of ${nftIndexerProperties.nftItemMetaExtenderWorkersCount} item meta extender workers")
+        val workers = (1..nftIndexerProperties.nftItemMetaExtenderWorkersCount).map {
+            ConsumerWorker(
+                consumer = InternalItemHandler.createInternalItemConsumer(
+                    applicationEnvironmentInfo,
+                    nftIndexerProperties.blockchain,
+                    nftIndexerProperties.kafkaReplicaSet
+                ),
+                properties = nftIndexerProperties.daemonWorkerProperties,
+                eventHandler = internalItemHandler,
+                meterRegistry = meterRegistry,
+                workerName = "nftItemMetaExtender.$it"
+            )
+        }
+        return BatchedConsumerWorker(workers)
+    }
+
+    @Bean
+    fun itemMetaExtenderWorkerStarter(itemMetaExtenderWorker: BatchedConsumerWorker<NftItemEventDto>): CommandLineRunner =
+        CommandLineRunner { itemMetaExtenderWorker.start() }
 }
