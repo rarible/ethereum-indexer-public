41c09f0bde25e142a465407556e498eac423fdf3
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-19 11:35:55 +0300

    Pull request #112: RPN-1212: API for order currencies

--- order/core/src/main/kotlin/com/rarible/protocol/order/core/repository/order/MongoOrderRepository.kt
+++ order/core/src/main/kotlin/com/rarible/protocol/order/core/repository/order/MongoOrderRepository.kt
@@ -87,6 +87,31 @@ class MongoOrderRepository(
         return template.query<Order>().matching(query).all().asFlow()
     }
 
+    override fun findTakeTypesOfSellOrders(token: Address, tokenId: EthUInt256): Flow<AssetType> {
+        val criteria = (Order::status isEqualTo OrderStatus.ACTIVE)
+            .and(Order::make / Asset::type / NftAssetType::token).isEqualTo(token)
+            .and(Order::make / Asset::type / NftAssetType::tokenId).isEqualTo(tokenId)
+        return template.findDistinct<AssetType>(
+            Query(criteria),
+            "${Order::take.name}.${Asset::type.name}",
+            Order::class.java,
+            AssetType::class.java
+        ).asFlow()
+    }
+
+    override fun findMakeTypesOfBidOrders(token: Address, tokenId: EthUInt256): Flow<AssetType> {
+        @Suppress("DuplicatedCode")
+        val criteria = (Order::status isEqualTo OrderStatus.ACTIVE)
+            .and(Order::take / Asset::type / NftAssetType::token).isEqualTo(token)
+            .and(Order::take / Asset::type / NftAssetType::tokenId).isEqualTo(tokenId)
+        return template.findDistinct<AssetType>(
+            Query(criteria),
+            "${Order::make.name}.${Asset::type.name}",
+            Order::class.java,
+            AssetType::class.java
+        ).asFlow()
+    }
+
     override fun findByTargetBalanceAndNotCanceled(maker: Address, token: Address): Flow<Order> {
         val criteria = Criteria.where("${Order::make.name}.${Asset::type.name}.${AssetType::nft.name}").isEqualTo(false)
             .and(Order::maker.name).isEqualTo(maker)
