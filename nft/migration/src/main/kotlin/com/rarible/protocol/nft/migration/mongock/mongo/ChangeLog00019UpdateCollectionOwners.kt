0f08ba8c6cda7fdc41cd4ce563232b62ed05a789
Sergey Patrikeev <s.patrikeev@rarible.com>
2021-10-29 10:50:36 +0300

    Fix migration on ownership recalculation.

--- nft/migration/src/main/kotlin/com/rarible/protocol/nft/migration/mongock/mongo/ChangeLog00019UpdateCollectionOwners.kt
+++ nft/migration/src/main/kotlin/com/rarible/protocol/nft/migration/mongock/mongo/ChangeLog00019UpdateCollectionOwners.kt
@@ -2,12 +2,12 @@ package com.rarible.protocol.nft.migration.mongock.mongo
 
 import com.github.cloudyrock.mongock.ChangeLog
 import com.github.cloudyrock.mongock.ChangeSet
-import com.rarible.core.common.retryOptimisticLock
+import com.rarible.core.common.optimisticLock
 import com.rarible.protocol.nft.core.model.CollectionOwnershipTransferred
 import com.rarible.protocol.nft.core.repository.TokenRepository
 import com.rarible.protocol.nft.core.repository.history.NftHistoryRepository
 import io.changock.migration.api.annotations.NonLockGuarded
-import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.reactive.collect
 import kotlinx.coroutines.runBlocking
 import org.slf4j.Logger
@@ -30,19 +30,17 @@ class ChangeLog00019UpdateCollectionOwners {
             error("Disable this migration by setting runAlways = false!")
         }
         logger.info("Started updating collection owners")
+        var seen = 0
         nftHistoryRepository.findAllByCollection(null).collect { logEvent ->
             val ownershipTransfer = logEvent.data as? CollectionOwnershipTransferred ?: return@collect
             try {
-                tokenRepository.findById(ownershipTransfer.id)
-                    .map {
-                        if (it.owner != ownershipTransfer.newOwner) {
-                            logger.info("Updating owner of ${ownershipTransfer.id} from ${it.owner} to ${ownershipTransfer.newOwner}")
-                        }
-                        it.copy(owner = ownershipTransfer.newOwner)
+                val token = tokenRepository.findById(ownershipTransfer.id).awaitFirstOrNull()
+                if (token != null && token.owner != ownershipTransfer.newOwner) {
+                    logger.info("Updating owner of ${ownershipTransfer.id} from ${token.owner} to ${ownershipTransfer.newOwner} (seen $seen)")
+                    optimisticLock {
+                        tokenRepository.save(token.copy(owner = ownershipTransfer.newOwner)).awaitFirstOrNull()
                     }
-                    .flatMap { tokenRepository.save(it) }
-                    .retryOptimisticLock()
-                    .awaitFirst()
+                }
             } catch (e: Exception) {
                 logger.error("Failed to update owner of collection ${ownershipTransfer.id}", e)
             }
