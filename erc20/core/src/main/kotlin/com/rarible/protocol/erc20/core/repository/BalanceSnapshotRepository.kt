3276cd135379b7f9ab70ba58956730d3ee4a4a8f
sergey.anufriev <s.anufriev@rarible.com>
2021-11-12 13:16:18 +0700

    RPN-1322 Fix tests in erc20

--- erc20/core/src/main/kotlin/com/rarible/protocol/erc20/core/repository/BalanceSnapshotRepository.kt
+++ erc20/core/src/main/kotlin/com/rarible/protocol/erc20/core/repository/BalanceSnapshotRepository.kt
@@ -1,6 +1,5 @@
 package com.rarible.protocol.erc20.core.repository
 
-import com.rarible.core.apm.CaptureSpan
 import com.rarible.core.reduce.repository.SnapshotRepository
 import com.rarible.protocol.erc20.core.model.BalanceId
 import com.rarible.protocol.erc20.core.model.BalanceReduceSnapshot
@@ -12,7 +11,6 @@ import org.springframework.data.mongodb.core.findById
 import org.springframework.stereotype.Component
 
 @Component
-@CaptureSpan(type = "db", subtype = "balance-snapshot")
 class BalanceSnapshotRepository(
     private val template: ReactiveMongoTemplate
 ) : SnapshotRepository<BalanceReduceSnapshot, Erc20Balance, Long, BalanceId> {
