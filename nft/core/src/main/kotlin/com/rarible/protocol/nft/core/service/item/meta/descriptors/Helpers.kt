37adad0acf5b418d6168c9825663eea48dedf4cf
sergey.anufriev <s.anufriev@rarible.com>
2021-11-15 12:50:48 +0700

    Add simple user agent generator for api requests

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/meta/descriptors/Helpers.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/meta/descriptors/Helpers.kt
@@ -2,6 +2,7 @@ package com.rarible.protocol.nft.core.service.item.meta
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.rarible.protocol.nft.core.model.ItemAttribute
+import io.netty.util.internal.ThreadLocalRandom
 import scalether.domain.Address
 import java.math.BigInteger
 import java.time.Instant
@@ -76,5 +77,28 @@ enum class TraitType(
     companion object {
         fun fromNode(node: JsonNode) = values().asSequence()
             .sortedBy { it.ordinal }.find { it.predicate(node) } ?: DEFAULT
+    }
+}
+
+object UserAgentGenerator {
+    private const val version1 = "#version1"
+    private const val version2 = "#version2"
+    private const val version3 = "#version3"
+
+    private val agents = listOf(
+        "Mozilla/$version1 (Macintosh; U; PPC Mac OS X; fr-fr) AppleWebKit/$version2 (KHTML, like Gecko) Safari/$version3",
+        "Opera/$version1 (X11; Linux x86_64; U; de) Presto/$version2 Version/$version3"
+    )
+
+    fun generateUserAgent(): String {
+        val template = agents.random()
+        return template
+            .replace(version1, randomVersion())
+            .replace(version2, randomVersion())
+            .replace(version3, randomVersion())
+    }
+
+    private fun randomVersion(): String {
+        return "${ThreadLocalRandom.current().nextInt(1, 30)}.${ThreadLocalRandom.current().nextInt(0, 100)}.${ThreadLocalRandom.current().nextInt(0, 200)}"
     }
 }
