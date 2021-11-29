07229c6818d179d6071b3df55546f31162a9fe8e
sergey.anufriev <s.anufriev@rarible.com>
2021-11-29 15:22:13 +0700

    Rollback some code

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/ItemReduceService.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/ItemReduceService.kt
@@ -205,7 +205,8 @@ class ItemReduceService(
                         }
                     }
                     is ItemRoyalty -> {
-                        item.copy(royalties = event.royalties)
+                        logger.info("Ignoring ItemRoyalty event: $event")
+                        item
                     }
                     is ItemLazyMint -> {
                         item.copy(royalties = event.royalties, creators = event.creators, creatorsFinal = true)
