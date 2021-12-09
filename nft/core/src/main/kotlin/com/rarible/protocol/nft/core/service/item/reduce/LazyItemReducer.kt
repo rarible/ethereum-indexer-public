07d9d3eb60cb7a9d58ae094cf9f0f23a83d33500
sergey.anufriev <s.anufriev@rarible.com>
2021-12-09 12:21:00 +0700

    RPN-1442 More tests for LazyItemReducer

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/reduce/LazyItemReducer.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/reduce/LazyItemReducer.kt
@@ -1,11 +1,11 @@
 package com.rarible.protocol.nft.core.service.item.reduce
 
+import com.rarible.core.entity.reducer.exception.ReduceException
 import com.rarible.core.entity.reducer.service.Reducer
 import com.rarible.ethereum.domain.EthUInt256
 import com.rarible.protocol.nft.core.model.Item
 import com.rarible.protocol.nft.core.model.ItemEvent
 import org.springframework.stereotype.Component
-import java.lang.IllegalStateException
 
 @Component
 class LazyItemReducer : Reducer<ItemEvent, Item> {
@@ -17,7 +17,7 @@ class LazyItemReducer : Reducer<ItemEvent, Item> {
                 is ItemEvent.LazyItemMintEvent -> entity.lazySupply + event.supply
                 is ItemEvent.LazyItemBurnEvent -> EthUInt256.ZERO
                 is ItemEvent.ItemBurnEvent, is ItemEvent.ItemMintEvent -> {
-                    throw IllegalStateException("This events can't be in this reducer")
+                    throw ReduceException("This events can't be in this reducer")
                 }
             }
             entity.copy(
