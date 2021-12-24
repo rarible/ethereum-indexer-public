f104dfd51d3641ff83d7e00dc7555b741d3ad5e2
sergey.anufriev <s.anufriev@rarible.com>
2021-12-24 21:02:51 +0700

    Fix group name

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/model/EntityEventListeners.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/model/EntityEventListeners.kt
@@ -3,5 +3,5 @@ package com.rarible.protocol.nft.core.model
 object EntityEventListeners {
     const val ITEM_HISTORY_LISTENER = "item.history.listener"
     const val OWNERSHIP_HISTORY_LISTENER = "ownership.history.listener"
-    const val TOKEN_HISTORY_LISTENER = "token,history.listener"
+    const val TOKEN_HISTORY_LISTENER = "token.history.listener"
 }
