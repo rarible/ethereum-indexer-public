f774bdc42416ebfc0c1560e1707c49ba67bcd93b
sanufriev <s.anufriev@rarible.com>
2022-01-18 16:48:16 +0700

    Change listener id for new scanners

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/model/EntityEventListeners.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/model/EntityEventListeners.kt
@@ -1,7 +1,17 @@
 package com.rarible.protocol.nft.core.model
 
 object EntityEventListeners {
-    const val ITEM_HISTORY_LISTENER = "item.history.listener"
-    const val OWNERSHIP_HISTORY_LISTENER = "ownership.history.listener"
-    const val TOKEN_HISTORY_LISTENER = "token.history.listener"
+    fun itemHistoryListenerId(env: String, blockchain: String): String {
+        return "${prefix(env, blockchain)}.item.history.listener"
+    }
+
+    fun ownershipHistoryListenerId(env: String, blockchain: String): String {
+        return "${prefix(env, blockchain)}.ownership.history.listener"
+    }
+
+    fun tokenHistoryListenerId(env: String, blockchain: String): String {
+        return "${prefix(env, blockchain)}.token.history.listener"
+    }
+
+    private fun prefix(env: String, blockchain: String): String = "$env.protocol.$blockchain.nft"
 }
--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/reduce/ItemEventReduceService.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/reduce/ItemEventReduceService.kt
@@ -2,7 +2,9 @@ package com.rarible.protocol.nft.core.service.item.reduce
 
 import com.rarible.blockchain.scanner.ethereum.model.ReversedEthereumLogRecord
 import com.rarible.blockchain.scanner.framework.data.LogRecordEvent
+import com.rarible.core.application.ApplicationEnvironmentInfo
 import com.rarible.core.entity.reducer.service.EventReduceService
+import com.rarible.protocol.nft.core.configuration.NftIndexerProperties
 import com.rarible.protocol.nft.core.converters.model.ItemEventConverter
 import com.rarible.protocol.nft.core.model.*
 import com.rarible.protocol.nft.core.service.EntityEventListener
@@ -14,11 +16,13 @@ class ItemEventReduceService(
     entityIdService: ItemIdService,
     templateProvider: ItemTemplateProvider,
     reducer: ItemReducer,
-    private val onNftItemLogEventListener: OnNftItemLogEventListener
+    private val onNftItemLogEventListener: OnNftItemLogEventListener,
+    properties: NftIndexerProperties,
+    environmentInfo: ApplicationEnvironmentInfo
 ) : EntityEventListener {
     private val delegate = EventReduceService(entityService, entityIdService, templateProvider, reducer)
 
-    override val id: String = EntityEventListeners.ITEM_HISTORY_LISTENER
+    override val id: String = EntityEventListeners.itemHistoryListenerId(environmentInfo.name, properties.blockchain.name)
 
     override val groupId: SubscriberGroup = SubscriberGroups.ITEM_HISTORY
 
