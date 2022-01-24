fbb575f356b08b7e55fc2c125e7519acc734747f
Eugene Nacu <evgeny.nacu@gmail.com>
2022-01-24 16:51:39 +0300

    chore: less APM spans

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/repository/TokenRepository.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/repository/TokenRepository.kt
@@ -1,27 +1,19 @@
 package com.rarible.protocol.nft.core.repository
 
-import com.rarible.core.apm.CaptureSpan
-import com.rarible.core.apm.SpanType
 import com.rarible.protocol.nft.core.model.Token
 import com.rarible.protocol.nft.core.model.TokenFilter
 import com.rarible.protocol.nft.core.model.TokenStandard
-import org.springframework.data.mongodb.core.ReactiveMongoOperations
-import org.springframework.data.mongodb.core.count
-import org.springframework.data.mongodb.core.find
-import org.springframework.data.mongodb.core.findAll
-import org.springframework.data.mongodb.core.findById
+import org.springframework.data.mongodb.core.*
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.and
 import org.springframework.data.mongodb.core.query.isEqualTo
-import org.springframework.data.mongodb.core.remove
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import scalether.domain.Address
 
 @Component
-@CaptureSpan(type = SpanType.DB)
 class TokenRepository(
     private val mongo: ReactiveMongoOperations
 ) {
@@ -44,11 +36,6 @@ class TokenRepository(
 
     fun findById(id: Address): Mono<Token> {
         return mongo.findById(id)
-    }
-
-    fun findByStandard(standard: TokenStandard): Flux<Token> {
-        val criteria = Criteria.where(Token::standard.name).isEqualTo(standard)
-        return mongo.find(Query(criteria))
     }
 
     fun search(filter: TokenFilter): Flux<Token> {
