7f1bc511588f183bdc8a4853d9f2f7893ed5f849
sanufriev <s.anufriev@rarible.com>
2022-01-29 00:51:25 +0700

    RPN-1652 Fix build

--- nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/ItemAndOwnershipsCheckTaskHandler.kt
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/ItemAndOwnershipsCheckTaskHandler.kt
@@ -1,30 +1,19 @@
 package com.rarible.protocol.nft.listener.service.item
 
-import com.rarible.core.task.RunTask
 import com.rarible.core.task.TaskHandler
 import com.rarible.core.telemetry.metrics.RegisteredCounter
-import com.rarible.protocol.nft.listener.configuration.NftListenerProperties
 import kotlinx.coroutines.flow.Flow
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class ItemAndOwnershipsCheckTaskHandler(
-    private val nftListenerProperties: NftListenerProperties,
     private val itemDataQualityService: ItemDataQualityService,
     private val itemDataQualityJobRunRegisteredCounter: RegisteredCounter
 ) : TaskHandler<String> {
 
     override val type: String
         get() = ITEM_AND_OWNERSHIPS_CHECK
-
-    override suspend fun isAbleToRun(param: String): Boolean {
-        return nftListenerProperties.enableCheckDataQualityJob
-    }
-
-    override fun getAutorunParams(): List<RunTask> {
-        return listOf(RunTask("", null))
-    }
 
     override fun runLongTask(from: String?, param: String): Flow<String> {
         logger.info("Start item/ownership data quality check from $from")
@@ -37,4 +26,3 @@ class ItemAndOwnershipsCheckTaskHandler(
         private val logger = LoggerFactory.getLogger(ItemAndOwnershipsCheckTaskHandler::class.java)
     }
 }
-
