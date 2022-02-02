c396a069b23b110b5a3397609fca8f6fbe682088
sanufriev <s.anufriev@rarible.com>
2022-02-02 17:33:59 +0700

    Add some logs

--- order/core/src/main/kotlin/com/rarible/protocol/order/core/configuration/OrderIndexerPropertiesConfiguration.kt
+++ order/core/src/main/kotlin/com/rarible/protocol/order/core/configuration/OrderIndexerPropertiesConfiguration.kt
@@ -3,6 +3,7 @@ package com.rarible.protocol.order.core.configuration
 import com.rarible.ethereum.domain.EthUInt256
 import com.rarible.ethereum.sign.domain.EIP712Domain
 import com.rarible.protocol.order.core.provider.ProtocolCommissionProvider
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -24,6 +25,8 @@ class OrderIndexerPropertiesConfiguration(
 
     @Bean
     fun eip712Domain(): EIP712Domain {
+        logger.info("Order-Api was started with chainId=${indexerProperties.chainId}")
+
         return with(indexerProperties) {
             EIP712Domain(
                 eip712DomainName,
@@ -37,5 +40,9 @@ class OrderIndexerPropertiesConfiguration(
     @Bean
     fun protocolCommissionProvider(): ProtocolCommissionProvider {
        return ProtocolCommissionProvider(EthUInt256.of(indexerProperties.protocolCommission.toLong()))
+    }
+
+    private companion object {
+        val logger = LoggerFactory.getLogger(OrderIndexerPropertiesConfiguration::class.java)
     }
 }
