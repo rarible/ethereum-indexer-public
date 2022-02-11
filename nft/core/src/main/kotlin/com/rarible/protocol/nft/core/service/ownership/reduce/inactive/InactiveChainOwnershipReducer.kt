01903f364613e8b07497f75e21251c2945dfc1b3
sanufriev <s.anufriev@rarible.com>
2022-02-11 21:45:28 +0700

    fix: RPN-1753 Fix pending logs on ownership was not deleting

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/ownership/reduce/inactive/InactiveChainOwnershipReducer.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/ownership/reduce/inactive/InactiveChainOwnershipReducer.kt
@@ -3,13 +3,13 @@ package com.rarible.protocol.nft.core.service.ownership.reduce.inactive
 import com.rarible.protocol.nft.core.model.Ownership
 import com.rarible.protocol.nft.core.model.OwnershipEvent
 import com.rarible.protocol.nft.core.model.OwnershipId
-import com.rarible.protocol.nft.core.service.EntityChainReducer
+import com.rarible.protocol.nft.core.service.RevertedEntityChainReducer
 import com.rarible.protocol.nft.core.service.ownership.OwnershipInactiveEventApplyPolicy
 import org.springframework.stereotype.Component
 
 @Component
 class InactiveChainOwnershipReducer(
     ownershipInactiveEventApplyPolicy: OwnershipInactiveEventApplyPolicy,
-) : EntityChainReducer<OwnershipId, OwnershipEvent, Ownership>(
+) : RevertedEntityChainReducer<OwnershipId, OwnershipEvent, Ownership>(
     ownershipInactiveEventApplyPolicy
 )
