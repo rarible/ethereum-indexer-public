7995d09dd39991dd52c5ebc8d60d8b1d7e10f8db
Sergey Ratushnyak <rtserg@yandex.ru>
2022-02-28 10:41:51 +0300

    ALPHA-239 Metrics/Alert for invalid parse of OpenSea orders

--- order/core/src/main/kotlin/com/rarible/protocol/order/core/configuration/OrderIndexerProperties.kt
+++ order/core/src/main/kotlin/com/rarible/protocol/order/core/configuration/OrderIndexerProperties.kt
@@ -22,6 +22,7 @@ data class OrderIndexerProperties(
     val chainId: Int,
     var operatorPrivateKey: Binary,
     val protocolCommission: Int,
+    val metricRootPath: String,
     @NestedConfigurationProperty
     val exchangeContractAddresses: ExchangeContractAddresses,
     @NestedConfigurationProperty
--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/configuration/MetricsCountersConfiguration.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/configuration/MetricsCountersConfiguration.kt
@@ -1,7 +1,7 @@
 package com.rarible.protocol.order.listener.configuration
 
 import com.rarible.core.telemetry.metrics.RegisteredCounter
-import com.rarible.ethereum.domain.Blockchain
+import com.rarible.protocol.order.core.configuration.OrderIndexerProperties
 import com.rarible.protocol.order.listener.misc.OpenSeaConverterErrorMetric
 import io.micrometer.core.instrument.MeterRegistry
 import org.springframework.context.annotation.Bean
@@ -9,11 +9,11 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class MetricsCountersConfiguration(
-    //private val properties: OrderIndexerProperties,
+    private val properties: OrderIndexerProperties,
     private val meterRegistry: MeterRegistry
 ) {
     @Bean
     fun openSeaConverterErrorRegisteredCounter() : RegisteredCounter {
-        return OpenSeaConverterErrorMetric("qwerty", Blockchain.ETHEREUM).bind(meterRegistry)
+        return OpenSeaConverterErrorMetric(properties.metricRootPath, properties.blockchain).bind(meterRegistry)
     }
 }
