a4fd3000796d77c350fc2fbf49b066f3ec784266
sanufriev <s.anufriev@rarible.com>
2022-03-01 11:53:00 +0700

    feat: ALPHA-268 Ignore old OpenSea order on balance/ownership events

--- order/listener/src/test/kotlin/com/rarible/protocol/order/listener/service/order/OrderBalanceServiceTest.kt
+++ order/listener/src/test/kotlin/com/rarible/protocol/order/listener/service/order/OrderBalanceServiceTest.kt
@@ -1,23 +1,27 @@
 package com.rarible.protocol.order.listener.service.order
 
 import com.rarible.core.common.nowMillis
+import com.rarible.core.test.data.randomAddress
 import com.rarible.core.test.data.randomString
 import com.rarible.core.test.wait.Wait
 import com.rarible.ethereum.domain.EthUInt256
 import com.rarible.protocol.dto.Erc20BalanceDto
 import com.rarible.protocol.dto.Erc20BalanceUpdateEventDto
 import com.rarible.protocol.dto.NftOwnershipDto
+import com.rarible.protocol.order.core.data.createOrderOpenSeaV1DataV1
 import com.rarible.protocol.order.core.model.Asset
 import com.rarible.protocol.order.core.model.Erc1155AssetType
 import com.rarible.protocol.order.core.model.Erc20AssetType
 import com.rarible.protocol.order.core.model.Erc721AssetType
 import com.rarible.protocol.order.core.model.EthAssetType
 import com.rarible.protocol.order.core.model.MakeBalanceState
+import com.rarible.protocol.order.core.model.OrderType
 import com.rarible.protocol.order.core.model.OrderVersion
 import com.rarible.protocol.order.listener.data.createNftOwnershipDeleteEvent
 import com.rarible.protocol.order.listener.data.createNftOwnershipDeleteEventLegacy
 import com.rarible.protocol.order.listener.data.createNftOwnershipDto
 import com.rarible.protocol.order.listener.data.createNftOwnershipUpdateEvent
+import com.rarible.protocol.order.listener.data.createOrder
 import com.rarible.protocol.order.listener.data.createOrderVersion
 import com.rarible.protocol.order.listener.integration.AbstractIntegrationTest
 import com.rarible.protocol.order.listener.integration.IntegrationTest
@@ -97,6 +101,59 @@ class OrderBalanceServiceTest : AbstractIntegrationTest() {
     }
 
     @Test
+    fun `should not update legacy OpenSea orders`() = runBlocking<Unit> {
+        val legacyOpenSea = randomAddress()
+        orderIndexerProperties.exchangeContractAddresses.openSeaV1 = legacyOpenSea
+
+        val targetMaker = AddressFactory.create()
+        val targetToken = AddressFactory.create()
+
+        val make = Asset(Erc20AssetType(targetToken), EthUInt256.TEN)
+        val take = Asset(Erc1155AssetType(AddressFactory.create(), EthUInt256.TEN), EthUInt256.TEN)
+
+        val oldStock = EthUInt256.of(2)
+        val newStock = EthUInt256.of(5)
+
+        clearMocks(assetBalanceProvider)
+        coEvery { assetBalanceProvider.getAssetStock(any(), any()) } returns MakeBalanceState(oldStock)
+
+        val order1 = createOrderVersion().copy(
+            maker = targetMaker,
+            make = make,
+            take = take
+        )
+        val order2 = createOrder().copy(
+            maker = targetMaker,
+            make = make,
+            take = take,
+            type = OrderType.OPEN_SEA_V1,
+            data = createOrderOpenSeaV1DataV1().copy(exchange = legacyOpenSea),
+            makeStock = oldStock
+        )
+        orderUpdateService.save(order1)
+        orderRepository.save(order2)
+
+        val updatedBalance = Erc20BalanceDto(
+            owner = targetMaker,
+            contract = targetToken,
+            balance = newStock.value
+        )
+        val event = Erc20BalanceUpdateEventDto(
+            eventId = randomString(),
+            balanceId = randomString(),
+            balance = updatedBalance
+        )
+
+        // Background job might update makeStock before this event is handled => try until the event solely changes the makeStock.
+        Wait.waitAssert {
+            orderBalanceService.handle(event)
+
+            assertThat(orderRepository.findById(order1.hash)?.makeStock).isEqualTo(newStock)
+            assertThat(orderRepository.findById(order2.hash)?.makeStock).isEqualTo(oldStock)
+        }
+    }
+
+    @Test
     fun `should update all not canceled nft orders`() = runBlocking<Unit> {
         val ownership = createNftOwnershipDto()
             .copy(tokenId = BigInteger.valueOf(2))
@@ -147,6 +204,53 @@ class OrderBalanceServiceTest : AbstractIntegrationTest() {
             assertThat(orderRepository.findById(order2.hash)?.makeStock?.value).isEqualTo(ownership.value)
             assertThat(orderRepository.findById(order3.hash)?.makeStock).isEqualTo(EthUInt256.ZERO) // because order #3 is a cancelled order.
             assertThat(orderRepository.findById(order4.hash)?.makeStock).isEqualTo(oldStock)
+        }
+    }
+
+    @Test
+    fun `should not update legacy OpenSea sell orders`() = runBlocking<Unit> {
+        val legacyOpenSea = randomAddress()
+        orderIndexerProperties.exchangeContractAddresses.openSeaV1 = legacyOpenSea
+
+        val ownership = createNftOwnershipDto()
+            .copy(tokenId = BigInteger.valueOf(2))
+            .copy(value = BigInteger.valueOf(5))
+
+        val targetToken = ownership.contract
+        val targetTokenId = EthUInt256.of(ownership.tokenId)
+
+        val make = Asset(Erc1155AssetType(targetToken, targetTokenId), EthUInt256.TEN)
+        val take = Asset(Erc20AssetType(AddressFactory.create()), EthUInt256.TEN)
+        val maker = ownership.owner
+
+        val oldStock = EthUInt256.ONE
+
+        clearMocks(assetBalanceProvider)
+        coEvery { assetBalanceProvider.getAssetStock(any(), any()) } returns MakeBalanceState(oldStock)
+
+        val order1 = createOrderVersion().copy(
+            maker = maker,
+            make = make,
+            take = take
+        )
+        val order2 = createOrder().copy(
+            maker = maker,
+            make = make,
+            take = take,
+            type = OrderType.OPEN_SEA_V1,
+            data = createOrderOpenSeaV1DataV1().copy(exchange = legacyOpenSea),
+            makeStock = oldStock
+        )
+        orderUpdateService.save(order1)
+        orderRepository.save(order2)
+
+        val event = createNftOwnershipUpdateEvent(ownership)
+
+        Wait.waitAssert {
+            orderBalanceService.handle(event)
+
+            assertThat(orderRepository.findById(order1.hash)?.makeStock?.value).isEqualTo(ownership.value)
+            assertThat(orderRepository.findById(order2.hash)?.makeStock?.value).isEqualTo(oldStock.value)
         }
     }
 
