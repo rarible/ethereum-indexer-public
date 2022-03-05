db33486008965c338d33f9936c244b6de205b1c9
Sergey Patrikeev <s.patrikeev@rarible.com>
2022-03-05 13:33:54 +0100

    fix: meta: ensure that on lazy minting we send the ItemUpdateEvent with meta.

--- nft/api/src/main/kotlin/com/rarible/protocol/nft/api/controller/LazyMintController.kt
+++ nft/api/src/main/kotlin/com/rarible/protocol/nft/api/controller/LazyMintController.kt
@@ -3,24 +3,18 @@ package com.rarible.protocol.nft.api.controller
 import com.rarible.core.common.convert
 import com.rarible.protocol.dto.LazyNftDto
 import com.rarible.protocol.dto.NftItemDto
-import com.rarible.protocol.nft.api.configuration.NftIndexerApiProperties
 import com.rarible.protocol.nft.api.service.mint.LazyNftValidator
 import com.rarible.protocol.nft.api.service.mint.MintService
-import com.rarible.protocol.nft.core.model.ExtendedItem
 import com.rarible.protocol.nft.core.model.ItemLazyMint
-import com.rarible.protocol.nft.core.service.item.meta.ItemMetaService
 import org.springframework.core.convert.ConversionService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
-import java.time.Duration
 
 @RestController
 class LazyMintController(
     private val mintService: MintService,
     private val lazyNftValidator: LazyNftValidator,
-    private val itemMetaService: ItemMetaService,
-    private val conversionService: ConversionService,
-    private val nftIndexerApiProperties: NftIndexerApiProperties
+    private val conversionService: ConversionService
 ) : NftLazyMintControllerApi {
 
     override suspend fun mintNftAsset(lazyNftDto: LazyNftDto): ResponseEntity<NftItemDto> {
@@ -28,13 +22,6 @@ class LazyMintController(
             .apply { lazyNftValidator.validate(this) }
             .let { conversionService.convert<ItemLazyMint>(it) }
             .let { mintService.createLazyNft(it) }
-            .let { item ->
-                val itemMeta = itemMetaService.getAvailableMetaOrLoadSynchronouslyWithTimeout(
-                    itemId = item.id,
-                    timeout = Duration.ofMillis(nftIndexerApiProperties.metaSyncLoadingTimeout)
-                )
-                ExtendedItem(item = item, itemMeta = itemMeta)
-            }
             .let { conversionService.convert<NftItemDto>(it) }
         return ResponseEntity.ok(result)
     }
