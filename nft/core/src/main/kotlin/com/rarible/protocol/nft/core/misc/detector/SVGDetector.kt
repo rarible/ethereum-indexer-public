e4a1482f371a777a6e5f0bd5760268ec651dc5f6
Artem Yakischik <a.yakischik@rarible.com>
2022-03-19 10:46:52 +0400

    SVG encoded # fix

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/misc/detector/SVGDetector.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/misc/detector/SVGDetector.kt
@@ -16,18 +16,20 @@ class SVGDetector(url: String) : ContentDetector(url) {
     }
 
     override fun getData(): String {
-        return if (url.startsWith(svgTag)) {
+        val content = if (url.startsWith(svgTag)) {
             url
         } else {
-            URLDecoder.decode(url.substring(url.indexOf(svgTag), url.length), "UTF-8")
+            URLDecoder.decode(url.substring(prefixIndex, url.length), "UTF-8")
         }
+        // Not sure why, but in some SVGs there are # chars encoded as %23
+        return content.replace("%23", "#")
     }
 
     override fun getMimeType(): String {
         return mimeTypePrefix
     }
 
-    override fun getDecodedData(): ByteArray? {
+    override fun getDecodedData(): ByteArray {
         return getData().toByteArray()
     }
 }
