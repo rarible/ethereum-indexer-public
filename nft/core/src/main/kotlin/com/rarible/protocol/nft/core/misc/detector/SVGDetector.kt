56b5a13ecbdc2abbadc15f3f73580043f9788c47
Artem Yakischik <a.yakischik@rarible.com>
2022-03-19 10:56:27 +0400

    SVG encoded # fix

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/misc/detector/SVGDetector.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/misc/detector/SVGDetector.kt
@@ -1,14 +1,18 @@
 package com.rarible.protocol.nft.core.misc.detector
 
-import java.net.URLDecoder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class SVGDetector(url: String) : ContentDetector(url) {
 
     private val prefixIndex = url.indexOf(svgTag)
 
     companion object {
-        private const val svgTag = "<svg"
-        private const val mimeTypePrefix = "image/svg+xml"
+
+        private val logger: Logger = LoggerFactory.getLogger(SVGDetector::class.java)
+        const val svgTag = "<svg"
+        const val mimeTypePrefix = "image/svg+xml"
+        const val spaceCode = "%20"
     }
 
     override fun canDecode(): Boolean {
@@ -16,20 +20,26 @@ class SVGDetector(url: String) : ContentDetector(url) {
     }
 
     override fun getData(): String {
-        val content = if (url.startsWith(svgTag)) {
-            url
+        return if (spaceCode in url) {
+            logger.warn("Broken svg: %s", url)
+            var decodedData = url
+                .replace(spaceCode, " ")
+                .replace(
+                    "fill:%23",
+                    "fill:#"
+                ) //TODO Workaround for BRAVO-1872.
+            decodedData.substring(decodedData.indexOf(svgTag), decodedData.length)
+
         } else {
-            URLDecoder.decode(url.substring(prefixIndex, url.length), "UTF-8")
+            url
         }
-        // Not sure why, but in some SVGs there are # chars encoded as %23
-        return content.replace("%23", "#")
     }
 
     override fun getMimeType(): String {
         return mimeTypePrefix
     }
 
-    override fun getDecodedData(): ByteArray {
+    override fun getDecodedData(): ByteArray? {
         return getData().toByteArray()
     }
 }
