a30e843aca1da0fe14db722c5902e7fa7a760013
Sergey Ratushnyak <rtserg@yandex.ru>
2022-03-29 17:29:07 +0300

    feat: ALPHA-321 autocorrect inconsistency

--- nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/ItemDataQualityService.kt
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/ItemDataQualityService.kt
@@ -12,9 +12,12 @@ import com.rarible.protocol.nft.listener.configuration.NftListenerProperties
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.slf4j.LoggerFactory
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.stereotype.Component
+import scalether.domain.Address
 
 @Component
 class ItemDataQualityService(
@@ -22,14 +25,16 @@ class ItemDataQualityService(
     private val ownershipRepository: OwnershipRepository,
     private val itemDataQualityErrorRegisteredCounter: RegisteredCounter,
     private val nftListenerProperties: NftListenerProperties,
-    private val itemReduceService: ItemReduceService
+    private val itemReduceService: ItemReduceService,
+    private val mongo: ReactiveMongoOperations
 ) {
-    fun checkItems(from: String?): Flow<String> {
+    fun checkItems(from: String?, dropCollection: Boolean = true): Flow<String> {
         val filter = ItemFilterAll(
             sort = ItemFilter.Sort.LAST_UPDATE_DESC,
             showDeleted = false
         )
         return flow {
+            initializeCollection(dropCollection)
             var continuation = from
             do {
                 val items = itemRepository.search(
@@ -46,9 +51,12 @@ class ItemDataQualityService(
                                 "ownershipsValue=$ownershipsValue."
                         logger.info("$message Try to fix.")
                         itemReduceService.update(item.token, item.tokenId).awaitFirstOrNull()
-                        if(!checkItem(item)) {
+
+                        if(!checkItem(item,fromRepository = true, writeToBd = true)) {
                             itemDataQualityErrorRegisteredCounter.increment()
                             logger.warn("$message Can't be fixed.")
+                        } else {
+                            logger.info("Item ${item.id}, lastUpdateAt ${item.date}: supply=${item.supply} was fixed")
                         }
                     }
                     emit(ItemContinuation(item.date, item.id).toString())
@@ -58,16 +66,29 @@ class ItemDataQualityService(
         }
     }
 
-    suspend fun checkItem(item: Item, showLog: Boolean = false):Boolean {
-        val ownershipsValue = getOwnershipsValue(item.id, nftListenerProperties.elementsFetchJobSize)
-
-        if(showLog) {
-            println("Ownership: $ownershipsValue")
-            println("Supply: ${item.supply}")
+    private suspend fun initializeCollection(dropInconsistentCollection: Boolean) {
+        if (dropInconsistentCollection) {
+            mongo.dropCollection(COLLECTION).awaitFirstOrNull()
         }
+        if(!mongo.collectionExists(COLLECTION).awaitFirst()) {
+            mongo.createCollection(COLLECTION).awaitFirst()
+        }
+    }
 
-        return ownershipsValue == item.supply
-
+    suspend fun checkItem(item: Item, fromRepository: Boolean = false, writeToBd: Boolean = false): Boolean {
+        var updatedItem = item
+        if (fromRepository) {
+            val repositoryItem = itemRepository.findById(ItemId(item.token, item.tokenId)).awaitFirstOrNull()
+            repositoryItem ?: return false
+            updatedItem = repositoryItem
+        }
+        val ownershipsValue = getOwnershipsValue(updatedItem.id, nftListenerProperties.elementsFetchJobSize)
+        val result = ownershipsValue == updatedItem.supply
+        if (!result && writeToBd) {
+            mongo.save(InconsistentItems(item.token, item.tokenId, item.supply, ownershipsValue), COLLECTION)
+                .awaitFirstOrNull()
+        }
+        return result
     }
 
     private suspend fun getOwnershipsValue(itemId: ItemId, limit: Int): EthUInt256 {
@@ -88,6 +109,16 @@ class ItemDataQualityService(
     }
 
     companion object {
+        const val COLLECTION = "inconsistent_items"
         private val logger = LoggerFactory.getLogger(ItemDataQualityService::class.java)
+
+        data class InconsistentItems(
+            val token: Address,
+            val tokenId: EthUInt256,
+            val supply: EthUInt256,
+            val supplyOwnership: EthUInt256
+        )
     }
+
 }
+
--- nft/listener/src/test/kotlin/com/rarible/protocol/nft/listener/service/item/ItemDataQualityServiceTest.kt
+++ nft/listener/src/test/kotlin/com/rarible/protocol/nft/listener/service/item/ItemDataQualityServiceTest.kt
@@ -12,6 +12,9 @@ import com.rarible.protocol.nft.core.model.Item
 import com.rarible.protocol.nft.core.model.ItemContinuation
 import com.rarible.protocol.nft.core.model.ItemTransfer
 import com.rarible.protocol.nft.core.model.Ownership
+import com.rarible.protocol.nft.core.model.OwnershipFilter
+import com.rarible.protocol.nft.core.model.OwnershipFilterByItem
+import com.rarible.protocol.nft.core.repository.ownership.OwnershipFilterCriteria.toCriteria
 import com.rarible.protocol.nft.core.service.item.ItemReduceService
 import com.rarible.protocol.nft.listener.configuration.NftListenerProperties
 import com.rarible.protocol.nft.listener.data.createRandomItem
@@ -24,15 +27,16 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import scalether.domain.Address
-import java.math.BigInteger
 import java.time.Duration
 
 
@@ -44,6 +48,7 @@ internal class ItemDataQualityServiceTest : AbstractIntegrationTest() {
     @BeforeEach
     fun setupDbIndexes() = runBlocking<Unit> {
         itemRepository.createIndexes()
+        nftItemHistoryRepository.createIndexes()
     }
 
     @Autowired
@@ -60,11 +65,11 @@ internal class ItemDataQualityServiceTest : AbstractIntegrationTest() {
             ownershipRepository = ownershipRepository,
             itemDataQualityErrorRegisteredCounter = counter,
             nftListenerProperties = NftListenerProperties().copy(elementsFetchJobSize = 2),
-            itemReduceService = itemReduceService
+            itemReduceService = itemReduceService,
+            mongo = mongo
         )
         val validItem1 = createRandomItem().copy(supply = EthUInt256.of(OWNERS_NUMBER), date = now - Duration.ofMinutes(5))
         val invalidItem1 = createRandomItem().copy(supply = EthUInt256.of(OWNERS_NUMBER), date = now - Duration.ofMinutes(4))
-        nftItemHistoryRepository.save(createValidLog(invalidItem1)).awaitFirst()
 
         listOf(validItem1, invalidItem1).forEach { itemRepository.save(it).awaitFirst() }
         listOf(
@@ -80,7 +85,6 @@ internal class ItemDataQualityServiceTest : AbstractIntegrationTest() {
 
         val validItem2 = createRandomItem().copy(supply = EthUInt256.of(OWNERS_NUMBER), date = now - Duration.ofMinutes(7))
         val invalidItem2 = createRandomItem().copy(supply = EthUInt256.of(OWNERS_NUMBER), date = now - Duration.ofMinutes(6))
-        nftItemHistoryRepository.save(createValidLog(invalidItem2)).awaitFirst()
 
         listOf(validItem2, invalidItem2).forEach { itemRepository.save(it).awaitFirst() }
         listOf(
@@ -88,25 +92,53 @@ internal class ItemDataQualityServiceTest : AbstractIntegrationTest() {
             createInvalidValidOwnerships(invalidItem2)
         ).flatten().forEach { ownershipRepository.save(it).awaitFirst() }
 
-        continuations = itemDataQualityService.checkItems(from = continuations.last()).toList()
+        continuations = itemDataQualityService.checkItems(from = continuations.last(), dropCollection = false).toList()
         assertThat(continuations).hasSize(2)
         assertThat(continuations[0].let { ItemContinuation.parse(it)?.afterId }).isEqualTo(invalidItem2.id)
         assertThat(continuations[1].let { ItemContinuation.parse(it)?.afterId }).isEqualTo(validItem2.id)
         verify(exactly = 2) { counter.increment() }
 
+        var invalidItemsCount =
+            mongo.getCollection(ItemDataQualityService.COLLECTION).awaitFirst().countDocuments().awaitFirstOrNull()
 
-        //itemDataQualityService.checkItems(null)
+        assertThat(invalidItemsCount).isEqualTo(2)
 
-        //assertThat(itemDataQualityService.checkItem(validItem1, true)).isTrue()
-        //assertThat(itemDataQualityService.checkItem(invalidItem2)).isTrue()
+        getOwnershipsByItem(invalidItem1).forEach {
+            nftItemHistoryRepository.save(createValidLog(invalidItem1, it)).awaitFirstOrNull()
+        }
+        getOwnershipsByItem(invalidItem2).forEach {
+            nftItemHistoryRepository.save(createValidLog(invalidItem2, it)).awaitFirstOrNull()
+        }
+
+        itemReduceService.update(invalidItem1.token, invalidItem1.tokenId).awaitFirstOrNull()
+        itemReduceService.update(invalidItem2.token, invalidItem2.tokenId).awaitFirstOrNull()
+
+        continuations = itemDataQualityService.checkItems(null).toList()
+        itemDataQualityService.checkItems(from = continuations.last()).collect()
+
+        assertThat(itemDataQualityService.checkItem(invalidItem1, fromRepository = true)).isTrue
+        assertThat(itemDataQualityService.checkItem(invalidItem2, fromRepository = true)).isTrue
+
+        invalidItemsCount =
+            mongo.getCollection(ItemDataQualityService.COLLECTION).awaitFirst().countDocuments().awaitFirstOrNull()
+        assertThat(invalidItemsCount).isEqualTo(0)
     }
 
-    private fun createValidLog(item: Item): LogEvent {
+    private suspend fun getOwnershipsByItem(item: Item): List<Ownership>{
+        val filter = OwnershipFilterByItem(
+            contract = item.token,
+            tokenId = item.tokenId.value,
+            sort = OwnershipFilter.Sort.LAST_UPDATE
+        )
+        return ownershipRepository.search(filter.toCriteria(null, limit = OWNERS_NUMBER))
+    }
+
+    private fun createValidLog(item: Item, ownership: Ownership): LogEvent {
         return createLog(
-            blockNumber = 1,
             token = item.token,
             tokenId = item.tokenId,
-            value = EthUInt256.of(10)//item.supply * EthUInt256.of(OWNERS_NUMBER)
+            value = EthUInt256.of(2),
+            owner = ownership.owner
         )
     }
 
@@ -135,10 +167,11 @@ internal class ItemDataQualityServiceTest : AbstractIntegrationTest() {
         token: Address = randomAddress(),
         blockNumber: Long = 1,
         tokenId: EthUInt256 = EthUInt256.of(randomBigInt()),
-        value: EthUInt256 = EthUInt256.ONE
+        value: EthUInt256 = EthUInt256.ONE,
+        owner: Address = randomAddress()
     ): LogEvent {
         val transfer = ItemTransfer(
-            owner = randomAddress(),
+            owner = owner,
             token = token,
             tokenId = tokenId,
             date = nowMillis(),
