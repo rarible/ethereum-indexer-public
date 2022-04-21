9dfed1a37774eb7d0b5061f742c503cbf023bf79
Artem Yakischik <a.yakischik@rarible.com>
2022-04-21 17:58:08 +0400

    feat: Support of JSON in tokenURI

---
+++ nft/core/src/test/kotlin/com/rarible/protocol/nft/core/service/item/meta/properties/JsonPropertiesParserTest.kt
@@ -0,0 +1,41 @@
+package com.rarible.protocol.nft.core.service.item.meta.properties
+
+import com.rarible.protocol.nft.core.data.createRandomItemId
+import com.rarible.protocol.nft.core.service.item.meta.descriptors.getText
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class JsonPropertiesParserTest {
+
+    @Test
+    fun `regular json`() {
+        val data = """{"a": "b"}"""
+        val node = JsonPropertiesParser.parse(createRandomItemId(), data)!!
+
+        assertThat(node.getText("a")).isEqualTo("b")
+    }
+
+    @Test
+    fun `regular json with trailing spaces`() {
+        val data = "\n\t\r " + """{"a": "b"}""" + "\n\t\r   "
+        val node = JsonPropertiesParser.parse(createRandomItemId(), data)!!
+
+        assertThat(node.getText("a")).isEqualTo("b")
+    }
+
+    @Test
+    fun `json with data type`() {
+        val data = """data:application/json;utf8, {"a": "b"} """
+        val node = JsonPropertiesParser.parse(createRandomItemId(), data)!!
+
+        assertThat(node.getText("a")).isEqualTo("b")
+    }
+
+    @Test
+    fun `base64 json`() {
+        val data = """data:application/json;base64,IHsiYSI6ICJiIn0g"""
+        val node = JsonPropertiesParser.parse(createRandomItemId(), data)!!
+
+        assertThat(node.getText("a")).isEqualTo("b")
+    }
+}
