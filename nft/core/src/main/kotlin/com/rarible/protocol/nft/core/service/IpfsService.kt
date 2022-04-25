52aaa63b6c077d44dcbb535f11beac2e56a6f489
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2022-04-25 15:38:40 +0400

    feat: Use original IPFS gateways of Item meta (#318)

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/IpfsService.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/IpfsService.kt
@@ -5,6 +5,7 @@ import com.rarible.protocol.nft.core.configuration.NftIndexerProperties
 import com.rarible.protocol.nft.core.service.item.meta.descriptors.IPFS_CAPTURE_SPAN_TYPE
 import com.rarible.protocol.nft.core.service.item.meta.descriptors.SVG_START
 import org.springframework.stereotype.Component
+import java.net.URL
 import java.util.*
 import java.util.regex.Pattern
 
@@ -20,22 +21,24 @@ class IpfsService(
     // Used only for internal operations, such urls should NOT be stored anywhere
     fun resolveInnerHttpUrl(uri: String): String {
         val randomInnerGateway = innerGateway[Random().nextInt(innerGateway.size)]
-        return resolveHttpUrl(uri, randomInnerGateway)
+        // For internal calls original IPFS host should be replaced in order to avoid rate limit of the original gateway
+        return resolveHttpUrl(uri, randomInnerGateway, true)
     }
 
     // Used to build url exposed to the DB cache or API responses
     fun resolvePublicHttpUrl(uri: String): String {
-        return resolveHttpUrl(uri, publicGateway)
+        // For public IPFS urls we want to keep original gateway URL (if possible)
+        return resolveHttpUrl(uri, publicGateway, false)
     }
 
-    private fun resolveHttpUrl(uri: String, gateway: String): String {
+    private fun resolveHttpUrl(uri: String, gateway: String, replaceOriginalHost: Boolean): String {
         // Embedded image, return 'as is'
         if (uri.startsWith(SVG_START)) {
             return uri
         }
 
         // Checking if foreign IPFS url contains /ipfs/ like http://ipfs.io/ipfs/lalala
-        checkForeignIpfsUri(uri, gateway)?.let { return it.encodeHtmlUrl() }
+        checkForeignIpfsUri(uri, gateway, replaceOriginalHost)?.let { return it.encodeHtmlUrl() }
 
         // Checking prefixed IPFS URI like ipfs://Qmlalala
         checkIpfsAbstractUrl(uri, gateway)?.let { return it.encodeHtmlUrl() }
@@ -47,10 +50,15 @@ class IpfsService(
         }.encodeHtmlUrl()
     }
 
-    private fun checkForeignIpfsUri(uri: String, gateway: String): String? {
+    private fun checkForeignIpfsUri(uri: String, gateway: String, replaceOriginalHost: Boolean): String? {
         val ipfsPathIndex = uri.lastIndexOf(IPFS_PATH_PART)
         if (ipfsPathIndex < 0) {
             return null
+        }
+
+        // If URL is valid, and we want to keep original IPFS gateway, return 'as is'
+        if (!replaceOriginalHost && isValidUrl(uri)) {
+            return uri
         }
 
         val pathEnd = uri.substring(ipfsPathIndex + IPFS_PATH_PART.length).removeLeadingSlashes()
@@ -99,6 +107,15 @@ class IpfsService(
             result = result.trimStart('/')
         }
         return result
+    }
+
+    private fun isValidUrl(uri: String): Boolean {
+        return try {
+            val url = URL(uri)
+            (url.protocol == "http" || url.protocol == "https")
+        } catch (e: Exception) {
+            false
+        }
     }
 
     fun isCid(test: String): Boolean {
