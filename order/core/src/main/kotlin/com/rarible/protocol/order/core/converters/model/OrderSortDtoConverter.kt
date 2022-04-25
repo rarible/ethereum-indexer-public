cfed441f63aff0c1380e635ad91ae01942061343
Sergey Ratushnyak <39464298+Rtserg@users.noreply.github.com>
2022-04-25 12:55:11 +0300

    Alpha 405 Add dbUpdatedAt to Ethereum for Orders (#305)
    ALPHA-405 Add dbUpdatedAt to Ethereum for Orders
    
    * Fix index
    * Use update field instead of save all order
    * Add integration test for sync controller
    * Default sorting is ascending
    * Added test
    * Small fixes

--- order/core/src/main/kotlin/com/rarible/protocol/order/core/converters/model/OrderSortDtoConverter.kt
+++ order/core/src/main/kotlin/com/rarible/protocol/order/core/converters/model/OrderSortDtoConverter.kt
@@ -1,7 +1,6 @@
 package com.rarible.protocol.order.core.converters.model
 
 import com.rarible.protocol.dto.OrderSortDto
-import com.rarible.protocol.order.core.model.order.OrderFilter
 import com.rarible.protocol.order.core.model.order.OrderFilterSort
 import org.springframework.core.convert.converter.Converter
 
---
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/order/OrderSetDbUpdatedFieldHandler.kt
@@ -0,0 +1,48 @@
+package com.rarible.protocol.order.listener.service.order
+
+import com.rarible.core.task.TaskHandler
+import com.rarible.protocol.order.core.model.Order
+import com.rarible.protocol.order.core.repository.order.OrderRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapNotNull
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class OrderSetDbUpdatedFieldHandler (
+    private val orderRepository: OrderRepository,
+) : TaskHandler<String> {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override val type: String
+        get() = ORDER_SET_DB_UPDATE_FIELD
+
+    override suspend fun isAbleToRun(param: String): Boolean {
+        return true
+    }
+
+    override fun runLongTask(from: String?, param: String): Flow<String> {
+        return orderRepository.findWithoutDbUpdatedField().mapNotNull {
+            updateOrder(it)
+        }
+    }
+
+    private suspend fun updateOrder(order: Order): String?  {
+        if(order.dbUpdatedAt != null ) {
+            logger.info("[$ORDER_SET_DB_UPDATE_FIELD] Shouldn't update this order $order!")
+            return null
+        }
+
+        orderRepository.orderDbUpdatedAtFieldUpdate(order)
+        val updatedOrder = orderRepository.findById(order.hash)
+
+        logger.info("[$ORDER_SET_DB_UPDATE_FIELD] Field Order::dbUpdatedAt of order:" +
+                " ${updatedOrder?.hash} has been updated and now is equal: ${updatedOrder?.dbUpdatedAt}.")
+
+        return updatedOrder.toString()
+    }
+
+    companion object {
+        const val ORDER_SET_DB_UPDATE_FIELD = "ORDER_SET_DB_UPDATE_FIELD"
+    }
+}
