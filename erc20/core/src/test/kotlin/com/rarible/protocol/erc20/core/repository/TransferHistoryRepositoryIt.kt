59f1ab203e4e18396a3d919a94d04a130f792c88
Artem Yakischik <a.yakischik@rarible.com>
2022-04-29 16:24:57 +0400

    fix for Balances by Token reduce

--- erc20/core/src/test/kotlin/com/rarible/protocol/erc20/core/repository/TransferHistoryRepositoryIt.kt
+++ erc20/core/src/test/kotlin/com/rarible/protocol/erc20/core/repository/TransferHistoryRepositoryIt.kt
@@ -1,86 +1,41 @@
 package com.rarible.protocol.erc20.core.repository
 
-import com.rarible.core.test.ext.MongoTest
-import com.rarible.ethereum.domain.EthUInt256
+import com.rarible.core.test.data.randomAddress
 import com.rarible.ethereum.listener.log.domain.LogEvent
-import com.rarible.ethereum.listener.log.domain.LogEventStatus
 import com.rarible.protocol.erc20.core.IntegrationTest
-import com.rarible.protocol.erc20.core.configuration.CoreConfiguration
-import com.rarible.protocol.erc20.core.model.*
-import com.rarible.protocol.erc20.core.repository.data.createAddress
-import io.daonomic.rpc.domain.Word
+import com.rarible.protocol.erc20.core.model.BalanceId
+import com.rarible.protocol.erc20.core.model.Erc20TokenHistory
+import com.rarible.protocol.erc20.core.repository.data.randomErc20Deposit
+import com.rarible.protocol.erc20.core.repository.data.randomErc20IncomeTransfer
+import com.rarible.protocol.erc20.core.repository.data.randomErc20OutcomeTransfer
+import com.rarible.protocol.erc20.core.repository.data.randomErc20Withdrawal
+import com.rarible.protocol.erc20.core.repository.data.randomLogEvent
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.runBlocking
-import org.apache.commons.lang3.RandomUtils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.query.Query
-import org.springframework.test.context.ContextConfiguration
 import scalether.domain.Address
-import java.util.*
 
-@MongoTest
-@DataMongoTest
-@ContextConfiguration(classes = [CoreConfiguration::class])
+@IntegrationTest
 class TransferHistoryRepositoryIt {
 
     @Autowired
     lateinit var historyRepository: Erc20TransferHistoryRepository
 
-    @Autowired
-    lateinit var mongo: ReactiveMongoTemplate
-
     @Test
-    fun `should save and get all types history`() = runBlocking {
-        val token = createAddress()
-        val owner = createAddress()
+    fun `find balance history - with block filter`() = runBlocking {
+        val token = randomAddress()
+        val owner = randomAddress()
 
-        val incomeTransfer = Erc20OutcomeTransfer(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
-        val outcomeTransfer = Erc20IncomeTransfer(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
-        val deposit = Erc20Deposit(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
-        val withdrawal = Erc20Withdrawal(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
+        val logEvent1 = randomLogEvent(randomErc20OutcomeTransfer(token, owner), blockNumber = 1)
+        val logEvent2 = randomLogEvent(randomErc20IncomeTransfer(token, owner), blockNumber = 2)
+        val logEvent3 = randomLogEvent(randomErc20Deposit(token, owner), blockNumber = 3)
+        val logEvent4 = randomLogEvent(randomErc20Withdrawal(token, owner), blockNumber = 4)
+        val logEvent5 = randomLogEvent(randomErc20Withdrawal(randomAddress(), owner), blockNumber = 4)
 
-        prepareStorage(token, incomeTransfer, outcomeTransfer, deposit, withdrawal)
-
-        val logs = historyRepository.findOwnerLogEvents(token, owner).collectList().awaitFirst()
-        assertThat(logs).hasSize(4)
-        assertEquals(logs[3].history, withdrawal)
-        assertEquals(logs[2].history, deposit)
-        assertEquals(logs[1].history, outcomeTransfer)
-        assertEquals(logs[0].history, incomeTransfer)
-    }
-
-    @Test
-    fun `should save and get all types history from target block`() = runBlocking {
-        val token = createAddress()
-        val owner = createAddress()
-
-        val logEvent1 = createLogEvent(
-            Erc20OutcomeTransfer(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
-        ).copy(blockNumber = 1)
-
-        val logEvent2 = createLogEvent(
-            Erc20IncomeTransfer(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
-        ).copy(blockNumber = 2)
-
-        val logEvent3 = createLogEvent(
-            Erc20Deposit(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
-        ).copy(blockNumber = 3)
-
-        val logEvent4 = createLogEvent(
-            Erc20Withdrawal(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
-        ).copy(blockNumber = 4)
-
-        val logEvent5 = createLogEvent(
-            Erc20Withdrawal(owner = owner, token = createAddress(), date = Date(), value = EthUInt256.of(1))
-        ).copy(blockNumber = 4)
-
-        prepareStorage(logEvent1, logEvent2, logEvent3, logEvent4, logEvent5)
+        saveAll(logEvent1, logEvent2, logEvent3, logEvent4, logEvent5)
 
         val logs = historyRepository.findBalanceLogEvents(BalanceId(token, owner), 1).collectList().awaitFirst()
 
@@ -91,23 +46,62 @@ class TransferHistoryRepositoryIt {
     }
 
     @Test
-    fun `should get transfers history by token and owner`() = runBlocking {
-        val token = createAddress()
-        val owner = createAddress()
+    fun `find balance history - without block filter`() = runBlocking {
+        val token = randomAddress()
+        val owner = randomAddress()
 
-        val incomeTransfer = Erc20OutcomeTransfer(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
-        val outcomeTransfer = Erc20IncomeTransfer(owner = owner, token = token, date = Date(), value = EthUInt256.of(1))
+        val logEvent1 = randomLogEvent(randomErc20OutcomeTransfer(token, owner), blockNumber = 1)
+        val logEvent2 = randomLogEvent(randomErc20IncomeTransfer(token, owner), blockNumber = 2)
+        val logEvent3 = randomLogEvent(randomErc20Withdrawal(randomAddress(), owner), blockNumber = 4)
+
+        saveAll(logEvent1, logEvent2, logEvent3)
+
+        val logs = historyRepository.findBalanceLogEvents(BalanceId(token, owner), null).collectList().awaitFirst()
+
+        assertThat(logs).hasSize(2)
+        assertEquals(logs[0].id, logEvent1.id)
+        assertEquals(logs[1].id, logEvent2.id)
+    }
+
+    @Test
+    fun `find balance events by owner - all types`() = runBlocking {
+        val token = randomAddress()
+        val owner = randomAddress()
+
+        val incomeTransfer = randomErc20IncomeTransfer(token, owner)
+        val outcomeTransfer = randomErc20OutcomeTransfer(token, owner)
+        val deposit = randomErc20Deposit(token, owner)
+        val withdrawal = randomErc20Withdrawal(token, owner)
+
+        saveAll(token, incomeTransfer, outcomeTransfer, deposit, withdrawal)
+
+        val logs = historyRepository.findOwnerLogEvents(token, owner).collectList().awaitFirst()
+        assertThat(logs).hasSize(4)
+        // All types of events should be returned
+        assertEquals(logs[3].history, withdrawal)
+        assertEquals(logs[2].history, deposit)
+        assertEquals(logs[1].history, outcomeTransfer)
+        assertEquals(logs[0].history, incomeTransfer)
+    }
+
+    @Test
+    fun `find balance events by owner - filtered by token & owner`() = runBlocking {
+        val token = randomAddress()
+        val owner = randomAddress()
+
+        val incomeTransfer = randomErc20IncomeTransfer(token, owner)
+        val outcomeTransfer = randomErc20OutcomeTransfer(token, owner)
 
         val transfers = listOf(
             incomeTransfer,
             outcomeTransfer,
-            Erc20IncomeTransfer(owner = createAddress(), token = token, date = Date(), value = EthUInt256.of(1)),
-            Erc20OutcomeTransfer(owner = owner, token = createAddress(), date = Date(), value = EthUInt256.of(1)),
-            Erc20IncomeTransfer(owner = createAddress(), token = createAddress(), date = Date(), value = EthUInt256.of(1)),
-            Erc20OutcomeTransfer(owner = createAddress(), token = createAddress(), date = Date(), value = EthUInt256.of(1))
+            randomErc20IncomeTransfer(token, randomAddress()),
+            randomErc20OutcomeTransfer(randomAddress(), owner),
+            randomErc20IncomeTransfer(randomAddress(), randomAddress()),
+            randomErc20OutcomeTransfer(randomAddress(), randomAddress())
         )
 
-        prepareStorage(token, *transfers.toTypedArray())
+        saveAll(token, *transfers.toTypedArray())
 
         val logs = historyRepository.findOwnerLogEvents(token, owner).collectList().awaitFirst()
         assertThat(logs).hasSize(2)
@@ -116,62 +110,75 @@ class TransferHistoryRepositoryIt {
     }
 
     @Test
-    fun `should get transfers history using from`() = runBlocking {
+    fun `find balance events by owner - filtered by from`() = runBlocking {
+        val transfer11 = randomErc20OutcomeTransfer(token = Address.ONE(), owner = Address.ONE())
+        val transfer12 = randomErc20OutcomeTransfer(token = Address.ONE(), owner = Address.TWO())
+        val transfer21 = randomErc20OutcomeTransfer(token = Address.TWO(), owner = Address.ONE())
 
-        val transfer11 = Erc20OutcomeTransfer(owner = Address.ONE(), token = Address.ONE(), date = Date(), value = EthUInt256.of(1))
-        val transfer12 = Erc20OutcomeTransfer(owner = Address.TWO(), token = Address.ONE(), date = Date(), value = EthUInt256.of(1))
-        val transfer21 = Erc20OutcomeTransfer(owner = Address.ONE(), token = Address.TWO(), date = Date(), value = EthUInt256.of(1))
+        saveAll(Address.ZERO(), transfer11, transfer12, transfer21)
 
-        prepareStorage(
-            Address.ZERO(),
-            transfer11,
-            transfer12,
-            transfer21
-        )
+        val logs1 = historyRepository.findOwnerLogEvents(
+            from = BalanceId(Address.ONE(), Address.ONE())
+        ).collectList().awaitFirst()
 
-        val logs1 = historyRepository.findOwnerLogEvents(from = Wallet(Address.ONE(), Address.ONE())).collectList().awaitFirst()
         assertThat(logs1).hasSize(2)
         assertEquals(logs1[0].history, transfer12)
         assertEquals(logs1[1].history, transfer21)
 
-        val logs2 = historyRepository.findOwnerLogEvents(token = Address.ONE(), from = Wallet(Address.ONE(), Address.ONE())).collectList().awaitFirst()
+        val logs2 = historyRepository.findOwnerLogEvents(
+            token = Address.ONE(),
+            from = BalanceId(Address.ONE(), Address.ONE())
+        ).collectList().awaitFirst()
+
         assertThat(logs2).hasSize(1)
         assertEquals(logs2[0].history, transfer12)
     }
 
-    private suspend fun prepareStorage(token: Address = createAddress(), vararg histories: Erc20TokenHistory) {
+    @Test
+    fun `find balance events by token - without owner`() = runBlocking<Unit> {
+        val token = randomAddress()
+
+        val event1 = randomErc20IncomeTransfer(token, Address.ONE())
+        val event2 = randomErc20OutcomeTransfer(randomAddress(), randomAddress())
+        val event3 = randomErc20Deposit(token, Address.ZERO())
+
+        saveAll(token, event1, event2, event3)
+
+        val result = historyRepository.findBalanceLogEventsForToken(token, null)
+            .collectList().awaitFirst()
+
+        // Should be sorted by owner
+        assertThat(result).hasSize(2)
+        assertThat(result[0].data).isEqualTo(event3)
+        assertThat(result[1].data).isEqualTo(event1)
+    }
+
+    @Test
+    fun `find balance events by token - with owner`() = runBlocking<Unit> {
+        val token = randomAddress()
+
+        val event1 = randomErc20IncomeTransfer(token, Address.ONE())
+        val event2 = randomErc20Deposit(token, Address.ZERO())
+
+        saveAll(token, event1, event2)
+
+        val result = historyRepository.findBalanceLogEventsForToken(token, Address.ZERO())
+            .collectList().awaitFirst()
+
+        assertThat(result).hasSize(1)
+        assertThat(result[0].data).isEqualTo(event1)
+    }
+
+    private suspend fun saveAll(token: Address = randomAddress(), vararg histories: Erc20TokenHistory) {
         histories.forEachIndexed { index, history ->
-            historyRepository.save(createLogEvent(history).copy(address = token, index = index)).awaitFirst()
+            historyRepository.save(randomLogEvent(history).copy(address = token, index = index)).awaitFirst()
         }
     }
 
-    private suspend fun prepareStorage(vararg logs: LogEvent) {
+    private suspend fun saveAll(vararg logs: LogEvent) {
         logs.forEach { log ->
             historyRepository.save(log).awaitFirst()
         }
     }
-
-    private fun createLogEvent(history: Erc20TokenHistory): LogEvent {
-        return LogEvent(
-            data = history,
-            address = history.token,
-            topic = word(),
-            transactionHash = word(),
-            status = LogEventStatus.CONFIRMED,
-            blockNumber = 1,
-            logIndex = 0,
-            minorLogIndex = 0,
-            index = 0
-        )
-    }
-
-    private fun word(): Word = Word(RandomUtils.nextBytes(32))
-
-    @BeforeEach
-    fun cleanDatabase() {
-        mongo.collectionNames
-            .filter { !it.startsWith("system") }
-            .flatMap { mongo.remove(Query(), it) }
-            .then().block()
-    }
 }
+
