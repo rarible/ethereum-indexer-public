a0c873e1bfc13e660a569ac0ba8c7fca0aab8e9e
Roman Novikov <mystdeim@gmail.com>
2022-05-13 14:49:21 +0300

    chore: handler drafts

---
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/admin/BlackListVerifiedTaskHandler.kt
@@ -0,0 +1,58 @@
+package com.rarible.protocol.nft.listener.admin
+
+import com.rarible.core.task.TaskHandler
+import com.rarible.ethereum.domain.EthUInt256
+import com.rarible.protocol.nft.core.repository.item.ItemRepository
+import com.rarible.protocol.nft.core.service.item.ReduceEventListenerListener
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.reactor.awaitSingle
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import scalether.domain.Address
+
+@Component
+class BlackListVerifiedTaskHandler(
+    private val itemRepository: ItemRepository,
+    private val reduceEventListenerListener: ReduceEventListenerListener
+) : TaskHandler<String> {
+
+    override val type: String
+        get() = NAME
+
+    override fun runLongTask(from: String?, paramStr: String): Flow<String> {
+        val params = parseParam(paramStr)
+        val fromTokenId = from?.let { EthUInt256.of(it) }
+        return itemRepository.findTokenItems(params.address, fromTokenId).map { item ->
+            val savedItem = when (params.feature) {
+                Feature.BLACKLIST -> itemRepository.save(item.copy(deleted = true)).awaitSingle()
+                Feature.VERIFIED -> item
+            }
+            reduceEventListenerListener.onItemChanged(savedItem)
+            item.id.token.prefixed()
+        }
+    }
+
+    companion object {
+        val NAME = "BLACKLIST_VERIFIED"
+        val logger: Logger = LoggerFactory.getLogger(BlackListVerifiedTaskHandler::class.java)
+        fun parseParam(param: String): Param {
+            val slices = param.split("_")
+            return Param(Address.apply(slices[0]), Feature.valueOf(slices[1]))
+        }
+    }
+
+    data class Param(
+        val address: Address,
+        val feature: Feature
+    ) {
+        override fun toString(): String {
+            return "${address}_${feature}"
+        }
+    }
+
+    enum class Feature {
+        BLACKLIST, VERIFIED
+    }
+}
---
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/job/CollectionBlackListVerifiedJob.kt
@@ -0,0 +1,62 @@
+package com.rarible.protocol.nft.listener.job
+
+import com.rarible.core.task.Task
+import com.rarible.core.task.TaskStatus
+import com.rarible.protocol.nft.core.repository.TempTaskRepository
+import com.rarible.protocol.nft.core.service.token.TokenEventListener
+import com.rarible.protocol.nft.core.service.token.TokenUpdateService
+import com.rarible.protocol.nft.listener.admin.BlackListVerifiedTaskHandler
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.scheduling.annotation.Scheduled
+
+class CollectionBlackListVerifiedJob(
+    @Value("\${listener.collectionBlackListVerified.enabled:false}")
+    private val enabled: Boolean,
+    private val taskRepository: TempTaskRepository,
+    private val tokenUpdateService: TokenUpdateService,
+    private val tokenListener: TokenEventListener
+) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Scheduled(
+        fixedDelayString = "\${listener.collectionBlackListVerifiedRefresh.rate:PT1H}",
+        initialDelayString = "PT1M"
+    )
+    fun execute() = runBlocking<Unit> {
+        logger.info("Starting CollectionBlackListVerifiedJob")
+        if (enabled) {
+
+            // TODO: get collections
+            val collections = emptyList<BlackListVerifiedTaskHandler.Param>()
+
+            collections.forEach { param ->
+                val existedTask =
+                    taskRepository.findByType(BlackListVerifiedTaskHandler.NAME, param.toString()).firstOrNull()
+                if (null == existedTask) {
+                    tokenUpdateService.getToken(param.address)?.let { token ->
+                        when (param.feature) {
+                            BlackListVerifiedTaskHandler.Feature.BLACKLIST -> {
+                                tokenUpdateService.removeToken(token.id)
+                            }
+                            BlackListVerifiedTaskHandler.Feature.VERIFIED -> {
+                                tokenListener.onTokenChanged(token)
+                            }
+                        }
+                    }
+                    val task = Task(
+                        type = BlackListVerifiedTaskHandler.NAME,
+                        param = param.toString(),
+                        state = TaskStatus.NONE,
+                        running = false,
+                        lastStatus = TaskStatus.NONE
+                    )
+                    taskRepository.save(task)
+                }
+            }
+        }
+    }
+}
