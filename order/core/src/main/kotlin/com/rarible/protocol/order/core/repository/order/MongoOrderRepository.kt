249045d1b066a6f273057b0aec264cdaf3cab79d
Sergey Anufriev <s.anufriev@rarible.com>
2022-05-23 12:59:01 +0700

    feat: ALPHA-460 Remove some depricated order controllers orders/sell/byMaker, orders/bids/byMaker (#326)

--- order/core/src/main/kotlin/com/rarible/protocol/order/core/repository/order/MongoOrderRepository.kt
+++ order/core/src/main/kotlin/com/rarible/protocol/order/core/repository/order/MongoOrderRepository.kt
@@ -64,6 +64,9 @@ class MongoOrderRepository(
             "platform_1_maker_1_data.val com.rarible.protocol.order.core.model.OrderOpenSeaV1DataV1.nonce: kotlin.Long?_1",
             OrderRepositoryIndexes.BIDS_BY_ITEM_PLATFORM_DEFINITION.indexName,
             OrderRepositoryIndexes.BIDS_BY_MAKER_PLATFORM_DEFINITION.indexName,
+            OrderRepositoryIndexes.BIDS_BY_MAKER_DEFINITION.indexName,
+            OrderRepositoryIndexes.SELL_ORDERS_BY_MAKER_PLATFORM_DEFINITION.indexName,
+            OrderRepositoryIndexes.BY_LAST_UPDATE_DEFINITION.indexName,
             OrderRepositoryIndexes.BY_LAST_UPDATE_AND_STATUS_AND_PLATFORM_AND_ID_DEFINITION.indexName
         )
     }
