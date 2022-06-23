f3098302e74a73cf563f19629fc0c9d4a6d451ca
sanufriev <s.anufriev@rarible.com>
2022-06-24 00:50:02 +0700

    feat: PT-524 Add main seaport load handler

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/opensea/OpenSeaOrderValidatorImp.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/opensea/OpenSeaOrderValidatorImp.kt
@@ -1,6 +1,7 @@
 package com.rarible.protocol.order.listener.service.opensea
 
 import com.rarible.core.telemetry.metrics.RegisteredCounter
+import com.rarible.ethereum.sign.domain.EIP712Domain
 import com.rarible.protocol.order.core.model.*
 import com.rarible.protocol.order.core.service.CallDataEncoder
 import com.rarible.protocol.order.core.service.CommonSigner
@@ -8,6 +9,7 @@ import com.rarible.protocol.order.core.service.OpenSeaSigner
 import io.daonomic.rpc.domain.Word
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import java.math.BigInteger
 
 @Component
 class OpenSeaOrderValidatorImp(
@@ -18,14 +20,42 @@ class OpenSeaOrderValidatorImp(
 ) : OpenSeaOrderValidator {
 
     override fun validate(order: OrderVersion): Boolean {
-        val result = innerValidate(order)
+        val result = when (order.type) {
+            OrderType.OPEN_SEA_V1 -> innerOpenSeaValidate(order)
+            OrderType.SEAPORT_V1 -> innerSeaportValidate(order)
+            OrderType.RARIBLE_V1, OrderType.RARIBLE_V2, OrderType.CRYPTO_PUNKS -> false
+        }
         if (!result) {
             openSeaOrderErrorRegisteredCounter.increment()
         }
         return result
     }
 
-    private fun innerValidate(order: OrderVersion): Boolean {
+    private fun innerSeaportValidate(order: OrderVersion): Boolean {
+        val data = order.data as? OrderSeaportDataV1 ?: run {
+            logger.info("Invalid Seaport order (data): $order")
+            return false
+        }
+        val signature = order.signature ?: run {
+            logger.info("Invalid Seaport order (empty signature): $order")
+            return false
+        }
+        val domain = EIP712Domain(
+            name = "Seaport",
+            version = "1.1",
+            chainId = BigInteger.ONE,
+            verifyingContract = data.protocol
+        )
+        return try {
+            val hashToSign = domain.hashToSign(order.hash)
+            commonSigner.recover(hashToSign, signature) == order.maker
+        } catch (ex: Exception) {
+            logger.error("Invalid Seaport order (exception signature): $order", ex)
+            false
+        }
+    }
+
+    private fun innerOpenSeaValidate(order: OrderVersion): Boolean {
         val data = order.data as? OrderOpenSeaV1DataV1 ?: run {
             logger.info("Invalid OpenSea order (data): $order")
             return false
