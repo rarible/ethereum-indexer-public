752dda829f89251d4d8eed782663c876c257a06d
sanufriev <s.anufriev@rarible.com>
2022-06-26 11:47:00 +0700

    feat: PT-524 Improve seaport loaded order handle

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/opensea/SeaportOrderLoadHandler.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/opensea/SeaportOrderLoadHandler.kt
@@ -5,6 +5,9 @@ import com.rarible.core.telemetry.metrics.RegisteredCounter
 import com.rarible.protocol.order.core.repository.order.OrderRepository
 import com.rarible.protocol.order.core.service.OrderUpdateService
 import com.rarible.protocol.order.listener.configuration.SeaportLoadProperties
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.time.delay
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -21,31 +24,42 @@ class SeaportOrderLoadHandler(
 
     override suspend fun handle() {
         val orders = openSeaOrderService.getNextSellOrders().orders
-        val createdAts = orders.map { it.createdAt }
-        val minCreatedAt = createdAts.minOrNull() ?: error("Can't be empty value")
-        val maxCreatedAt = createdAts.maxOrNull() ?: error("Can't be empty value")
-
-        logger.seaportInfo(
-            buildString {
-                append("Fetched ${orders.size}, ")
-                append("minCreatedAt=$minCreatedAt, ")
-                append("maxCreatedAt=$maxCreatedAt, ")
-                append("new Seaport orders: ${orders.joinToString { it.orderHash.toString() }}")
-            }
-        )
         if (orders.isNotEmpty()) {
-            orders
-                .mapNotNull {
-                    openSeaOrderConverter.convert(it)
-                }.filter {
-                    openSeaOrderValidator.validate(it)
-                }.forEach {
-                    if (properties.saveEnabled && orderRepository.findById(it.hash) == null) {
-                        orderUpdateService.save(it)
-                        seaportSaveCounter.increment()
-                        logger.seaportInfo("Saved new Seaport order ${it.hash}")
-                    }
+            val createdAts = orders.map { it.createdAt }
+            val minCreatedAt = createdAts.minOrNull()
+            val maxCreatedAt = createdAts.maxOrNull()
+
+            logger.seaportInfo(
+                buildString {
+                    append("Fetched ${orders.size}, ")
+                    append("minCreatedAt=$minCreatedAt, ")
+                    append("maxCreatedAt=$maxCreatedAt, ")
+                    append("new orders: ${orders.joinToString { it.orderHash.toString() }}")
                 }
+            )
+            coroutineScope {
+                @Suppress("ConvertCallChainIntoSequence")
+                orders
+                    .mapNotNull {
+                        openSeaOrderConverter.convert(it)
+                    }.filter {
+                        openSeaOrderValidator.validate(it)
+                    }
+                    .chunked(properties.saveBatchSize)
+                    .map { chunk ->
+                        chunk.map {
+                            async {
+                                if (properties.saveEnabled && orderRepository.findById(it.hash) == null) {
+                                    orderUpdateService.save(it)
+                                    seaportSaveCounter.increment()
+                                    logger.seaportInfo("Saved new order ${it.hash}")
+                                }
+                            }
+                        }.awaitAll()
+                    }
+                    .flatten()
+                    .lastOrNull()
+            }
         } else {
             logger.seaportInfo("No new orders was fetched")
             delay(properties.pollingPeriod)
