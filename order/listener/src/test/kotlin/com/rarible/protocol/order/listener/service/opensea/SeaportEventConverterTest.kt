fa4ac01a6c77c24f5fc8b0a957f6870c71cd44e2
sanufriev <s.anufriev@rarible.com>
2022-06-27 21:50:20 +0700

    feat: PT-539 Add base descriptor bid/sell tests for erc1155

--- order/listener/src/test/kotlin/com/rarible/protocol/order/listener/service/opensea/SeaportEventConverterTest.kt
+++ order/listener/src/test/kotlin/com/rarible/protocol/order/listener/service/opensea/SeaportEventConverterTest.kt
@@ -8,6 +8,7 @@ import com.rarible.protocol.contracts.seaport.v1.events.OrderFulfilledEvent
 import com.rarible.protocol.order.core.data.randomBidOrderUsdValue
 import com.rarible.protocol.order.core.data.randomSellOrderUsdValue
 import com.rarible.protocol.order.core.model.Asset
+import com.rarible.protocol.order.core.model.Erc1155AssetType
 import com.rarible.protocol.order.core.model.Erc20AssetType
 import com.rarible.protocol.order.core.model.Erc721AssetType
 import com.rarible.protocol.order.core.model.EthAssetType
@@ -177,6 +178,96 @@ internal class SeaportEventConverterTest {
         assertThat(right.source).isEqualTo(HistorySource.OPEN_SEA)
         assertThat(left.adhoc).isFalse()
         assertThat(left.counterAdhoc).isTrue()
+    }
+
+    @Test
+    fun `should convert basic erc1155 full sell OrderFulfilledEvent`() = runBlocking<Unit> {
+        val data = Instant.now()
+        val log = log(
+            listOf(
+                Word.apply("0x9d9af8e38d66c62e2c12f0225249fd9d721c54b83f48d9352c97c6cacdcb6f31"),
+                Word.apply("0x0000000000000000000000006c8ba1dafb22eae61e9cd3da724cbc3d164c27b9"),
+                Word.apply("0x00000000000000000000000000000000e88fe2628ebc5da81d2b3cead633e89e")
+            ),
+            "0x4d0ae0fee315be795d0fb0f750fe065c99865a52a0fb0ff1a7463961d56e372f00000000000000000000000047921676a46ccfe3d80b161c7b4ddc8ed9e716b600000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000120000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000030000000000000000000000002ebecabbbe8a8c629b99ab23ed154d74cd5d4342000000000000000000000000000000000000000000000000000000000001aea9000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000c2d8199ce67c0000000000000000000000000006c8ba1dafb22eae61e9cd3da724cbc3d164c27b9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004fefa17b7240000000000000000000000000008de9c5a032463c561423387a9648c5c7bcc5bc90"
+        )
+        val expectedNft = Asset(
+            Erc1155AssetType(
+                Address.apply("0x2ebecabbbe8a8c629b99ab23ed154d74cd5d4342"),
+                EthUInt256.of("110249")
+            ),
+            EthUInt256.of(3)
+        )
+        val expectedPayment = Asset(
+            EthAssetType,
+            EthUInt256.of("900000000000000000")
+        )
+        coEvery { priceUpdateService.getAssetsUsdValue(make = expectedNft, take = expectedPayment, at = data) } returns null
+        coEvery { priceUpdateService.getAssetsUsdValue(make = expectedPayment, take = expectedNft, at = data) } returns null
+
+        val event = OrderFulfilledEvent.apply(log)
+        val matches = converter.convert(event, data)
+
+        val left = matches[0]
+        assertThat(left.side).isEqualTo(OrderSide.LEFT)
+        assertThat(left.make).isEqualTo(expectedNft)
+        assertThat(left.take).isEqualTo(expectedPayment)
+        assertThat(left.fill).isEqualTo(expectedPayment.value)
+        assertThat(left.takeValue).isEqualTo(BigDecimal("0.900000000000000000"))
+        assertThat(left.makeValue).isEqualTo(BigDecimal.valueOf(3))
+        val right = matches[1]
+        assertThat(right.side).isEqualTo(OrderSide.RIGHT)
+        assertThat(right.make).isEqualTo(expectedPayment)
+        assertThat(right.take).isEqualTo(expectedNft)
+        assertThat(right.fill).isEqualTo(expectedNft.value)
+        assertThat(right.makeValue).isEqualTo(BigDecimal("0.900000000000000000"))
+        assertThat(right.takeValue).isEqualTo(BigDecimal.valueOf(3))
+    }
+
+    @Test
+    fun `should convert basic erc1155 full bid OrderFulfilledEvent`() = runBlocking<Unit> {
+        val data = Instant.now()
+        val contract = Erc20Token(name = "", id = randomAddress(), symbol = "", decimals = 18)
+        val log = log(
+            listOf(
+                Word.apply("0x9d9af8e38d66c62e2c12f0225249fd9d721c54b83f48d9352c97c6cacdcb6f31"),
+                Word.apply("0x0000000000000000000000006c8ba1dafb22eae61e9cd3da724cbc3d164c27b9"),
+                Word.apply("0x00000000000000000000000000000000e88fe2628ebc5da81d2b3cead633e89e")
+            ),
+            "0xfd4cd594d6e06ab46a987c95422e9515a4da65a583671b288009b0216395d1e100000000000000000000000047921676a46ccfe3d80b161c7b4ddc8ed9e716b60000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000012000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000c778417e063141139fce010982780140aa0cd5ab0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008e1bc9bf040000000000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000030000000000000000000000002ebecabbbe8a8c629b99ab23ed154d74cd5d4342000000000000000000000000000000000000000000000000000000000001aea900000000000000000000000000000000000000000000000000000000000000020000000000000000000000006c8ba1dafb22eae61e9cd3da724cbc3d164c27b90000000000000000000000000000000000000000000000000000000000000001000000000000000000000000c778417e063141139fce010982780140aa0cd5ab000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000038d7ea4c680000000000000000000000000008de9c5a032463c561423387a9648c5c7bcc5bc90"
+        )
+        val expectedNft = Asset(
+            Erc1155AssetType(
+                Address.apply("0x2ebecabbbe8a8c629b99ab23ed154d74cd5d4342"),
+                EthUInt256.of("110249")
+            ),
+            EthUInt256.of(2)
+        )
+        val expectedPayment = Asset(
+            Erc20AssetType(Address.apply("0xc778417e063141139fce010982780140aa0cd5ab")),
+            EthUInt256.of("40000000000000000")
+        )
+        coEvery { contractService.get(Address.apply("0xc778417e063141139fce010982780140aa0cd5ab")) } returns contract
+        coEvery { priceUpdateService.getAssetsUsdValue(make = expectedNft, take = expectedPayment, at = data) } returns null
+        coEvery { priceUpdateService.getAssetsUsdValue(make = expectedPayment, take = expectedNft, at = data) } returns null
+
+        val event = OrderFulfilledEvent.apply(log)
+        val matches = converter.convert(event, data)
+
+        val left = matches[0]
+        assertThat(left.side).isEqualTo(OrderSide.LEFT)
+        assertThat(left.make).isEqualTo(expectedPayment)
+        assertThat(left.take).isEqualTo(expectedNft)
+        assertThat(left.fill).isEqualTo(expectedNft.value)
+        assertThat(left.makeValue).isEqualTo(BigDecimal("0.040000000000000000"))
+        assertThat(left.takeValue).isEqualTo(BigDecimal.valueOf(2))
+        val right = matches[1]
+        assertThat(right.side).isEqualTo(OrderSide.RIGHT)
+        assertThat(right.make).isEqualTo(expectedNft)
+        assertThat(right.take).isEqualTo(expectedPayment)
+        assertThat(right.fill).isEqualTo(expectedPayment.value)
+        assertThat(right.takeValue).isEqualTo(BigDecimal("0.040000000000000000"))
+        assertThat(right.makeValue).isEqualTo(BigDecimal.valueOf(2))
     }
 
     private fun log(topics: List<Word>,data: String) = Log(
