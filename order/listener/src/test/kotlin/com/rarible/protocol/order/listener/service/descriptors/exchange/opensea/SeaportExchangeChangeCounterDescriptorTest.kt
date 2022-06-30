8252aa6c6a647ced2c0a7b07aea4d9e9383711ec
sanufriev <s.anufriev@rarible.com>
2022-06-30 10:33:00 +0700

    feat: PT-620 Add test for SeaportExchangeChangeCounterDescriptor

---
+++ order/listener/src/test/kotlin/com/rarible/protocol/order/listener/service/descriptors/exchange/opensea/SeaportExchangeChangeCounterDescriptorTest.kt
@@ -0,0 +1,53 @@
+package com.rarible.protocol.order.listener.service.descriptors.exchange.opensea
+
+import com.rarible.core.telemetry.metrics.RegisteredCounter
+import com.rarible.core.test.data.randomAddress
+import com.rarible.ethereum.domain.EthUInt256
+import com.rarible.protocol.order.core.configuration.OrderIndexerProperties
+import com.rarible.protocol.order.listener.data.log
+import io.daonomic.rpc.domain.Word
+import io.mockk.every
+import io.mockk.excludeRecords
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import reactor.kotlin.core.publisher.toFlux
+import scalether.domain.Address
+import scalether.domain.response.Transaction
+import java.time.Instant
+
+internal class SeaportExchangeChangeCounterDescriptorTest {
+    private val exchangeContractAddresses = mockk<OrderIndexerProperties.ExchangeContractAddresses> {
+        every { seaportV1 } returns randomAddress()
+    }
+    private val seaportCounterEventCounter = mockk<RegisteredCounter> {
+        every { increment() } returns Unit
+    }
+    private val descriptor =  SeaportExchangeChangeCounterDescriptor(
+        exchangeContractAddresses,
+        seaportCounterEventCounter
+    )
+
+    @Test
+    fun `should convert SeaportExchangeChangeCounterDescriptor to event`() = runBlocking<Unit> {
+        val transaction = mockk<Transaction>()
+        val date = Instant.ofEpochSecond(1)
+        val log = log(
+            topics = listOf(
+                Word.apply("0x721c20121297512b72821b97f5326877ea8ecf4bb9948fea5bfcb6453074d37f"),
+                Word.apply("0x00000000000000000000000050bd1d1d160928a1c5923646a8474036e3c91c7d")
+            ),
+            data = "0x0000000000000000000000000000000000000000000000000000000000000001"
+        )
+        val events = descriptor.convert(log, transaction, date.epochSecond, 1, 1).toFlux().collectList().awaitSingle()
+        assertThat(events).hasSize(1)
+        assertThat(events.single().date).isEqualTo(date)
+        assertThat(events.single().newNonce).isEqualTo(EthUInt256.ONE)
+        assertThat(events.single().maker).isEqualTo(Address.apply("0x50bd1d1d160928a1c5923646a8474036e3c91c7d"))
+
+        verify(exactly = 1) { seaportCounterEventCounter.increment() }
+    }
+}
