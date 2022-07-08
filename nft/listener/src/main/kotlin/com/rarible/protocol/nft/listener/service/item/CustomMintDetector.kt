f5d0d2bb4d81edc41689dfcea422fbf54ab51e96
nftguru28 <108570959+nftguru28@users.noreply.github.com>
2022-07-08 17:29:24 +0700

    feat: PT-673 Add airdrop method to detect mint

--- nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/CustomMintDetector.kt
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/CustomMintDetector.kt
@@ -9,10 +9,12 @@ import scalether.domain.response.Transaction
 @Component
 class CustomMintDetector {
     fun isMint(event: TransferSingleEvent, transaction: Transaction): Boolean {
-        return event._from() == event._to() && transaction.input().methodSignatureId() == MINT_METHOD_ID_SIGNATURE
+        return event._from() == event._to() && transaction.input().methodSignatureId() in MINT_METHODS
     }
 
     companion object {
         internal val MINT_METHOD_ID_SIGNATURE: Binary = Binary.apply("0x731133e9")
+        internal val AIRDROP_METHOD_ID_SIGNATURE: Binary = Binary.apply("0xc204642c")
+        internal val MINT_METHODS: List<Binary> = listOf(MINT_METHOD_ID_SIGNATURE, AIRDROP_METHOD_ID_SIGNATURE)
     }
 }
