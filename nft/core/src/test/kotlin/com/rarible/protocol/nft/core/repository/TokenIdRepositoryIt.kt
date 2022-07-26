33e37151a1ed73a276c9c538f54a4cbdf11be793
Artem Yakischik <a.yakischik@rarible.com>
2022-07-26 13:09:09 +0400

    fix: Configurable initial value for tokenId (for testnet)

--- nft/core/src/test/kotlin/com/rarible/protocol/nft/core/repository/TokenIdRepositoryIt.kt
+++ nft/core/src/test/kotlin/com/rarible/protocol/nft/core/repository/TokenIdRepositoryIt.kt
@@ -1,5 +1,6 @@
 package com.rarible.protocol.nft.core.repository
 
+import com.rarible.protocol.nft.core.configuration.NftIndexerProperties
 import com.rarible.protocol.nft.core.integration.AbstractIntegrationTest
 import com.rarible.protocol.nft.core.integration.IntegrationTest
 import com.rarible.protocol.nft.core.model.TokenId
@@ -7,25 +8,44 @@ import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 
 @IntegrationTest
 class TokenIdRepositoryIt : AbstractIntegrationTest() {
 
-    @Autowired
-    private lateinit var tokenIdRepository: TokenIdRepository
+    private val zeroInitValue = NftIndexerProperties.CollectionProperties(0)
+    private val nonZeroInitValue = NftIndexerProperties.CollectionProperties(100)
 
     @Test
-    fun newTokenId() = runBlocking<Unit> {
+    fun `new token id - 0 initial value`() = runBlocking<Unit> {
+        val tokenIdRepository = TokenIdRepository(mongo, zeroInitValue)
         val newTokenId = tokenIdRepository.generateTokenId("non-existent")
         assertThat(newTokenId)
             .isEqualTo(1L)
     }
 
     @Test
-    fun existingTokenId() = runBlocking<Unit> {
+    fun `existing token id - 0 initial value`() = runBlocking<Unit> {
+        val tokenIdRepository = TokenIdRepository(mongo, zeroInitValue)
         mongo.save(TokenId("exists", 10)).awaitFirst()
         val newTokenId = tokenIdRepository.generateTokenId("exists")
+        assertThat(newTokenId)
+            .isEqualTo(11L)
+    }
+
+    @Test
+    fun `new token id - 100 initial value`() = runBlocking<Unit> {
+        val tokenIdRepository = TokenIdRepository(mongo, nonZeroInitValue)
+        val newTokenId = tokenIdRepository.generateTokenId("non-existent")
+        assertThat(newTokenId)
+            .isEqualTo(101L)
+    }
+
+    @Test
+    fun `existing token id - 100 initial value`() = runBlocking<Unit> {
+        val tokenIdRepository = TokenIdRepository(mongo, nonZeroInitValue)
+        mongo.save(TokenId("exists", 10)).awaitFirst()
+        val newTokenId = tokenIdRepository.generateTokenId("exists")
+        // Should not affect existing values
         assertThat(newTokenId)
             .isEqualTo(11L)
     }
