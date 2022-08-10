6b3c5d28dc26bd05fb849becf2ab3bf0f72cf7d0
Mikhail Nasimov <m.nasimov@rarible.com>
2022-08-10 09:41:32 +0300

    PT-879 [Ethereum] x2y2 fetch order sign, get cancel input methods

--- order/api/src/main/kotlin/com/rarible/protocol/order/api/controller/OrderSignatureController.kt
+++ order/api/src/main/kotlin/com/rarible/protocol/order/api/controller/OrderSignatureController.kt
@@ -2,16 +2,51 @@ package com.rarible.protocol.order.api.controller
 
 import com.rarible.ethereum.sign.service.ERC1271SignService
 import com.rarible.protocol.dto.EthereumSignatureValidationFormDto
+import com.rarible.protocol.dto.X2Y2GetCancelInputRequestDto
+import com.rarible.protocol.dto.X2Y2OrderSignRequestDto
+import com.rarible.protocol.dto.X2Y2SignResponseDto
+import com.rarible.x2y2.client.X2Y2ApiClient
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class OrderSignatureController(
-    private val erc1271SignService: ERC1271SignService
+    private val erc1271SignService: ERC1271SignService,
+    private val x2Y2ApiClient: X2Y2ApiClient
 ) : OrderSignatureControllerApi {
 
     override suspend fun validate(form: EthereumSignatureValidationFormDto): ResponseEntity<Boolean> {
         val result = erc1271SignService.isSigner(form.signer, form.message, form.signature)
+
         return ResponseEntity.ok(result)
+    }
+
+    override suspend fun x2y2CancelSign(x2Y2GetCancelInputRequestDto: X2Y2GetCancelInputRequestDto): ResponseEntity<X2Y2SignResponseDto> {
+        val response = with(x2Y2GetCancelInputRequestDto) {
+            x2Y2ApiClient.getCancelInput(
+                caller = caller,
+                op = op,
+                orderId = orderId,
+                signMessage = signMessage,
+                sign = sign
+            )
+        }
+
+        return ResponseEntity.ok(X2Y2SignResponseDto(response.data.single().input))
+    }
+
+    override suspend fun x2y2OrderSign(x2Y2OrderSignRequestDto: X2Y2OrderSignRequestDto): ResponseEntity<X2Y2SignResponseDto> {
+        val response = with(x2Y2OrderSignRequestDto) {
+            x2Y2ApiClient.fetchOrderSign(
+                caller = caller,
+                op = op,
+                orderId = orderId,
+                currency = currency,
+                price = price,
+                tokenId = tokenId
+            )
+        }
+
+        return ResponseEntity.ok(X2Y2SignResponseDto(response.data.single().input))
     }
 }
