73e3fe643cc8609e2aca35671ed5b6046086141f
sanufriev <s.anufriev@rarible.com>
2022-08-10 22:25:30 +0700

    feat: PT-916 Add tests for X2Y2OrderService

---
+++ order/listener/src/test/kotlin/com/rarible/protocol/order/listener/data/X2Y2Factory.kt
@@ -0,0 +1,43 @@
+package com.rarible.protocol.order.listener.data
+
+import com.rarible.core.test.data.randomAddress
+import com.rarible.core.test.data.randomBigInt
+import com.rarible.core.test.data.randomBinary
+import com.rarible.core.test.data.randomBoolean
+import com.rarible.core.test.data.randomInt
+import com.rarible.core.test.data.randomLong
+import com.rarible.core.test.data.randomWord
+import com.rarible.looksrare.client.model.v1.LooksrareOrder
+import com.rarible.looksrare.client.model.v1.Status
+import com.rarible.x2y2.client.model.Order
+import com.rarible.x2y2.client.model.OrderStatus
+import com.rarible.x2y2.client.model.OrderType
+import com.rarible.x2y2.client.model.Token
+import io.daonomic.rpc.domain.Word
+import scalether.domain.Address
+import java.math.BigInteger
+import java.time.Duration
+import java.time.Instant
+
+fun randomX2Y2Order(): Order {
+    return Order(
+        id = randomBigInt(),
+        maker = randomAddress(),
+        currency = randomAddress(),
+        taker = randomAddress(),
+        price = randomBigInt(),
+        createdAt = Instant.now(),
+        itemHash = Word.apply(randomWord()),
+        type = OrderType.values().random(),
+        isCollectionOffer = randomBoolean(),
+        endAt = Instant.now(),
+        isBundle = randomBoolean(),
+        side = randomInt(),
+        status = OrderStatus.values().random(),
+        token = Token(
+            contract = randomAddress(),
+            tokenId = randomBigInt()
+        ),
+        updatedAt = Instant.now()
+    )
+}
