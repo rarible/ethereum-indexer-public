cf619627a7bbe5d81db18d95bfd128d90baf427c
sanufriev <s.anufriev@rarible.com>
2022-08-17 19:54:59 +0700

    feat: PT-980 Make looksrare order service  retry requests

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/looksrare/LooksrareOrderService.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/looksrare/LooksrareOrderService.kt
@@ -2,7 +2,10 @@ package com.rarible.protocol.order.listener.service.looksrare
 
 import com.rarible.core.telemetry.metrics.RegisteredCounter
 import com.rarible.looksrare.client.LooksrareClient
+import com.rarible.looksrare.client.model.LooksrareError
+import com.rarible.looksrare.client.model.OperationResult
 import com.rarible.looksrare.client.model.v1.LooksrareOrder
+import com.rarible.looksrare.client.model.v1.LooksrareOrders
 import com.rarible.looksrare.client.model.v1.OrdersRequest
 import com.rarible.looksrare.client.model.v1.Pagination
 import com.rarible.looksrare.client.model.v1.Sort
@@ -11,6 +14,7 @@ import com.rarible.protocol.order.listener.configuration.LooksrareLoadProperties
 import com.rarible.protocol.order.listener.misc.LOOKSRARE_LOG
 import com.rarible.protocol.order.listener.misc.looksrareInfo
 import io.daonomic.rpc.domain.Word
+import kotlinx.coroutines.time.delay
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.time.Instant
@@ -37,7 +41,7 @@ class LooksrareOrderService(
             )
             logger.looksrareInfo("Load next: startTime=${request.startTime?.epochSecond}, cursor=${request.pagination?.cursor}")
 
-            val result = looksrareClient.getOrders(request).ensureSuccess()
+            val result = getOrders(request)
             if (result.success.not()) throw IllegalStateException("$LOOKSRARE_LOG Can't load orders: ${result.message}")
             looksrareLoadCounter.increment(result.data.size)
             loadOrders.addAll(result.data)
@@ -47,5 +51,20 @@ class LooksrareOrderService(
         } while (lastLoadOrder != null && lastLoadOrder.startTime > listedAfter)
 
         return loadOrders.toList()
+    }
+
+    private suspend fun getOrders(request: OrdersRequest): LooksrareOrders {
+        var lastError: LooksrareError? = null
+        var retries = 0
+
+        while (retries < properties.retry) {
+            when (val result = looksrareClient.getOrders(request)) {
+                is OperationResult.Success -> return result.result
+                is OperationResult.Fail -> lastError = result.error
+            }
+            retries += 1
+            delay(properties.retryDelay)
+        }
+        throw IllegalStateException("Can't fetch Looksrare orders, number of attempts exceeded, last error: $lastError")
     }
 }
