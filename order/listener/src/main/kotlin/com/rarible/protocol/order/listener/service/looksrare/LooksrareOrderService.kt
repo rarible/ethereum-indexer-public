b6d9c09b800cfabbfc686773837b3f3d353b88ff
sanufriev <s.anufriev@rarible.com>
2022-08-17 14:49:25 +0700

    feat: Add lr load log

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/looksrare/LooksrareOrderService.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/looksrare/LooksrareOrderService.kt
@@ -9,7 +9,9 @@ import com.rarible.looksrare.client.model.v1.Sort
 import com.rarible.looksrare.client.model.v1.Status
 import com.rarible.protocol.order.listener.configuration.LooksrareLoadProperties
 import com.rarible.protocol.order.listener.misc.LOOKSRARE_LOG
+import com.rarible.protocol.order.listener.misc.looksrareInfo
 import io.daonomic.rpc.domain.Word
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.time.Instant
 
@@ -19,6 +21,8 @@ class LooksrareOrderService(
     private val looksrareLoadCounter: RegisteredCounter,
     private val properties: LooksrareLoadProperties
 ) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     suspend fun getNextSellOrders(listedAfter: Instant, listedBefore: Instant): List<LooksrareOrder> {
         val loadOrders = mutableSetOf<LooksrareOrder>()
         var nextHash: Word? = null
@@ -31,6 +35,8 @@ class LooksrareOrderService(
                 sort = Sort.NEWEST,
                 pagination = Pagination(first = properties.loadMaxSize, cursor = nextHash?.prefixed())
             )
+            logger.looksrareInfo("Load next: startTime=${request.startTime}, cursor=${request.pagination?.cursor}")
+
             val result = looksrareClient.getOrders(request).ensureSuccess()
             if (result.success.not()) throw IllegalStateException("$LOOKSRARE_LOG Can't load orders: ${result.message}")
             looksrareLoadCounter.increment(result.data.size)
