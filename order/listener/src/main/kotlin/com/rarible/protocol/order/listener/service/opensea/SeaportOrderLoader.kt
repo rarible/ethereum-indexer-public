b59eee39734030b252839b715f5ee2f0ea982625
sanufriev <s.anufriev@rarible.com>
2022-08-17 23:29:20 +0700

    feat: PT-1004 Try too speed up Seaport load job

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/opensea/SeaportOrderLoader.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/opensea/SeaportOrderLoader.kt
@@ -7,8 +7,10 @@ import com.rarible.protocol.order.core.service.OrderUpdateService
 import com.rarible.protocol.order.listener.configuration.SeaportLoadProperties
 import com.rarible.protocol.order.listener.misc.seaportError
 import com.rarible.protocol.order.listener.misc.seaportInfo
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.coroutineScope
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -22,10 +24,11 @@ class SeaportOrderLoader(
     private val properties: SeaportLoadProperties,
     private val seaportSaveCounter: RegisteredCounter
 ) {
-    suspend fun load(cursor: String?): SeaportOrders {
-        val result = safeGetNextSellOrders(cursor)
-        val orders = result.orders
-        if (orders.isNotEmpty()) {
+    suspend fun load(cursor: String?) = coroutineScope {
+        var lastSeaResult: SeaportOrders? = null
+        for (result in produceNextSellOrders(cursor, properties.channelCapacity)) {
+            lastSeaResult = result
+            val orders = result.orders
             val createdAts = orders.map { it.createdAt }
             val minCreatedAt = createdAts.minOrNull()
             val maxCreatedAt = createdAts.maxOrNull()
@@ -39,44 +42,46 @@ class SeaportOrderLoader(
                     append("new orders: ${orders.joinToString { it.orderHash.toString() }}")
                 }
             )
-            coroutineScope {
-                @Suppress("ConvertCallChainIntoSequence")
-                orders
-                    .chunked(properties.saveBatchSize)
-                    .map { chunk ->
-                        chunk.map {
-                            async {
-                                val order = openSeaOrderConverter.convert(it)
-                                if (
-                                    order != null &&
-                                    properties.saveEnabled &&
-                                    openSeaOrderValidator.validate(order) &&
-                                    orderRepository.findById(order.hash) == null
-                                ) {
-                                    orderUpdateService.save(order)
-                                    seaportSaveCounter.increment()
-                                    orderUpdateService.updateMakeStock(order.hash)
-                                    logger.seaportInfo("Saved new order ${order.hash}")
-                                }
+            @Suppress("ConvertCallChainIntoSequence")
+            orders
+                .chunked(properties.saveBatchSize)
+                .map { chunk ->
+                    chunk.map {
+                        async {
+                            val order = openSeaOrderConverter.convert(it)
+                            if (
+                                order != null &&
+                                properties.saveEnabled &&
+                                openSeaOrderValidator.validate(order) &&
+                                orderRepository.findById(order.hash) == null
+                            ) {
+                                orderUpdateService.save(order)
+                                seaportSaveCounter.increment()
+                                orderUpdateService.updateMakeStock(order.hash)
+                                logger.seaportInfo("Saved new order ${order.hash}")
                             }
-                        }.awaitAll()
-                    }
-                    .flatten()
-                    .lastOrNull()
-            }
-        } else {
-            logger.seaportInfo("No new orders was fetched")
+                        }
+                    }.awaitAll()
+                }
+                .flatten()
+                .lastOrNull()
         }
-        return result
+        lastSeaResult ?: throw IllegalStateException("Unexpected null result for cursor $cursor")
     }
 
-    private suspend fun safeGetNextSellOrders(cursor: String?): SeaportOrders {
-        return try {
-            openSeaOrderService.getNextSellOrders(cursor)
-        } catch (ex: Throwable) {
-            logger.seaportError("Can't get next orders with cursor $cursor", ex)
-            throw ex
-        }
+    @Suppress("EXPERIMENTAL_API_USAGE", "OPT_IN_USAGE")
+    private fun CoroutineScope.produceNextSellOrders(cursor: String?, capacity: Int) = produce(capacity = capacity) {
+        do {
+            var previous = cursor
+            try {
+                val result = openSeaOrderService.getNextSellOrders(previous)
+                previous = result.previous
+                send(result)
+            } catch (ex: Throwable) {
+                logger.seaportError("Can't get next orders with cursor $cursor", ex)
+                throw ex
+            }
+        } while (previous != null)
     }
 
     private companion object {
