640e26514fef2914feb18ab4297c67a7ea17ef2e
Denis Kilchichakov <kdenis87@gmail.com>
2022-09-28 17:15:25 +0300

    PT-1219: feat: item-ownership-consistency job added (scaffolds)

--- nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/ItemOwnershipConsistencyHandler.kt
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/ItemOwnershipConsistencyHandler.kt
@@ -1,12 +1,16 @@
 package com.rarible.protocol.nft.listener.service.item
 
 import com.rarible.core.daemon.job.JobHandler
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class ItemOwnershipConsistencyHandler : JobHandler {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override suspend fun handle() {
+        logger.info("ItemOwnershipConsistencyHandler handle() called")
         try {
             // get state
 
