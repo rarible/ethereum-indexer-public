4d770422b97a17593f0427d366585c79de03a224
Artem Yakischik <a.yakischik@rarible.com>
2022-10-04 16:07:21 +0500

    fix: Polygon mint detector added for custom mint method

--- nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/CustomMintDetector.kt
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/item/CustomMintDetector.kt
@@ -10,24 +10,51 @@ import scalether.domain.response.Transaction
 @Component
 class CustomMintDetector {
 
+    companion object {
+
+        // Ethereum
+        internal val MINT_METHOD_ID_SIGNATURE: Binary = Binary.apply("0x731133e9")
+        internal val DISTRIBUTE_NFT_METHOD_ID_SIGNATURE: Binary = Binary.apply("0x3a9e8069")
+
+        // Polygon
+        // Example: https://polygonscan.com/tx/0x7726beeb43d56ff516cc5c842ab756a0da5e8dc35efe9731e87104cddd06a726
+        internal val AIRDROP_METHOD_ID_SIGNATURE: Binary = Binary.apply("0xc204642c")
+
+        // Example: https://polygonscan.com/tx/0x8c330658db60ebd4befdf9edfd3753e7f1448e7fed1a840322b9390408fef1e8
+        internal val POLYGON_MINT_METHOD_ID_SIGNATURE: Binary = Binary.apply("0xea66696c")
+    }
+
+    private val erc721methodDetectors = listOf<MethodMintDetector<TransferEvent>>(
+        MethodMintDetector(DISTRIBUTE_NFT_METHOD_ID_SIGNATURE) { e, t -> e.from() == e.log().address() }
+    )
+
+    private val erc1155methodDetectors = listOf<MethodMintDetector<TransferSingleEvent>>(
+        MethodMintDetector(DISTRIBUTE_NFT_METHOD_ID_SIGNATURE) { e, t -> e._from() == e.log().address() },
+        MethodMintDetector(MINT_METHOD_ID_SIGNATURE) { e, t -> e._from() == e._to() },
+        MethodMintDetector(AIRDROP_METHOD_ID_SIGNATURE) { e, t -> t.from() == e._from() },
+        MethodMintDetector(POLYGON_MINT_METHOD_ID_SIGNATURE) { e, t -> t.from() == e._from() },
+    )
+
     fun isErc1155Mint(event: TransferSingleEvent, transaction: Transaction): Boolean {
-        return (isMethodCalled(transaction, MINT_METHOD_ID_SIGNATURE) && event._from() == event._to()) ||
-            (isMethodCalled(transaction, AIRDROP_METHOD_ID_SIGNATURE) && transaction.from() == event._from()) ||
-            (isMethodCalled(transaction, DISTRIBUTE_NFT_METHOD_ID_SIGNATURE) && event._from() == event.log().address())
+        return erc1155methodDetectors.find { it.isMint(event, transaction) } != null
     }
 
     fun isErc721Mint(event: TransferEvent, transaction: Transaction): Boolean {
-        return isMethodCalled(transaction, DISTRIBUTE_NFT_METHOD_ID_SIGNATURE) && event.from() == event.log().address()
+        return erc721methodDetectors.find { it.isMint(event, transaction) } != null
     }
 
-    private fun isMethodCalled(transaction: Transaction, method: Binary): Boolean {
-        return transaction.input().methodSignatureId() == method
-    }
+    class MethodMintDetector<E>(
+        private val method: Binary,
+        private val match: (event: E, transaction: Transaction) -> Boolean
+    ) {
 
-    companion object {
+        fun isMint(event: E, transaction: Transaction): Boolean {
+            return isMethodCalled(transaction, method) && match(event, transaction)
+        }
 
-        internal val MINT_METHOD_ID_SIGNATURE: Binary = Binary.apply("0x731133e9")
-        internal val AIRDROP_METHOD_ID_SIGNATURE: Binary = Binary.apply("0xc204642c")
-        internal val DISTRIBUTE_NFT_METHOD_ID_SIGNATURE: Binary = Binary.apply("0x3a9e8069")
+        private fun isMethodCalled(transaction: Transaction, method: Binary): Boolean {
+            return transaction.input().methodSignatureId() == method
+        }
+
     }
 }
