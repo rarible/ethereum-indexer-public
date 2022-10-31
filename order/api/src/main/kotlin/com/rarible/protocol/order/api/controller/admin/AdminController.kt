ab355c4f38a05e70623ce5516390dd898cf2c0eb
sanufriev <s.anufriev@rarible.com>
2022-11-01 04:24:42 +0700

    fix: Fix controller

--- order/api/src/main/kotlin/com/rarible/protocol/order/api/controller/admin/AdminController.kt
+++ order/api/src/main/kotlin/com/rarible/protocol/order/api/controller/admin/AdminController.kt
@@ -7,7 +7,7 @@ import com.rarible.protocol.order.api.service.order.OrderService
 import com.rarible.protocol.order.core.converters.dto.OrderDtoConverter
 import com.rarible.protocol.order.core.model.OrderState
 import com.rarible.protocol.order.core.repository.order.OrderStateRepository
-import com.rarible.protocol.order.core.service.OrderReduceService
+import com.rarible.protocol.order.core.service.OrderUpdateService
 import io.daonomic.rpc.domain.Word
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.springframework.http.ResponseEntity
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController
 @ExperimentalCoroutinesApi
 @RestController
 class AdminController(
-    private val orderReduceService: OrderReduceService,
+    private val orderUpdateService: OrderUpdateService,
     private val orderService: OrderService,
     private val orderStateRepository: OrderStateRepository,
     private val orderDtoConverter: OrderDtoConverter,
@@ -39,7 +39,8 @@ class AdminController(
                 ?: OrderState.toState(order.hash, orderStateDto)
 
             orderStateRepository.save(state)
-            orderReduceService.updateOrder(order.hash) ?: error("Unexpected empty reduce result for hash $hash")
+            orderUpdateService.update(order.hash)
+            orderService.get(Word.apply(hash))
         }.let { ResponseEntity.ok(orderDtoConverter.convert(it)) }
     }
 }
