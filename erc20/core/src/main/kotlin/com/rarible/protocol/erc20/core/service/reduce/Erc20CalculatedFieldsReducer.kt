3179c3bdd7825191ae195b058b258548f8cd48c2
Sergey Yurov <s.yurov@rarible.com>
2022-11-23 15:17:59 +0300

    PT-1393 add calculated field reducer

---
+++ erc20/core/src/main/kotlin/com/rarible/protocol/erc20/core/service/reduce/Erc20CalculatedFieldsReducer.kt
@@ -0,0 +1,19 @@
+package com.rarible.protocol.erc20.core.service.reduce
+
+import com.rarible.blockchain.scanner.ethereum.model.EthereumLogStatus
+import com.rarible.core.entity.reducer.service.Reducer
+import com.rarible.protocol.erc20.core.model.Erc20Balance
+import com.rarible.protocol.erc20.core.model.Erc20Event
+import org.springframework.stereotype.Component
+
+@Component
+class Erc20CalculatedFieldsReducer : Reducer<Erc20Event, Erc20Balance> {
+    override suspend fun reduce(entity: Erc20Balance, event: Erc20Event): Erc20Balance {
+        val updatedAt =
+            // We try to get timestamp of the latest blockchain event
+            entity.revertableEvents.lastOrNull { it.log.status == EthereumLogStatus.CONFIRMED }?.log?.createdAt ?:
+            entity.lastUpdatedAt
+
+        return entity.copy(lastUpdatedAt = updatedAt)
+    }
+}
