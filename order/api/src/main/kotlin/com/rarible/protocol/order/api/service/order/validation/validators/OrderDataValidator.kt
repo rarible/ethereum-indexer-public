a6d2b2c377882a5bdf3bd3ef1882e127bf114551
Denis Kilchichakov <kdenis87@gmail.com>
2022-11-23 16:08:26 +0400

    PT-1726: added validation on payout sum

--- order/api/src/main/kotlin/com/rarible/protocol/order/api/service/order/validation/validators/OrderDataValidator.kt
+++ order/api/src/main/kotlin/com/rarible/protocol/order/api/service/order/validation/validators/OrderDataValidator.kt
@@ -1,20 +1,29 @@
 package com.rarible.protocol.order.api.service.order.validation.validators
 
 import com.rarible.protocol.dto.EthereumOrderUpdateApiErrorDto
+import com.rarible.protocol.order.api.exceptions.OrderDataException
 import com.rarible.protocol.order.api.exceptions.OrderUpdateException
 import com.rarible.protocol.order.api.service.order.validation.OrderVersionValidator
 import com.rarible.protocol.order.core.model.OrderAmmData
 import com.rarible.protocol.order.core.model.OrderCryptoPunksData
+import com.rarible.protocol.order.core.model.OrderData
 import com.rarible.protocol.order.core.model.OrderDataLegacy
 import com.rarible.protocol.order.core.model.OrderLooksrareDataV1
 import com.rarible.protocol.order.core.model.OrderRaribleV2Data
+import com.rarible.protocol.order.core.model.OrderRaribleV2DataV1
+import com.rarible.protocol.order.core.model.OrderRaribleV2DataV2
 import com.rarible.protocol.order.core.model.OrderType
 import com.rarible.protocol.order.core.model.OrderVersion
 import com.rarible.protocol.order.core.model.OrderX2Y2DataV1
+import com.rarible.protocol.order.core.model.Part
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
+import java.math.BigInteger
 
 @Component
 class OrderDataValidator : OrderVersionValidator {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
     override suspend fun validate(orderVersion: OrderVersion) {
         val isValidOrderDataType = when (orderVersion.type) {
             OrderType.RARIBLE_V1 -> orderVersion.data is OrderDataLegacy
@@ -30,6 +39,21 @@ class OrderDataValidator : OrderVersionValidator {
                 "Order with type ${orderVersion.type} has invalid order data",
                 EthereumOrderUpdateApiErrorDto.Code.INCORRECT_ORDER_DATA
             )
+        }
+        validatePayouts(orderVersion.data)
+    }
+
+    private fun validatePayouts(orderData: OrderData) {
+        val payouts = when (orderData) {
+            is OrderRaribleV2DataV1 -> orderData.payouts
+            is OrderRaribleV2DataV2 -> orderData.payouts
+            else -> emptyList()
+        }
+
+        if (payouts.isNotEmpty()) {
+            val sum = payouts.sumOf { it.value.value }
+            if (sum != BigInteger.valueOf(10000))
+                throw OrderDataException("Payouts sum not equal 100%")
         }
     }
 }
