b9e90362b972bc53f43466c87b9d028e3588f7a6
Sergey Yurov <s.yurov@rarible.com>
2022-11-24 18:04:26 +0300

    PT-1393 fix monitoring

--- erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/configuration/BlockchainScannerV1Configuration.kt
+++ erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/configuration/BlockchainScannerV1Configuration.kt
@@ -1,10 +1,12 @@
 package com.rarible.protocol.erc20.listener.configuration
 
 import com.rarible.ethereum.listener.log.EnableLogListeners
+import com.rarible.ethereum.monitoring.EnableBlockchainMonitoring
 import com.rarible.protocol.erc20.listener.Erc20ListenerApplication
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableOnScannerV1
+@EnableBlockchainMonitoring
 @EnableLogListeners(scanPackage = [Erc20ListenerApplication::class])
 class BlockchainScannerV1Configuration
