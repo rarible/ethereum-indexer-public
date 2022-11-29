e3e684a46845d3a7431172941ec63fd84c4c92a8
Sergey Yurov <s.yurov@rarible.com>
2022-11-30 00:34:32 +0300

    PT-1569 Drop unused indexes in ethereum-indexer

---
+++ nft/migration/src/main/kotlin/com/rarible/protocol/nft/migration/mongock/mongo/ChangeLog00027RemoveUnusedIndexes.kt
@@ -0,0 +1,40 @@
+package com.rarible.protocol.nft.migration.mongock.mongo
+
+import com.github.cloudyrock.mongock.ChangeLog
+import com.github.cloudyrock.mongock.ChangeSet
+import com.rarible.protocol.nft.core.repository.item.ItemRepository
+import com.rarible.protocol.nft.core.repository.ownership.OwnershipRepository
+import io.changock.migration.api.annotations.NonLockGuarded
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
+
+@ChangeLog(order = "00027")
+class ChangeLog00027RemoveUnusedIndexes {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @ChangeSet(
+        id = "ChangeLog00027RemoveUnusedIndexes", order = "1", author = "protocol"
+    )
+    fun removeIndexes(@NonLockGuarded template: ReactiveMongoOperations) = runBlocking<Unit> {
+
+        dropIndex(template, ItemRepository.COLLECTION, "creator_1_date_-1__id_-1")
+        dropIndex(template, ItemRepository.COLLECTION, "owners_1_date_-1__id_-1")
+        dropIndex(template, ItemRepository.COLLECTION, "token_1_tokenId_1_date_1__id_1")
+
+        dropIndex(template, OwnershipRepository.COLLECTION, "token_1_tokenId_1_owner_1")
+    }
+
+    private suspend fun dropIndex(template: ReactiveMongoOperations, collection: String, indexName: String) {
+        val exists = template.indexOps(collection).indexInfo.any { it.name == indexName }.awaitFirst()
+
+        if (!exists) {
+            logger.warn("We want to drop index $collection.$indexName but it now found. Skipping ...")
+            return
+        }
+
+        template.indexOps(collection).dropIndex(indexName).awaitFirst()
+    }
+}
---
+++ order/migration/src/main/kotlin/com/rarible/protocol/order/migration/mongock/mongo/ChangeLog00026RemoveUnusedIndexes.kt
@@ -0,0 +1,42 @@
+package com.rarible.protocol.order.migration.mongock.mongo
+
+import com.github.cloudyrock.mongock.ChangeLog
+import com.github.cloudyrock.mongock.ChangeSet
+import com.rarible.protocol.order.core.repository.approval.ApprovalHistoryRepository
+import com.rarible.protocol.order.core.repository.exchange.ExchangeHistoryRepository
+import com.rarible.protocol.order.core.repository.order.MongoOrderRepository
+import io.changock.migration.api.annotations.NonLockGuarded
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
+
+@ChangeLog(order = "00026")
+class ChangeLog00026RemoveUnusedIndexes {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @ChangeSet(
+        id = "ChangeLog00026RemoveUnusedIndexes", order = "1", author = "protocol"
+    )
+    fun removeIndexes(@NonLockGuarded template: ReactiveMongoOperations) = runBlocking<Unit> {
+
+        dropIndex(template, MongoOrderRepository.COLLECTION, "createdAt_1__id_1")
+        dropIndex(template, MongoOrderRepository.COLLECTION, "make.type.nft_1_make.type.token_1_status_1_lastUpdateAt_1__id_1")
+
+        dropIndex(template, ApprovalHistoryRepository.COLLECTION, "transactionHash_1_blockHash_1_logIndex_1_minorLogIndex_1")
+
+        dropIndex(template, ExchangeHistoryRepository.COLLECTION, "data.hash_1_data.source_1_blockNumber_1_logIndex_1_minorLogIndex_1")
+    }
+
+    private suspend fun dropIndex(template: ReactiveMongoOperations, collection: String, indexName: String) {
+        val exists = template.indexOps(collection).indexInfo.any { it.name == indexName }.awaitFirst()
+
+        if (!exists) {
+            logger.warn("We want to drop index $collection.$indexName but it now found. Skipping ...")
+            return
+        }
+
+        template.indexOps(collection).dropIndex(indexName).awaitFirst()
+    }
+}
