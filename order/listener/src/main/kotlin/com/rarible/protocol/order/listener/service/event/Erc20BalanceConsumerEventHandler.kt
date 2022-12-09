8be13607809dd66b4b7e54409c8e61839c7ba1a4
sanufriev <s.anufriev@rarible.com>
2022-12-09 17:53:47 +0700

    feat: Remove unneeded import

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/Erc20BalanceConsumerEventHandler.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/Erc20BalanceConsumerEventHandler.kt
@@ -3,9 +3,7 @@ package com.rarible.protocol.order.listener.service.event
 import com.rarible.core.daemon.sequential.ConsumerEventHandler
 import com.rarible.protocol.dto.Erc20BalanceEventDto
 import com.rarible.protocol.order.listener.service.order.OrderBalanceService
-import kotlinx.coroutines.time.delay
 import org.slf4j.LoggerFactory
-import java.time.Duration
 
 class Erc20BalanceConsumerEventHandler(
     private val orderBalanceService: OrderBalanceService
--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/NftOwnershipConsumerEventHandler.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/NftOwnershipConsumerEventHandler.kt
@@ -3,9 +3,7 @@ package com.rarible.protocol.order.listener.service.event
 import com.rarible.core.daemon.sequential.ConsumerEventHandler
 import com.rarible.protocol.dto.NftOwnershipEventDto
 import com.rarible.protocol.order.listener.service.order.OrderBalanceService
-import kotlinx.coroutines.time.delay
 import org.slf4j.LoggerFactory
-import java.time.Duration
 
 class NftOwnershipConsumerEventHandler(
     private val orderBalanceService: OrderBalanceService
