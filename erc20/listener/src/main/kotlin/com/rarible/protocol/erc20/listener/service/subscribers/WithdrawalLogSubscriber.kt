2068639d10868243404c4587170e21d5e37acb7e
sanufriev <s.anufriev@rarible.com>
2022-12-16 19:38:24 +0700

    fix: Fix erc20 indexer

--- erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/service/subscribers/WithdrawalLogSubscriber.kt
+++ erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/service/subscribers/WithdrawalLogSubscriber.kt
@@ -2,9 +2,9 @@ package com.rarible.protocol.erc20.listener.service.subscribers
 
 import com.rarible.protocol.erc20.core.model.Erc20TokenHistory
 import com.rarible.protocol.erc20.core.model.SubscriberGroups
-import com.rarible.protocol.erc20.listener.service.descriptors.erc20.TransferLogDescriptor
+import com.rarible.protocol.erc20.listener.service.descriptors.erc20.WithdrawalLogDescriptor
 import org.springframework.stereotype.Component
 
 @Component
-class WithdrawalLogSubscriber(descriptor: TransferLogDescriptor) :
+class WithdrawalLogSubscriber(descriptor: WithdrawalLogDescriptor) :
     AbstractBalanceLogEventSubscriber<Erc20TokenHistory>(SubscriberGroups.ERC20_HISTORY, descriptor)
