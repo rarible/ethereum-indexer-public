4950b2c2630f7d7a99f14e98b6f54555f3c26c54
sanufriev <s.anufriev@rarible.com>
2023-01-25 13:27:49 +0700

    feat: Fix order order without version

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/job/RaribleBidsCanceledAfterExpiredJob.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/job/RaribleBidsCanceledAfterExpiredJob.kt
@@ -3,14 +3,20 @@ package com.rarible.protocol.order.listener.job
 import com.rarible.core.daemon.DaemonWorkerProperties
 import com.rarible.core.daemon.sequential.SequentialDaemonWorker
 import com.rarible.protocol.order.core.configuration.OrderIndexerProperties
+import com.rarible.protocol.order.core.model.OrderVersion
 import com.rarible.protocol.order.core.repository.order.OrderRepository
+import com.rarible.protocol.order.core.repository.order.OrderVersionRepository
 import com.rarible.protocol.order.core.service.OrderUpdateService
 import com.rarible.protocol.order.listener.configuration.OrderListenerProperties
+import io.daonomic.rpc.domain.Word
 import io.micrometer.core.instrument.MeterRegistry
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.time.delay
 
 class RaribleBidsCanceledAfterExpiredJob(
     private val orderRepository: OrderRepository,
+    private val orderVersionRepository: OrderVersionRepository,
     private val orderUpdateService: OrderUpdateService,
     raribleOrderExpiration: OrderIndexerProperties.RaribleOrderExpirationProperties,
     properties: OrderListenerProperties,
@@ -24,10 +30,41 @@ class RaribleBidsCanceledAfterExpiredJob(
 
     override suspend fun handle() {
         orderRepository.findAllLiveBidsHashesLastUpdatedBefore(fixedExpireDate).collect {
-            logger.info("Found expire bid $it after $fixedExpireDate, bid is cancelled.")
+            fixOrder(it)
             orderUpdateService.update(it)
-            logger.info("Bid $it updated")
+            logger.info("Found expire bid $it after $fixedExpireDate, bid is cancelled.")
         }
         delay(delayPeriod)
+    }
+
+    private suspend fun fixOrder(hash: Word) {
+        val orderVersions = orderVersionRepository.findAllByHash(hash).toList()
+        if (orderVersions.isEmpty()) {
+            val order = orderRepository.findById(hash) ?: return
+            val orderVersion = OrderVersion(
+                    maker = order.maker,
+                    make = order.make,
+                    take = order.take,
+                    taker = order.taker,
+                    type = order.type,
+                    salt = order.salt,
+                    start = order.start,
+                    end = order.end,
+                    data = order.data,
+                    signature = order.signature,
+                    platform = order.platform,
+                    hash = order.hash,
+                    approved = order.approved,
+                    createdAt = order.createdAt,
+                    makePriceUsd = null,
+                    takePriceUsd = null,
+                    makePrice = null,
+                    takePrice = null,
+                    makeUsd = null,
+                    takeUsd = null
+            )
+            orderVersionRepository.save(orderVersion).awaitFirst()
+            logger.info("Fixed order $hash without version")
+        }
     }
 }
