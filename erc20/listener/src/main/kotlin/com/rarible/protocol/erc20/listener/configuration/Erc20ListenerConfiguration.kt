f544f9df4a61006d5934eb909b374d1a8b5bfae0
sanufriev <s.anufriev@rarible.com>
2023-02-27 19:17:31 +0700

    feat: Enable rarible task

--- erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/configuration/Erc20ListenerConfiguration.kt
+++ erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/configuration/Erc20ListenerConfiguration.kt
@@ -3,6 +3,7 @@ package com.rarible.protocol.erc20.listener.configuration
 import com.github.cloudyrock.spring.v5.EnableMongock
 import com.rarible.core.reduce.blockchain.BlockchainSnapshotStrategy
 import com.rarible.core.reduce.service.ReduceService
+import com.rarible.core.task.EnableRaribleTask
 import com.rarible.ethereum.contract.EnableContractService
 import com.rarible.ethereum.converters.EnableScaletherMongoConversions
 import com.rarible.protocol.erc20.core.configuration.ProducerConfiguration
@@ -27,6 +28,7 @@ import scalether.transaction.ReadOnlyMonoTransactionSender
 @EnableMongock
 @Configuration
 @EnableContractService
+@EnableRaribleTask
 @EnableScaletherMongoConversions
 @EnableConfigurationProperties(Erc20ListenerProperties::class)
 @Import(ProducerConfiguration::class)
