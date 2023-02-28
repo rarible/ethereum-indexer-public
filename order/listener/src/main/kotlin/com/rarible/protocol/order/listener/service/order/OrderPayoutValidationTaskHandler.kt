a6ad081f03d5280e3247830d74162174576eb450
Artem Yakischik <a.yakischik@rarible.com>
2023-02-28 18:19:10 +0500

    fix: Cancel all orders with incorrect payouts

---
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/order/OrderPayoutValidationTaskHandler.kt
@@ -0,0 +1,74 @@
+package com.rarible.protocol.order.listener.service.order
+
+import com.rarible.core.common.nowMillis
+import com.rarible.core.task.TaskHandler
+import com.rarible.protocol.dto.OrderUpdateEventDto
+import com.rarible.protocol.dto.offchainEventMark
+import com.rarible.protocol.order.core.converters.dto.OrderDtoConverter
+import com.rarible.protocol.order.core.model.Order
+import com.rarible.protocol.order.core.model.OrderStatus
+import com.rarible.protocol.order.core.model.Platform
+import com.rarible.protocol.order.core.producer.ProtocolOrderPublisher
+import com.rarible.protocol.order.core.repository.order.OrderRepository
+import com.rarible.protocol.order.core.validator.PayoutValidator
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Sort
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.inValues
+import org.springframework.data.mongodb.core.query.isEqualTo
+import org.springframework.stereotype.Component
+import java.time.Instant
+import java.util.UUID
+
+@Component
+class OrderPayoutValidationTaskHandler(
+    private val orderRepository: OrderRepository,
+    private val orderDtoConverter: OrderDtoConverter,
+    private val publisher: ProtocolOrderPublisher,
+) : TaskHandler<Long> {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override val type = "ORDER_PAYOUT_VALIDATION_TASK"
+
+    override fun runLongTask(from: Long?, param: String): Flow<Long> = flow {
+        val updatedAt = from?.let { Instant.ofEpochMilli(it) } ?: nowMillis()
+        val criteria = Criteria()
+            .and(Order::platform.name).isEqualTo(Platform.RARIBLE)
+            .and(Order::status.name).inValues(OrderStatus.ACTIVE, OrderStatus.INACTIVE, OrderStatus.NOT_STARTED)
+            .and(Order::lastUpdateAt.name).lt(updatedAt)
+
+        val query = Query(criteria)
+            //.withHint(OrderRepositoryIndexes.SELL_ORDERS_PLATFORM_STATUS_DEFINITION.indexKeys)
+            .with(Sort.by(Order::lastUpdateAt.name, "_id"))
+            .limit(1000)
+
+        val orders = orderRepository.search(query)
+
+        orders.filter { !PayoutValidator.arePayoutsValid(it.data) }
+            .forEach { cancelOrder(it) }
+
+        emit(orders.lastOrNull()?.lastUpdateAt?.toEpochMilli() ?: 0L) // Should not be null
+    }
+
+    private suspend fun cancelOrder(order: Order) {
+        val cancelled = order
+            .withCancel(true)
+            .withUpdatedStatus()
+
+        val saved = orderRepository.save(cancelled)
+        logger.info("Order ${saved.id} cancelled due to incorrect payouts")
+
+        val updateEvent = OrderUpdateEventDto(
+            eventId = UUID.randomUUID().toString(),
+            orderId = saved.id.toString(),
+            order = orderDtoConverter.convert(saved),
+            eventTimeMarks = offchainEventMark("indexer-out_order")
+        )
+        publisher.publish(updateEvent)
+    }
+
+}
