1ae621c010e63324bcef69941548ae14d3e6e064
Roman Novikov <mystdeim@gmail.com>
2023-03-16 10:59:49 +0000

    fix: adjust migration

--- erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/mongock/mongo/ChangeLog00002HistoryIndexes.kt
+++ erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/mongock/mongo/ChangeLog00002HistoryIndexes.kt
@@ -7,12 +7,19 @@ import com.rarible.protocol.erc20.core.model.Erc20TokenHistory
 import com.rarible.protocol.erc20.core.repository.Erc20ApprovalHistoryRepository
 import com.rarible.protocol.erc20.core.repository.Erc20TransferHistoryRepository
 import io.changock.migration.api.annotations.NonLockGuarded
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.data.mongodb.core.index.Index
 
 @ChangeLog(order = "00002")
 class ChangeLog00002HistoryIndexes {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @ChangeSet(id = "ChangeLog00002HistoryIndexes.historyIndex001", order = "1", author = "protocol", runAlways = true)
     fun createHistoryIndexes(@NonLockGuarded template: MongoOperations) {
@@ -22,14 +29,16 @@ class ChangeLog00002HistoryIndexes {
                 Index()
                     .on("${LogEvent::data.name}.${Erc20TokenHistory::token.name}", Sort.Direction.ASC)
                     .on("${LogEvent::data.name}.${Erc20TokenHistory::owner.name}", Sort.Direction.ASC)
-                    .on("blockNumber", Sort.Direction.ASC)
-                    .on("logIndex", Sort.Direction.ASC)
+                    .on(LogEvent::blockNumber.name, Sort.Direction.ASC)
+                    .on(LogEvent::logIndex.name, Sort.Direction.ASC)
+                    .on(LogEvent::minorLogIndex.name, Sort.Direction.ASC)
             )
             indexOps.ensureIndex(
                 Index()
                     .on("${LogEvent::data.name}.${Erc20TokenHistory::token.name}", Sort.Direction.ASC)
-                    .on("blockNumber", Sort.Direction.ASC)
-                    .on("logIndex", Sort.Direction.ASC)
+                    .on(LogEvent::blockNumber.name, Sort.Direction.ASC)
+                    .on(LogEvent::logIndex.name, Sort.Direction.ASC)
+                    .on(LogEvent::minorLogIndex.name, Sort.Direction.ASC)
             )
             indexOps.ensureIndex(
                 Index()
@@ -37,5 +46,21 @@ class ChangeLog00002HistoryIndexes {
                     .on("logIndex", Sort.Direction.ASC)
             )
         }
+    }
+
+    @ChangeSet(id = "ChangeLog00002HistoryIndexes.dropIndexes", order = "2", author = "protocol", runAlways = true)
+    fun dropUnneededIndexes(@NonLockGuarded template: ReactiveMongoOperations) = runBlocking {
+        dropIndex(template, Erc20TransferHistoryRepository.COLLECTION, "data.token_1_blockNumber_1_logIndex_1")
+        dropIndex(template, Erc20TransferHistoryRepository.COLLECTION, "data.token_1_data.owner_1_blockNumber_1_logIndex_1")
+    }
+
+    private suspend fun dropIndex(template: ReactiveMongoOperations, collection: String, indexName: String) {
+        val exists = template.indexOps(collection).indexInfo.any { it.name == indexName }.awaitFirst()
+
+        if (!exists) {
+            logger.warn("We want to drop index $collection.$indexName but it wasn't found. Skipping ...")
+            return
+        }
+        template.indexOps(collection).dropIndex(indexName).awaitFirstOrNull()
     }
 }
