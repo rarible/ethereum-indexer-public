f2fd78a5241bfdc257df0bd40c82e79e75eb1d15
Roman Novikov <mystdeim@gmail.com>
2023-04-10 16:22:43 +0100

    feat: add ownerships checker

---
+++ nft/listener/src/main/kotlin/com/rarible/protocol/nft/listener/service/checker/OwnershipBatchCheckerHandler.kt
@@ -0,0 +1,153 @@
+package com.rarible.protocol.nft.listener.service.checker
+
+import com.rarible.contracts.erc1155.IERC1155
+import com.rarible.contracts.erc721.IERC721
+import com.rarible.core.daemon.sequential.ConsumerBatchEventHandler
+import com.rarible.ethereum.domain.EthUInt256
+import com.rarible.protocol.dto.NftOwnershipDeleteEventDto
+import com.rarible.protocol.dto.NftOwnershipEventDto
+import com.rarible.protocol.dto.NftOwnershipUpdateEventDto
+import com.rarible.protocol.nft.core.model.OwnershipId
+import com.rarible.protocol.nft.core.model.TokenStandard
+import com.rarible.protocol.nft.core.metric.CheckerMetrics
+import com.rarible.protocol.nft.core.repository.token.TokenRepository
+import com.rarible.protocol.nft.listener.configuration.NftListenerProperties
+import io.daonomic.rpc.domain.Binary
+import io.daonomic.rpc.domain.Request
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import org.slf4j.LoggerFactory
+import scala.Some
+import scala.Tuple2
+import scala.jdk.javaapi.CollectionConverters
+import scalether.core.MonoEthereum
+import scalether.domain.Address
+import java.math.BigInteger
+import java.time.Duration
+import java.time.Instant
+import kotlin.random.Random
+
+class OwnershipBatchCheckerHandler(
+    private val nftListenerProperties: NftListenerProperties,
+    private val ethereum: MonoEthereum,
+    private val tokenRepository: TokenRepository,
+    private val checkerMetrics: CheckerMetrics
+) : ConsumerBatchEventHandler<NftOwnershipEventDto> {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private var lastUpdated = Instant.MIN
+    private var lastBlockNumber: Long = 0
+
+    // Map<Block number, Map<ownershipId, balance>>
+    private val blockBuffer = emptyMap<Long, MutableMap<OwnershipId, BigInteger>>().toSortedMap()
+    private val props = nftListenerProperties.ownershipCheckerProperties
+
+    override suspend fun handle(events: List<NftOwnershipEventDto>) {
+        logger.info("Handling ${events.size} NftOwnershipEventDto events")
+        try {
+            events.forEach { fillBuffer(it) }
+            checkBuffer()
+            consumeBuffer()
+        } catch (ex: Exception) {
+            logger.error("Error during checking ownership", ex)
+        }
+    }
+
+    private suspend fun fillBuffer(event: NftOwnershipEventDto) {
+        checkerMetrics.onIncoming()
+        val blockNumber = currentBlockNumber()
+        val eventBlockNumber = event.blockNumber
+        if (eventBlockNumber != null && blockNumber - eventBlockNumber < props.skipNumberOfBlocks) {
+            val eventBuffer: MutableMap<OwnershipId, BigInteger> =
+                blockBuffer.getOrPut(eventBlockNumber) { mapOf<OwnershipId, BigInteger>().toMutableMap() }
+            when (event) {
+                is NftOwnershipUpdateEventDto -> eventBuffer[OwnershipId.parseId(event.ownershipId)] = event.ownership.value
+                is NftOwnershipDeleteEventDto -> eventBuffer[OwnershipId.parseId(event.ownershipId)] = BigInteger.ZERO
+            }
+        }
+    }
+
+    private suspend fun checkBuffer() {
+        val blockNumber = currentBlockNumber()
+        blockBuffer.entries.removeIf {
+            val isDeleted = blockNumber - it.key >= props.skipNumberOfBlocks
+            if (isDeleted) {
+                logger.info("Events for ${it.key} are outdated")
+            }
+            isDeleted
+        }
+    }
+
+    private suspend fun consumeBuffer() {
+//        blockBuffer.asIterable().take(maxOf(blockBuffer.size - props.confirms, 0)).forEach {
+        blockBuffer.asIterable().forEach {
+            val eventBlockNumber = it.key
+            val events = blockBuffer.remove(eventBlockNumber)
+            logger.info("Start process ${events?.size} ownerships from ${eventBlockNumber} block [${blockBuffer.size} cached block]")
+            events?.forEach { (ownershipId, value) ->
+                val blockChainBalance = valueOfToken(ownershipId, eventBlockNumber)
+                if (blockChainBalance != null) {
+                    checkerMetrics.onCheck()
+                    if (value != blockChainBalance) {
+                        checkerMetrics.onInvalid()
+                        logger.error("Ownership is invalid: [id=${ownershipId} value=${value}(actual=${blockChainBalance}) block=${eventBlockNumber}")
+                    }
+                } else {
+                    // We don't care about null ownerships
+                }
+            }
+        }
+    }
+
+    // to prevent additional requests to the node we cache current block for some time
+    private suspend fun currentBlockNumber(): Long {
+        val now = Instant.now()
+        if (Duration.between(lastUpdated, now) > props.updateLastBlock) {
+            lastBlockNumber = ethereum.ethBlockNumber().awaitFirst().toLong()
+            lastUpdated = now
+        }
+        return lastBlockNumber
+    }
+
+    private suspend fun valueOfToken(ownershipId: OwnershipId, blockNumber: Long): BigInteger? {
+        val token = tokenRepository.findById(ownershipId.token).awaitFirstOrNull()
+        return when (token?.standard) {
+            TokenStandard.ERC721 -> valueOf721Token(ownershipId, blockNumber)
+            TokenStandard.ERC1155 -> valueOf1151Token(ownershipId, blockNumber)
+            else -> {
+                logger.warn("Ignoring checking token ownership due to token standard is missed or token doesn't exist: $ownershipId")
+                null
+            }
+        }
+    }
+
+    private suspend fun valueOf721Token(ownershipId: OwnershipId, blockNumber: Long): BigInteger {
+        val data = IERC721.ownerOfSignature().encode(ownershipId.tokenId.value)
+        val owner = Address.apply("0x" + makeRawRequest(data, ownershipId, blockNumber).substring(26))
+        return when (owner) {
+            ownershipId.owner -> BigInteger.ONE
+            else -> BigInteger.ZERO
+        }
+    }
+
+    private suspend fun valueOf1151Token(ownershipId: OwnershipId, blockNumber: Long): BigInteger {
+        val data = IERC1155.balanceOfSignature().encode(Tuple2.apply(ownershipId.owner, ownershipId.tokenId.value))
+        val response = makeRawRequest(data, ownershipId, blockNumber)
+        return EthUInt256.of(response).value
+    }
+
+    private suspend fun makeRawRequest(data: Binary, ownershipId: OwnershipId, blockNumber: Long): String {
+        val hexBlock = "0x%x".format(blockNumber)
+        val seq = CollectionConverters.asScala(
+            listOf(
+                mapOf(
+                    "data" to data.prefixed(),
+                    "to" to ownershipId.token.prefixed()
+                ), hexBlock
+            )
+        ).toSeq()
+        val request = Request.apply(Random.nextLong(), "eth_call", seq)
+        val response = ethereum.executeRaw(request).awaitFirst()
+        return (response.result() as Some).value().textValue()
+    }
+}
---
+++ nft/listener/src/test/kotlin/com/rarible/protocol/nft/listener/service/checker/OwnershipCheckerIt.kt
@@ -0,0 +1,139 @@
+package com.rarible.protocol.nft.listener.service.checker
+
+import com.rarible.core.test.wait.BlockingWait
+import com.rarible.ethereum.domain.Blockchain
+import com.rarible.ethereum.domain.EthUInt256
+import com.rarible.protocol.contracts.erc1155.rarible.ERC1155Rarible
+import com.rarible.protocol.contracts.erc721.rarible.ERC721Rarible
+import com.rarible.protocol.nft.core.metric.CheckerMetrics
+import com.rarible.protocol.nft.listener.test.AbstractIntegrationTest
+import com.rarible.protocol.nft.listener.test.IntegrationTest
+import io.micrometer.core.instrument.ImmutableTag
+import io.micrometer.core.instrument.MeterRegistry
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.runBlocking
+import org.apache.commons.lang3.RandomUtils
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.web3j.crypto.Keys
+import org.web3j.utils.Numeric
+import reactor.core.publisher.Mono
+import scala.Tuple2
+import scala.Tuple5
+import scala.Tuple6
+import scalether.domain.Address
+import scalether.domain.AddressFactory
+import scalether.transaction.MonoSigningTransactionSender
+import scalether.transaction.MonoSimpleNonceProvider
+import scalether.util.Hex
+import java.math.BigInteger
+
+// This test is flaky due to kafka events from other tests can change metrics
+// Better to run these test manually one by one
+@Disabled
+@IntegrationTest
+class OwnershipCheckerIt : AbstractIntegrationTest() {
+
+    @Autowired
+    lateinit var meterRegistry: MeterRegistry
+
+    @BeforeEach
+    fun clean() = runBlocking<Unit> {
+        meterRegistry.clear()
+    }
+
+    @Test
+    fun `should check erc721`() = runBlocking<Unit> {
+        val privateKey = Numeric.toBigInt(RandomUtils.nextBytes(32))
+        val address = Address.apply(Keys.getAddressFromPrivateKey(privateKey))
+
+        val userSender = MonoSigningTransactionSender(
+            ethereum,
+            MonoSimpleNonceProvider(ethereum),
+            privateKey,
+            BigInteger.valueOf(8000000)
+        ) { Mono.just(BigInteger.ZERO) }
+
+        val token = ERC721Rarible.deployAndWait(userSender, poller).awaitFirst()
+        token.__ERC721Rarible_init("Test", "TestSymbol", "BASE", "URI").execute().verifySuccess()
+
+        val tokenId = EthUInt256.of("0x${Hex.to(address.bytes())}00000000000000000000006B")
+
+        val mintData = Tuple5(
+            tokenId.value,
+            "TestUri",
+            arrayOf(
+                Tuple2(
+                    address,
+                    BigInteger.valueOf(10000)
+                )
+            ),
+            emptyArray<Tuple2<Address, BigInteger>>(),
+            arrayOf(ByteArray(0))
+        )
+
+        val transferTo = AddressFactory.create()
+        token.mintAndTransfer(mintData, transferTo)
+            .withSender(userSender)
+            .execute()
+            .verifySuccess()
+
+        BlockingWait.waitAssert {
+            assertThat(counter(CheckerMetrics.BALANCE_CHECK)).isEqualTo(1.0)
+            assertThat(counter(CheckerMetrics.BALANCE_INCOMING)).isEqualTo(1.0)
+            assertThat(counter(CheckerMetrics.BALANCE_INVALID)).isEqualTo(0.0)
+        }
+    }
+
+    @Test
+    fun `should check erc1155`() = runBlocking<Unit> {
+        val privateKey = Numeric.toBigInt(RandomUtils.nextBytes(32))
+        val address = Address.apply(Keys.getAddressFromPrivateKey(privateKey))
+
+        val userSender = MonoSigningTransactionSender(
+            ethereum,
+            MonoSimpleNonceProvider(ethereum),
+            privateKey,
+            BigInteger.valueOf(8000000)
+        ) { Mono.just(BigInteger.ZERO) }
+
+        val token = ERC1155Rarible.deployAndWait(userSender, poller).awaitFirst()
+        token.__ERC1155Rarible_init("Test", "TestSymbol", "BASE", "URI").execute().verifySuccess()
+
+        val tokenId = EthUInt256.of("0x${Hex.to(address.bytes())}00000000000000000000006B")
+
+        val mintData = Tuple6(
+            tokenId.value,
+            "TestUri",
+            BigInteger.ONE,
+            arrayOf(
+                Tuple2(
+                    address,
+                    BigInteger.valueOf(10000)
+                )
+            ),
+            emptyArray<Tuple2<Address, BigInteger>>(),
+            arrayOf(ByteArray(0))
+        )
+
+        val transferTo = AddressFactory.create()
+        token.mintAndTransfer(mintData, transferTo, BigInteger.ONE)
+            .withSender(userSender)
+            .execute()
+            .verifySuccess()
+
+        BlockingWait.waitAssert {
+            assertThat(counter(CheckerMetrics.BALANCE_CHECK)).isEqualTo(1.0)
+            assertThat(counter(CheckerMetrics.BALANCE_INCOMING)).isEqualTo(1.0)
+            assertThat(counter(CheckerMetrics.BALANCE_INVALID)).isEqualTo(0.0)
+        }
+    }
+
+    private fun counter(name: String): Double {
+        return meterRegistry.counter(name, listOf(ImmutableTag("blockchain", Blockchain.ETHEREUM.name.lowercase()))).count()
+    }
+
+}
