28579546e24963e7f8b5db4cc5fe1850fd83a0f6
sanufriev <s.anufriev@rarible.com>
2023-04-14 16:08:40 +0700

    feat: Add all

---
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/descriptors/exchange/looksrare/LooksrareV2ExchangeTakerBidDescriptor.kt
@@ -0,0 +1,78 @@
+package com.rarible.protocol.order.listener.service.descriptors.exchange.looksrare
+
+import com.rarible.core.apm.CaptureSpan
+import com.rarible.core.apm.SpanType
+import com.rarible.core.telemetry.metrics.RegisteredCounter
+import com.rarible.ethereum.domain.EthUInt256
+import com.rarible.protocol.contracts.looksrare.v2.TakerAskEvent
+import com.rarible.protocol.contracts.looksrare.v2.TakerBidEvent
+import com.rarible.protocol.order.core.repository.order.OrderRepository
+import com.rarible.protocol.order.core.service.PriceNormalizer
+import com.rarible.protocol.order.core.service.PriceUpdateService
+import com.rarible.protocol.order.core.service.ContractsProvider
+import com.rarible.protocol.order.listener.service.looksrare.TokenStandardProvider
+import io.daonomic.rpc.domain.Word
+import org.springframework.stereotype.Service
+import scalether.domain.Address
+import scalether.domain.response.Log
+import java.math.BigInteger
+
+@Service
+@CaptureSpan(type = SpanType.EVENT)
+class LooksrareV2ExchangeTakerBidDescriptor(
+    contractsProvider: ContractsProvider,
+    orderRepository: OrderRepository,
+    looksrareCancelOrdersEventMetric: RegisteredCounter,
+    looksrareTakeAskEventMetric: RegisteredCounter,
+    wrapperLooksrareMatchEventMetric: RegisteredCounter,
+    tokenStandardProvider: TokenStandardProvider,
+    priceUpdateService: PriceUpdateService,
+    prizeNormalizer: PriceNormalizer,
+) : AbstractLooksrareV1ExchangeTakerDescriptor(
+    name = "lr_v2_taker_bid",
+    TakerBidEvent.id(),
+    contractsProvider.looksrareV2(),
+    contractsProvider,
+    orderRepository,
+    looksrareCancelOrdersEventMetric,
+    looksrareTakeAskEventMetric,
+    wrapperLooksrareMatchEventMetric,
+    tokenStandardProvider,
+    priceUpdateService,
+    prizeNormalizer,
+) {
+    override fun getTakeEvent(log: Log): TakeEvent? {
+        val event = TakerBidEvent.apply(log)
+        val tokenId = event.itemIds().takeIf { ids -> ids.size == 1 }?.single() ?: run {
+            logger.warn("Item id is not single: ${event.itemIds()}, tx=${log.transactionHash()}")
+            return null
+        }
+        val amount = event.amounts().takeIf { ids -> ids.size == 1 }?.single() ?: run {
+            logger.warn("Item amount is not single: ${event.amounts()}, tx=${log.transactionHash()}")
+            return null
+        }
+        val taker = event.bidUser()
+        val maker = event.feeRecipients()[0]
+        val creator = event.feeRecipients()[1]
+        val price = event.feeAmounts()[0]
+        val royalty = event.feeAmounts()[1]
+        val fee = event.feeAmounts()[2]
+        return TakeEvent(
+            maker = maker,
+            taker = taker,
+            orderHash = Word.apply(event.nonceInvalidationParameters()._1()),
+            orderNonce = event.nonceInvalidationParameters()._2(),
+            currency = event.currency(),
+            collection = event.collection(),
+            tokenId = EthUInt256.of(tokenId),
+            amount = EthUInt256.of(amount),
+            price = EthUInt256.of(price),
+            isAsk = false,
+            royalty = if (creator == Address.ZERO() || royalty == BigInteger.ZERO) null else {
+                Royalty(creator, EthUInt256.of(royalty))
+            },
+            protocolFee = fee,
+            isNonceInvalidated = event.nonceInvalidationParameters()._3(),
+        )
+    }
+}
--- order/listener/src/test/kotlin/com/rarible/protocol/order/listener/service/descriptors/exchange/looksrare/LooksrareV2ExchangeTakerDescriptorTest.kt
+++ order/listener/src/test/kotlin/com/rarible/protocol/order/listener/service/descriptors/exchange/looksrare/LooksrareV2ExchangeTakerDescriptorTest.kt
@@ -1,5 +1,6 @@
 package com.rarible.protocol.order.listener.service.descriptors.exchange.looksrare
 
+import com.rarible.core.contract.model.Erc20Token
 import com.rarible.core.telemetry.metrics.RegisteredCounter
 import com.rarible.core.test.data.randomAddress
 import com.rarible.ethereum.contract.service.ContractService
@@ -7,6 +8,7 @@ import com.rarible.ethereum.domain.EthUInt256
 import com.rarible.protocol.order.core.data.randomBidOrderUsdValue
 import com.rarible.protocol.order.core.data.randomSellOrderUsdValue
 import com.rarible.protocol.order.core.model.Asset
+import com.rarible.protocol.order.core.model.Erc20AssetType
 import com.rarible.protocol.order.core.model.Erc721AssetType
 import com.rarible.protocol.order.core.model.EthAssetType
 import com.rarible.protocol.order.core.model.HistorySource
@@ -54,7 +56,17 @@ internal class LooksrareV2ExchangeTakerDescriptorTest {
     private val orderRepository = mockk<OrderRepository>()
     private val prizeNormalizer = PriceNormalizer(contractService)
 
-    private val descriptorBid = LooksrareV2ExchangeTakerAskDescriptor(
+    private val descriptorAsk = LooksrareV2ExchangeTakerAskDescriptor(
+        contractsProvider,
+        orderRepository,
+        looksrareCancelOrdersEventMetric,
+        looksrareTakeEventMetric,
+        wrapperLooksrareMetric,
+        tokenStandardProvider,
+        priceUpdateService,
+        prizeNormalizer,
+    )
+    private val descriptorBid = LooksrareV2ExchangeTakerBidDescriptor(
         contractsProvider,
         orderRepository,
         looksrareCancelOrdersEventMetric,
@@ -118,7 +130,7 @@ internal class LooksrareV2ExchangeTakerDescriptorTest {
         coEvery { tokenStandardProvider.getTokenStandard(nftAssetType.token) } returns TokenStandard.ERC721
         coEvery { orderRepository.findByMakeAndByCounters(any(), any(), any()) } returns emptyFlow()
 
-        val matches = descriptorBid.convert(log, transaction, data.epochSecond, 0, 0).toFlux().collectList()
+        val matches = descriptorAsk.convert(log, transaction, data.epochSecond, 0, 0).toFlux().collectList()
             .awaitFirst()
 
         assertThat(matches).hasSize(2)
@@ -154,6 +166,106 @@ internal class LooksrareV2ExchangeTakerDescriptorTest {
         assertThat(right.makePriceUsd).isEqualTo(bidOrderUsd.makePriceUsd)
         assertThat(right.takePriceUsd).isEqualTo(bidOrderUsd.takePriceUsd)
         assertThat(right.makeValue).isEqualTo(BigDecimal("0.009800000000000000"))
+        assertThat(right.takeValue).isEqualTo(BigDecimal.ONE)
+        assertThat(right.source).isEqualTo(HistorySource.LOOKSRARE)
+        assertThat(left.adhoc).isFalse()
+        assertThat(left.counterAdhoc).isTrue()
+    }
+
+    @Test
+    fun `should convert erc721 bid event`() = runBlocking<Unit> {
+        val transaction = mockk<Transaction> { every { input() } returns Binary.empty() }
+        val data = Instant.now().truncatedTo(ChronoUnit.SECONDS)
+        val bidOrderUsd = randomBidOrderUsdValue()
+        val sellOrderUsd = randomSellOrderUsdValue()
+        val contract = Erc20Token(name = "", id = randomAddress(), symbol = "", decimals = 18)
+        val log = log(
+            listOf(
+                Word.apply("0x9aaa45d6db2ef74ead0751ea9113263d1dec1b50cea05f0ca2002cb8063564a4"),
+            ),
+               "d4c830c73bef43192c2a91303ce93187f1cf0b8f74963385de1ba477f9a0a2ad" +
+                    "0000000000000000000000000000000000000000000000000000000000000001" +
+                    "0000000000000000000000000000000000000000000000000000000000000001" +
+                    "0000000000000000000000006c8ba1dafb22eae61e9cd3da724cbc3d164c27b9" +
+                    "00000000000000000000000047921676a46ccfe3d80b161c7b4ddc8ed9e716b6" +
+                    "0000000000000000000000000000000000000000000000000000000000000000" +
+                    "000000000000000000000000b4fbf271143f4fbf7b91a5ded31805e42b2208d6" +
+                    "000000000000000000000000d8560c88d1dc85f9ed05b25878e366c49b68bef9" +
+                    "00000000000000000000000000000000000000000000000000000000000001e0" +
+                    "0000000000000000000000000000000000000000000000000000000000000220" +
+                    "0000000000000000000000006c8ba1dafb22eae61e9cd3da724cbc3d164c27b9" +
+                    "0000000000000000000000000000000000000000000000000000000000000000" +
+                    "0000000000000000000000000000000000000000000000000011688627664000" +
+                    "0000000000000000000000000000000000000000000000000000000000000000" +
+                    "00000000000000000000000000000000000000000000000000005af3107a4000" +
+                    "0000000000000000000000000000000000000000000000000000000000000001" +
+                    "47921676a46ccfe3d80b161c7b4ddc8ed9e716b6000000000000000000000001" +
+                    "0000000000000000000000000000000000000000000000000000000000000001" +
+                    "0000000000000000000000000000000000000000000000000000000000000001"
+        )
+        val nftAssetType = Erc721AssetType(
+            Address.apply("0xd8560c88d1dc85f9ed05b25878e366c49b68bef9"),
+            EthUInt256.of("32372326957878872325869669322028881416287194712918919938492792330334129618945")
+        )
+        val expectedNft = Asset(
+            nftAssetType,
+            EthUInt256.ONE
+        )
+        val expectedPayment = Asset(
+            Erc20AssetType(Address.apply("0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6")),
+            EthUInt256.of("4900000000000000")
+        )
+        coEvery {
+            contractService.get(Address.apply("0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6"))
+        } returns contract
+        coEvery {
+            priceUpdateService.getAssetsUsdValue(make = expectedNft, take = expectedPayment, at = data)
+        } returns sellOrderUsd
+
+        coEvery {
+            priceUpdateService.getAssetsUsdValue(make = expectedPayment, take = expectedNft, at = data)
+        } returns bidOrderUsd
+
+        coEvery { tokenStandardProvider.getTokenStandard(nftAssetType.token) } returns TokenStandard.ERC721
+        coEvery { orderRepository.findByMakeAndByCounters(any(), any(), any()) } returns emptyFlow()
+
+        val matches = descriptorBid
+            .convert(log, transaction, data.epochSecond, 0, 0).toFlux().collectList()
+            .awaitFirst()
+
+        assertThat(matches).hasSize(2)
+        val left = matches[0] as OrderSideMatch
+        assertThat(left.hash).isEqualTo(Word.apply("d4c830c73bef43192c2a91303ce93187f1cf0b8f74963385de1ba477f9a0a2ad"))
+        assertThat(left.side).isEqualTo(OrderSide.LEFT)
+        assertThat(left.maker).isEqualTo(Address.apply("0x6c8ba1dafb22eae61e9cd3da724cbc3d164c27b9"))
+        assertThat(left.taker).isEqualTo(Address.apply("0x6c8ba1dafb22eae61e9cd3da724cbc3d164c27b9"))
+        assertThat(left.make).isEqualTo(expectedPayment)
+        assertThat(left.take).isEqualTo(expectedNft)
+        assertThat(left.fill).isEqualTo(expectedNft.value)
+        assertThat(left.date).isEqualTo(data)
+        assertThat(left.makeUsd).isEqualTo(sellOrderUsd.makeUsd)
+        assertThat(left.takeUsd).isEqualTo(sellOrderUsd.takeUsd)
+        assertThat(left.makePriceUsd).isEqualTo(sellOrderUsd.makePriceUsd)
+        assertThat(left.takePriceUsd).isEqualTo(sellOrderUsd.takePriceUsd)
+        assertThat(left.takeValue).isEqualTo(BigDecimal("0.009800000000000000"))
+        assertThat(left.makeValue).isEqualTo(BigDecimal.ONE)
+        assertThat(left.source).isEqualTo(HistorySource.LOOKSRARE)
+        assertThat(left.adhoc).isFalse()
+        assertThat(left.counterAdhoc).isTrue()
+        val right = matches[1] as OrderSideMatch
+        assertThat(right.counterHash).isEqualTo(Word.apply("0x4b5927612372ee90e7c770328f999a62e862d815c133a2ad75e840997c735760"))
+        assertThat(right.side).isEqualTo(OrderSide.RIGHT)
+        assertThat(right.maker).isEqualTo(Address.apply("0x47921676A46CcFe3D80b161c7B4DDC8Ed9e716B6"))
+        assertThat(right.taker).isEqualTo(Address.apply("0x6c8ba1dafb22eae61e9cd3da724cbc3d164c27b9"))
+        assertThat(right.make).isEqualTo(expectedPayment)
+        assertThat(right.take).isEqualTo(expectedNft)
+        assertThat(right.fill).isEqualTo(expectedNft.value)
+        assertThat(right.date).isEqualTo(data)
+        assertThat(right.makeUsd).isEqualTo(bidOrderUsd.makeUsd)
+        assertThat(right.takeUsd).isEqualTo(bidOrderUsd.takeUsd)
+        assertThat(right.makePriceUsd).isEqualTo(bidOrderUsd.makePriceUsd)
+        assertThat(right.takePriceUsd).isEqualTo(bidOrderUsd.takePriceUsd)
+        assertThat(right.makeValue).isEqualTo(BigDecimal("0.004900000000000000"))
         assertThat(right.takeValue).isEqualTo(BigDecimal.ONE)
         assertThat(right.source).isEqualTo(HistorySource.LOOKSRARE)
         assertThat(left.adhoc).isFalse()
