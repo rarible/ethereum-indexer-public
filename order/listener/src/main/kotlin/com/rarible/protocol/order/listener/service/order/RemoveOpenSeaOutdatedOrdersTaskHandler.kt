cd33be5684176de1e044f12e250c0c8cc07252b6
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2023-05-03 13:04:34 +0400

    fix: Seaport orders V1 cancellation (#605)

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/order/RemoveOpenSeaOutdatedOrdersTaskHandler.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/order/RemoveOpenSeaOutdatedOrdersTaskHandler.kt
@@ -3,6 +3,7 @@ package com.rarible.protocol.order.listener.service.order
 import com.rarible.core.task.TaskHandler
 import com.rarible.protocol.order.core.configuration.OrderIndexerProperties
 import com.rarible.protocol.order.core.model.Order
+import com.rarible.protocol.order.core.model.OrderBasicSeaportDataV1
 import com.rarible.protocol.order.core.model.OrderStatus
 import com.rarible.protocol.order.core.model.OrderType
 import com.rarible.protocol.order.core.model.Platform
@@ -11,7 +12,6 @@ import com.rarible.protocol.order.core.service.OrderUpdateService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
@@ -22,31 +22,33 @@ class RemoveOpenSeaOutdatedOrdersTaskHandler(
     private val exchangeContractAddresses: OrderIndexerProperties.ExchangeContractAddresses,
 ) : TaskHandler<String> {
 
-    val logger: Logger = LoggerFactory.getLogger(RemoveOpenSeaOutdatedOrdersTaskHandler::class.java)
+    private val logger = LoggerFactory.getLogger(javaClass)
 
-    override val type: String
-        get() = REMOVE_OPEN_SEA_OUTDATED_ORDERS
+    override val type = "REMOVE_OPEN_SEA_OUTDATED_ORDERS"
 
     override fun runLongTask(from: String?, param: String): Flow<String> {
         val status = OrderStatus.valueOf(param)
-        logger.info("Start $REMOVE_OPEN_SEA_OUTDATED_ORDERS task with $status param")
+        logger.info("Start $type task with $status param")
         return orderRepository
             .findAll(Platform.OPEN_SEA, status, fromHash = null)
-            .filter { isOpenSeaOrderType(it) }
+            .filter { isCandidateToBeCancelled(it) }
             .map { updateOrder(it) }
     }
 
-    private fun isOpenSeaOrderType(order: Order): Boolean {
-        return order.type == OrderType.OPEN_SEA_V1
+    private fun isCandidateToBeCancelled(order: Order): Boolean {
+        // All opensea legacy orders should be cancelled
+        if (order.type == OrderType.OPEN_SEA_V1) {
+            return true
+        }
+        if (order.type == OrderType.SEAPORT_V1) {
+            // Seaport V1.1 is not supported anymore
+            return (order.data as? OrderBasicSeaportDataV1)?.protocol == exchangeContractAddresses.seaportV1
+        }
+        return false
     }
 
     private suspend fun updateOrder(order: Order): String {
         orderUpdateService.update(order.hash)
         return order.id.toString()
-    }
-
-    companion object {
-        val logger: Logger = LoggerFactory.getLogger(RemoveOpenSeaOutdatedOrdersTaskHandler::class.java)
-        const val REMOVE_OPEN_SEA_OUTDATED_ORDERS = "REMOVE_OPEN_SEA_OUTDATED_ORDERS"
     }
 }
