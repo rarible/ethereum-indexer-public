ea488b7f148b267f281fb0e8d0cf1b4a94b50802
Dmitry Saraykin <31763675+dmitry-saraykin@users.noreply.github.com>
2023-05-26 14:56:22 +0300

    fix(PT-2978): ff for bids with no expiry

--- order/core/src/main/kotlin/com/rarible/protocol/order/core/configuration/OrderIndexerProperties.kt
+++ order/core/src/main/kotlin/com/rarible/protocol/order/core/configuration/OrderIndexerProperties.kt
@@ -134,7 +134,8 @@ data class OrderIndexerProperties(
         val findIgnoredEvent: Boolean = false,
         val removeOpenSeaOrdersInTask: Boolean = false,
         val parseBlurMarketPlaceV2: Boolean = false,
-        val cancelOrderOnGetSignatureError: Boolean = true
+        val cancelOrderOnGetSignatureError: Boolean = true,
+        val enableBidWithNoExpiryCancelation: Boolean = true,
     )
 
     data class RaribleOrderExpirationProperties(
