4bf88288e0e3fcef4b5f3e2d7c1895c87621f45a
Dmitry Saraykin <31763675+dmitry-saraykin@users.noreply.github.com>
2023-06-27 15:11:13 +0300

    fix(PT-3194): fix properties resolver (#672)

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/meta/descriptors/RariblePropertiesResolver.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/item/meta/descriptors/RariblePropertiesResolver.kt
@@ -1,13 +1,11 @@
 package com.rarible.protocol.nft.core.service.item.meta.descriptors
 
-import com.rarible.core.apm.CaptureSpan
 import com.rarible.core.common.ifNotBlank
 import com.rarible.core.meta.resource.model.UrlResource
 import com.rarible.protocol.nft.core.model.ItemId
 import com.rarible.protocol.nft.core.model.ItemProperties
 import com.rarible.protocol.nft.core.service.UrlService
 import com.rarible.protocol.nft.core.service.item.meta.BlockchainTokenUriResolver
-import com.rarible.protocol.nft.core.service.item.meta.ITEM_META_CAPTURE_SPAN_TYPE
 import com.rarible.protocol.nft.core.service.item.meta.MetaException
 import com.rarible.protocol.nft.core.service.item.meta.logMetaLoading
 import com.rarible.protocol.nft.core.service.item.meta.properties.ItemPropertiesParser
@@ -105,7 +103,6 @@ abstract class AbstractRariblePropertiesResolver(
 }
 
 @Component
-@CaptureSpan(type = ITEM_META_CAPTURE_SPAN_TYPE)
 class RariblePropertiesResolver(
     urlService: UrlService,
     rawPropertiesProvider: RawPropertiesProvider,
