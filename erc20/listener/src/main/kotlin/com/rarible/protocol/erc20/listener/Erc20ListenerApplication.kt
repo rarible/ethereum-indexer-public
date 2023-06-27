2795b4a14379834cc2169657116cf57ff3faffe1
sanufriev <s.anufriev@rarible.com>
2023-06-27 17:45:08 +0700

    feat: PT-3169 Start consumers

--- erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/Erc20ListenerApplication.kt
+++ erc20/listener/src/main/kotlin/com/rarible/protocol/erc20/listener/Erc20ListenerApplication.kt
@@ -1,10 +1,19 @@
 package com.rarible.protocol.erc20.listener
 
+import com.rarible.core.kafka.RaribleKafkaConsumerWorker
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class Erc20ListenerApplication
+class Erc20ListenerApplication(
+    private val kafkaConsumers: List<RaribleKafkaConsumerWorker<*>>,
+) : CommandLineRunner {
+
+    override fun run(vararg args: String?) {
+        kafkaConsumers.forEach { it.start() }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Erc20ListenerApplication>(*args)
