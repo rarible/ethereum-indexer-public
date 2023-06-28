074611f49f622539d8807898292647d0d442b526
Artem Yakischik <a.yakischik@rarible.com>
2023-06-28 16:27:49 +0400

    CR fix

--- erc20/migration/src/test/java/com/rarible/protocol/erc20/migration/test/IntegrationTest.kt
+++ erc20/migration/src/test/java/com/rarible/protocol/erc20/migration/test/IntegrationTest.kt
@@ -1,6 +1,5 @@
 package com.rarible.protocol.erc20.migration.test
 
-import com.rarible.core.test.ext.KafkaTest
 import com.rarible.core.test.ext.MongoTest
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson
 import org.springframework.boot.test.context.SpringBootTest
@@ -10,7 +9,6 @@ import org.testcontainers.junit.jupiter.Testcontainers
 
 @Retention
 @MongoTest
-@KafkaTest
 @AutoConfigureJson
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.MOCK,
