3c783ab5775dea5d00619a956e07657858948fce
sanufriev <s.anufriev@rarible.com>
2023-06-29 14:10:25 +0700

    feat: Fix compilation

--- order/core/src/main/kotlin/com/rarible/protocol/order/core/model/SeaportOrderType.kt
+++ order/core/src/main/kotlin/com/rarible/protocol/order/core/model/SeaportOrderType.kt
@@ -5,7 +5,7 @@ enum class SeaportOrderType(val value: Int) {
     PARTIAL_OPEN(1), // Partial fills supported, anyone can execute
     FULL_RESTRICTED(2), // No partial fills, only offerer or zone can execute
     PARTIAL_RESTRICTED(3), // Partial fills supported, only offerer or zone can execute
-    CONTRACT(4),
+    CONTRACT(4), // Contract order type
     ;
 
     companion object {
