ea3a918ca0008aefdbaa9589f2816c53b22bd07d
sanufriev <s.anufriev@rarible.com>
2023-07-12 12:47:56 +0700

    feat: Add comment

--- erc20/core/src/main/kotlin/com/rarible/protocol/erc20/core/service/reduce/reversed/RevertBalanceCompactEventsReducer.kt
+++ erc20/core/src/main/kotlin/com/rarible/protocol/erc20/core/service/reduce/reversed/RevertBalanceCompactEventsReducer.kt
@@ -6,6 +6,7 @@ import com.rarible.protocol.erc20.core.model.Erc20Balance
 import com.rarible.protocol.erc20.core.model.Erc20Event
 import org.springframework.stereotype.Component
 
+//TODO: Full support [CompactEventsReducer]
 @Component
 class RevertBalanceCompactEventsReducer : RevertCompactEventsReducer<BalanceId, Erc20Event, Erc20Balance>() {
     override fun merge(reverted: Erc20Event, compact: Erc20Event): Erc20Event {
