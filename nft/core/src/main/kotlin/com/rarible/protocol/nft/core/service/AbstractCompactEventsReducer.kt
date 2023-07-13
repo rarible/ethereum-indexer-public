01baa2be39ba7ba7eccc5773b992508e31b9270e
sanufriev <s.anufriev@rarible.com>
2023-07-13 10:04:42 +0700

    feat: Improve scanning

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/AbstractCompactEventsReducer.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/service/AbstractCompactEventsReducer.kt
@@ -5,6 +5,7 @@ import com.rarible.blockchain.scanner.ethereum.model.EthereumEntityEvent
 import com.rarible.core.entity.reducer.model.Entity
 import com.rarible.protocol.nft.core.configuration.NftIndexerProperties
 import com.rarible.protocol.nft.core.model.FeatureFlags
+import org.slf4j.LoggerFactory
 
 abstract class AbstractCompactEventsReducer<Id, Event : EthereumEntityEvent<Event>, E : Entity<Id, Event, E>>(
     private val featureFlags: FeatureFlags,
@@ -12,10 +13,15 @@ abstract class AbstractCompactEventsReducer<Id, Event : EthereumEntityEvent<Even
 ) : CompactEventsReducer<Id, Event, E>() {
 
     override suspend fun reduce(entity: E, event: Event): E {
-        if (featureFlags.compactRevertableEvents.not() ||
-            entity.revertableEvents.size < properties.maxRevertableEventsAmount) {
+        if (featureFlags.compactRevertableEvents.not()) {
             return entity
         }
+        if (entity.revertableEvents.size < properties.maxRevertableEventsAmount) {
+            return entity
+        }
+        logger.info("Detect compacting for entity: ${entity.id}, size: ${entity.revertableEvents.size}")
         return super.reduce(entity, event)
     }
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 }
