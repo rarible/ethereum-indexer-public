2e14294c7d00c33907787fdd9b322786bde82a69
Sasha Bukata <96521086+alexbukata@users.noreply.github.com>
2023-08-10 15:46:17 +0100

    Merge main -> 1.57 (#724)

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/model/Ownership.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/model/Ownership.kt
@@ -83,11 +83,15 @@ data class Ownership(
 
         fun parseId(id: String): OwnershipId {
             val parts = id.split(":")
-            if (parts.size < 3) {
-                throw IllegalArgumentException("Incorrect format of ownershipId: $id")
+            try {
+                return OwnershipId(
+                    token = Address.apply(parts[0].trim()),
+                    tokenId = EthUInt256.of(parts[1].trim()),
+                    owner = Address.apply(parts[2].trim())
+                )
+            } catch (e: Exception) {
+                throw IllegalArgumentException("Incorrect format of ownershipId: $id", e)
             }
-            val tokenId = EthUInt256.of(parts[1].trim())
-            return OwnershipId(Address.apply(parts[0].trim()), tokenId, Address.apply(parts[2].trim()))
         }
 
         fun empty(token: Address, tokenId: EthUInt256, owner: Address, version: Long?): Ownership {
