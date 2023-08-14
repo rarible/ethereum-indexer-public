13c10311c2ba92854df89107e914429965069df3
Artem Yakischik <a.yakischik@rarible.com>
2023-08-14 17:46:30 +0400

    feat: Async ownership handling in order-indexer

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/OwnershipConsumerEventHandler.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/event/OwnershipConsumerEventHandler.kt
@@ -1,20 +1,34 @@
 package com.rarible.protocol.order.listener.service.event
 
-import com.rarible.core.kafka.RaribleKafkaEventHandler
+import com.rarible.core.kafka.RaribleKafkaBatchEventHandler
 import com.rarible.protocol.dto.NftOwnershipEventDto
 import com.rarible.protocol.order.listener.service.order.OrderBalanceService
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
 class OwnershipConsumerEventHandler(
     private val orderBalanceService: OrderBalanceService
-) : RaribleKafkaEventHandler<NftOwnershipEventDto> {
+) : RaribleKafkaBatchEventHandler<NftOwnershipEventDto> {
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    override suspend fun handle(event: NftOwnershipEventDto) {
-        logger.info("Got nft ownership event: $event")
-        orderBalanceService.handle(event)
+    override suspend fun handle(event: List<NftOwnershipEventDto>) {
+        val start = System.currentTimeMillis()
+        coroutineScope {
+            event.groupBy { it.ownershipId }.map { group ->
+                async {
+                    val ownershipEvents = group.value
+                    ownershipEvents.forEach { ownershipEvent ->
+                        logger.info("Got Ownership event: $ownershipEvent")
+                        orderBalanceService.handle(ownershipEvent)
+                    }
+                }
+            }.awaitAll()
+        }
+        logger.info("Handled group of {} Ownership events ({}ms)", event.size, System.currentTimeMillis() - start)
     }
 }
