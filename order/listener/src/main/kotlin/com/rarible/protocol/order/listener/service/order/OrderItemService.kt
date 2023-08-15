53259011ee918387904c80ef48f2221def4d197b
Artem Yakischik <83639804+yakischik@users.noreply.github.com>
2023-08-15 11:16:10 +0400

    feat: Async order cancellation for suspicious items (#727)
    * feat: Async order cancellation for suspicious items
    
    * CR update

--- order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/order/OrderItemService.kt
+++ order/listener/src/main/kotlin/com/rarible/protocol/order/listener/service/order/OrderItemService.kt
@@ -1,6 +1,7 @@
 package com.rarible.protocol.order.listener.service.order
 
 import com.rarible.core.common.EventTimeMarks
+import com.rarible.core.common.asyncWithTraceId
 import com.rarible.ethereum.domain.EthUInt256
 import com.rarible.protocol.dto.NftItemDto
 import com.rarible.protocol.dto.NftItemUpdateEventDto
@@ -11,6 +12,12 @@ import com.rarible.protocol.order.core.model.ItemId
 import com.rarible.protocol.order.core.model.Platform
 import com.rarible.protocol.order.core.repository.order.OrderRepository
 import com.rarible.protocol.order.core.service.OrderCancelService
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
@@ -40,13 +47,41 @@ class OrderItemService(
     }
 
     private suspend fun onOsSuspiciousItem(itemId: ItemId, eventTimeMarks: EventTimeMarks) {
-        orderRepository.findSellOrdersNotCancelledByItemId(
+        val start = System.currentTimeMillis()
+        val toCancel = orderRepository.findSellOrdersNotCancelledByItemId(
             Platform.OPEN_SEA,
             itemId.contract,
             EthUInt256.of(itemId.tokenId)
-        ).collect { order ->
-            logger.info("Cancelled Order {} - Item {} marked as suspicious", order.hash.prefixed(), itemId)
-            orderCancelService.cancelOrder(order.hash, eventTimeMarks)
+        ).take(ORDER_TO_CANCEL_MAX_COUNT).map { it.hash }.toList()
+
+        // Should never happen since there are not a lot of such items/orders, but let's monitor it
+        if (toCancel.size == ORDER_TO_CANCEL_MAX_COUNT) {
+            logger.warn("WARNING! Too many active Orders found for Item {} marked as suspicious", itemId)
         }
+
+        if (toCancel.isEmpty()) {
+            return
+        }
+        coroutineScope {
+            toCancel.chunked(ORDER_TO_CANCEL_CHUNK_SIZE).forEach { chunk ->
+                chunk.map { hash ->
+                    asyncWithTraceId(context = NonCancellable) {
+                        logger.info("Cancelled Order {} for Item {} marked as suspicious", hash.prefixed(), itemId)
+                        orderCancelService.cancelOrder(hash, eventTimeMarks)
+                    }
+                }.awaitAll()
+            }
+        }
+        logger.info(
+            "Cancelled {} active Orders for Item {} marked as suspicious ({}ms)",
+            toCancel.size,
+            itemId,
+            System.currentTimeMillis() - start
+        )
+    }
+
+    companion object {
+        const val ORDER_TO_CANCEL_MAX_COUNT = 1000
+        const val ORDER_TO_CANCEL_CHUNK_SIZE = 100
     }
 }
