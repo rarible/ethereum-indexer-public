4533d6b32e6bc8373b2273cfc7e7b59bef624ece
Alex Bukata <a.bukata@rarible.com>
2023-10-04 13:09:03 +0100

    PT-3851 forbid order creation for paused collection

---
+++ order/api/src/main/kotlin/com/rarible/protocol/order/api/service/order/validation/validators/TokenPauseOrderValidator.kt
@@ -0,0 +1,31 @@
+package com.rarible.protocol.order.api.service.order.validation.validators
+
+import com.rarible.protocol.dto.NftCollectionFlagDto
+import com.rarible.protocol.order.api.form.OrderForm
+import com.rarible.protocol.order.api.service.order.validation.OrderFormValidator
+import com.rarible.protocol.order.core.exception.ValidationApiException
+import com.rarible.protocol.order.core.model.token
+import com.rarible.protocol.order.core.service.nft.NftCollectionApiService
+import org.springframework.stereotype.Component
+
+@Component
+class TokenPauseOrderValidator(
+    val nftCollectionApiService: NftCollectionApiService
+) : OrderFormValidator {
+    override suspend fun validate(form: OrderForm) {
+        val token = if (form.make.type.nft) {
+            form.make.type.token
+        } else if (form.take.type.nft) {
+            form.take.type.token
+        } else {
+            return
+        }
+        val collection = nftCollectionApiService.getNftCollectionById(token) ?: return
+        val paused = collection.flags.orEmpty()
+            .firstOrNull { it.flag == NftCollectionFlagDto.Flag.PAUSED }
+            ?.value?.toBoolean() ?: false
+        if (paused) {
+            throw ValidationApiException("Collection $token is paused")
+        }
+    }
+}
