b214a56bcb2e0827b7a6883839a1f0d66a94b240
Roman Novikov <mystdeim@gmail.com>
2023-10-06 15:28:56 +0100

    feat: add not for sale

--- nft/core/src/main/kotlin/com/rarible/protocol/nft/core/model/TokenFeature.kt
+++ nft/core/src/main/kotlin/com/rarible/protocol/nft/core/model/TokenFeature.kt
@@ -9,5 +9,10 @@ enum class TokenFeature(val erc165: List<Binary> = emptyList()) {
     BURN,
     MINT_WITH_ADDRESS(listOf(Binary.apply(Hash.sha3("MINT_WITH_ADDRESS".toByteArray(Charsets.US_ASCII))).slice(0, 4))),
     SECONDARY_SALE_FEES(listOf(Binary.apply("0xb7799584"))),
-    MINT_AND_TRANSFER(listOf(Binary.apply("0x8486f69f"), Binary.apply("0x6db15a0f")))
+    MINT_AND_TRANSFER(listOf(Binary.apply("0x8486f69f"), Binary.apply("0x6db15a0f"))),
+    NOT_FOR_SALE(listOf(
+        Binary.apply("0x0489b56f"), // eip-5484
+        Binary.apply("0x0e89341c"), // eip-5516
+        Binary.apply("0x911ec470")) // eip-5633
+    ),
 }
